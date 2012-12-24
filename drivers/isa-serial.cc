bacfd9fd5a59b8a6ad673cdfb9adafd2c8e2a203
Dor Laor <dor.laor@gmail.com>
2012-12-24 15:37:39 +0200

    Add uart16550 serial basic driver. At the moment it can only print to the console.

---
+++ drivers/isa-serial.cc
@@ -0,0 +1,45 @@
+#include "isa-serial.hh"
+
+IsaSerialConsole::IsaSerialConsole() {
+	reset();
+}
+
+void IsaSerialConsole::write(const char *str)
+{
+    while (*str) {
+    	writeByte(*str++);
+    }
+
+}
+
+void IsaSerialConsole::writeByte(const char letter)
+{
+	pci::u8 lsr;
+
+	do {
+		lsr = pci::inb(ioport + LSR_ADDRESS);
+	} while (!(lsr & LSR_TRANSMIT_HOLD_EMPTY));
+
+	pci::outb(letter, ioport);
+}
+
+void IsaSerialConsole::newline()
+{
+	writeByte('\n');
+	writeByte('\r');
+}
+
+void IsaSerialConsole::reset() {
+	// Put the line control register in acceptance mode (latch)
+	lcr = pci::inb(ioport + LCR_ADDRESS);
+	lcr |= 1 << LCR_DIVISOR_LATCH_ACCESS_BIT & LCR_DIVISOR_LATCH_ACCESS_BIT_HIGH;
+    pci::outb(lcr, ioport + LCR_ADDRESS);
+
+    pci::outb(1, ioport + BAUD_GEN0_ADDRESS);
+    pci::outb(0, ioport + BAUD_GEN1_ADDRESS);
+
+	// Close the latch
+	lcr = pci::inb(ioport + LCR_ADDRESS);
+	lcr &= ~(1 << LCR_DIVISOR_LATCH_ACCESS_BIT & LCR_DIVISOR_LATCH_ACCESS_BIT_HIGH);
+    pci::outb(lcr, ioport + LCR_ADDRESS);
+}
