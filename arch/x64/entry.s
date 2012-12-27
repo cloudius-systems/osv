882cfc17c36cd40c86f6e17e20de54d3688bfa51
Avi Kivity <avi.kivity@gmail.com>
2012-12-27 11:02:01 +0200

    x64: extract cfi macros into a new file

--- arch/x64/entry.s
+++ arch/x64/entry.s
@@ -1,15 +1,5 @@
 
-.macro pushq_cfi reg
-	pushq \reg
-	.cfi_adjust_cfa_offset 8
-	.cfi_rel_offset \reg, 0
-.endm
-
-.macro popq_cfi reg
-	popq \reg
-	.cfi_adjust_cfa_offset -8
-	.cfi_restore \reg
-.endm
+.include "arch/x64/cfi.s"
 
 .macro exception_entry name, handler, has_error_code
 	.global \name
