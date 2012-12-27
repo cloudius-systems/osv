b68650ddd843f2390f0d0a03a7e6e8883068e310
Avi Kivity <avi.kivity@gmail.com>
2012-12-27 12:45:45 +0200

    linux: implement gettid() syscall (stub)

--- linux.cc
+++ linux.cc
@@ -11,8 +11,17 @@ extern Console* debug_console;
 
 typedef boost::format fmt;
 
+long gettid()
+{
+    return 0; // FIXME
+}
+
 long syscall(long number, ...)
 {
+    switch (number) {
+    case __NR_gettid: return gettid();
+    }
+
     debug_console->writeln(fmt("unimplemented syscall %d") % number);
     abort();
 }
