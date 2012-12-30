0816bd23bdc9dade69279fc6158db3b290abfcc9
Avi Kivity <avi.kivity@gmail.com>
2012-12-30 17:49:04 +0200

    x64: add definitions for model-specific registers

---
+++ arch/x64/msr.hh
@@ -0,0 +1,19 @@
+#ifndef MSR_HH_
+#define MSR_HH_
+
+#include "processor.hh"
+
+enum class msr : uint32_t {
+    IA32_FS_BASE = 0xc0000100,
+};
+
+namespace processor {
+
+void wrmsr(msr index, u64 value)
+{
+    wrmsr(static_cast<u32>(index), value);
+}
+
+}
+
+#endif /* MSR_HH_ */
