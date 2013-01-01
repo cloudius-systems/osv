d425f16f5898843a85076b01bc1492d2ac0be45d
Dor Laor <dor.laor@gmail.com>
2013-01-01 11:29:03 +0200

    DriverFactory keets a set of registered drivers and lookup functions to access them.

---
+++ drivers/driver-factory.cc
@@ -0,0 +1,38 @@
+#include "drivers/driver-factory.hh"
+#include "drivers/driver.hh"
+#include "debug.hh"
+
+DriverFactory* DriverFactory::pinstance = 0;
+
+void
+DriverFactory::RegisterDriver(Driver* d) {
+    if (d) _drivers.insert(d);
+}
+
+Driver*
+DriverFactory::lookup(u16 id, u16 vid) {
+    Driver dummy(id, vid);
+    Driver::equal compare;
+
+    for (auto ii = _drivers.begin() ; ii != _drivers.end() ; ii++ )
+         if (compare(&dummy, *ii)) return *ii;
+
+    return nullptr;
+}
+
+bool
+DriverFactory::InitializeDriver(Device* dev) {
+    if (!dev) return false;
+
+    Driver *drv = lookup(dev->getvid(), dev->getid());
+    if (!drv) return false;
+
+    return drv->Init(dev);
+}
+
+
+void
+DriverFactory::DumpDrivers() {
+    for (auto ii = _drivers.begin() ; ii != _drivers.end() ; ii++ )
+         (*ii)->dumpConfig();
+}
