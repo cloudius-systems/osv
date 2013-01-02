f436fb6636628e0a047231a8556de16922065059
Avi Kivity <avi.kivity@gmail.com>
2013-01-02 20:22:41 +0200

    scripts: run without SDL display
    We no longer put any useful information on the VGA device.

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,1 +1,1 @@
-qemu-system-x86_64 -enable-kvm -m 1G -chardev stdio,mux=on,id=stdio -mon chardev=stdio,mode=readline,default -device isa-serial,chardev=stdio -kernel build/debug/loader.bin
+qemu-system-x86_64 -vnc :1 -enable-kvm -m 1G -chardev stdio,mux=on,id=stdio -mon chardev=stdio,mode=readline,default -device isa-serial,chardev=stdio -kernel build/debug/loader.bin
