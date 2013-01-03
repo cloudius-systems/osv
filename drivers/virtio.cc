bc94b78e411f5ac7d3810893e2265e8bf2a250f6
Dor Laor <dor.laor@gmail.com>
2013-01-04 01:29:59 +0200

    Add all of the virtio header definitions and ring accessor code. It was portion of BSD license and taken from Linux/uapi. A matching license clause is under the license directory.

--- drivers/virtio.cc
+++ drivers/virtio.cc
@@ -39,3 +39,34 @@ void Virtio::dumpConfig() const {
     Driver::dumpConfig();
     debug(fmt("Virtio vid:id= %x:%x") % _vid % _id);
 }
+
+void
+Virtio::vring_init(struct vring *vr, unsigned int num, void *p, unsigned long align) {
+    vr->num = num;
+    vr->desc = reinterpret_cast<struct vring_desc*>(p);
+    vr->avail = reinterpret_cast<struct vring_avail*>(p) + num*sizeof(struct vring_desc);
+    vr->used = reinterpret_cast<struct vring_used*>(((unsigned long)&vr->avail->ring[num] + sizeof(u16) \
+        + align-1) & ~(align - 1));
+}
+
+unsigned
+Virtio::vring_size(unsigned int num, unsigned long align) {
+    return ((sizeof(struct vring_desc) * num + sizeof(u16) * (3 + num)
+         + align - 1) & ~(align - 1))
+        + sizeof(u16) * 3 + sizeof(struct vring_used_elem) * num;
+}
+
+/* The following is used with USED_EVENT_IDX and AVAIL_EVENT_IDX */
+/* Assuming a given event_idx value from the other size, if
+ * we have just incremented index from old to new_idx,
+ * should we trigger an event? */
+int
+Virtio::vring_need_event(u16 event_idx, u16 new_idx, u16 old) {
+    /* Note: Xen has similar logic for notification hold-off
+     * in include/xen/interface/io/ring.h with req_event and req_prod
+     * corresponding to event_idx + 1 and new_idx respectively.
+     * Note also that req_event and req_prod in Xen start at 1,
+     * event indexes in virtio start at 0. */
+    return (u16)(new_idx - event_idx - 1) < (u16)(new_idx - old);
+}
+
