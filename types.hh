a6861106c6371209e84ccc1f410bd6f8b51afba7
Avi Kivity <avi.kivity@gmail.com>
2013-01-06 13:31:14 +0200

    types: add signed fixed-width types

--- types.hh
+++ types.hh
@@ -8,6 +8,10 @@ typedef uint8_t u8;
 typedef uint16_t u16;
 typedef uint32_t u32;
 typedef uint64_t u64;
+typedef int8_t s8;
+typedef int16_t s16;
+typedef int32_t s32;
+typedef int64_t s64;
 
 typedef unsigned long ulong;
 
