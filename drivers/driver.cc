fb859bc848e0adb8957c58d2725c826e593a69f5
Guy Zana <guy@cloudius-systems.com>
2013-01-15 22:33:48 +0200

    Added pci config capability parsing and moved the virtual call to earlyInitChecks() to the base class

--- drivers/driver.cc
+++ drivers/driver.cc
@@ -85,6 +85,12 @@ bool
 Driver::Init(Device* dev) {
     if (!dev) return false;
 
+    if (!earlyInitChecks()) {
+        return false;
+    }
+
+    parse_pci_config();
+
     debug(fmt("Driver:Init %x:%x") % _vid % _id);
 
     setBusMaster(true);
@@ -114,6 +120,74 @@ u8
 Driver::getFunc() {
     if (!_present) throw new InitException();
     return _func;
+}
+
+bool Driver::parse_pci_config(void)
+{
+    // Parse capabilities
+    bool parse_ok = parse_pci_capabilities();
+
+    return parse_ok;
+}
+
+bool Driver::parse_pci_capabilities(void)
+{
+    // FIXME: check pci device type (act differently if bridge)
+    u8 capabilities_base = pci_readb(PCI_CAPABILITIES_PTR);
+    u8 off = capabilities_base;
+
+    while (off != 0) {
+        if (off > 255) {
+            return (false);
+        }
+
+        u8 capability = pci_readb(off + PCI_CAP_OFF_ID);
+        switch (capability) {
+        case PCI_CAP_MSIX:
+            _have_msix = true;
+            debug(fmt("Have MSI-X!"));
+            break;
+        }
+
+        off = pci_readb(off + PCI_CAP_OFF_NEXT);
+    }
+
+    return true;
+}
+
+bool Driver::parse_pci_msix(void)
+{
+    return true;
+}
+
+u8 Driver::pci_readb(u8 offset)
+{
+    return read_pci_config_byte(_bus, _slot, _func, offset);
+}
+
+u16 Driver::pci_readw(u8 offset)
+{
+    return read_pci_config_word(_bus, _slot, _func, offset);
+}
+
+u32 Driver::pci_readl(u8 offset)
+{
+    return read_pci_config(_bus, _slot, _func, offset);
+}
+
+void Driver::pci_writeb(u8 offset, u8 val)
+{
+    write_pci_config_byte(_bus, _slot, _func, offset, val);
+}
+
+void Driver::pci_writew(u8 offset, u16 val)
+{
+    write_pci_config_word(_bus, _slot, _func, offset, val);
+}
+
+void Driver::pci_writel(u8 offset, u32 val)
+{
+    write_pci_config(_bus, _slot, _func, offset, val);
 }
 
 void Driver::dumpConfig() const {
