22fec1553c7ea0d13338c5b3abd86dfcc90004a9
Guy Zana <guy@cloudius-systems.com>
2013-01-16 03:45:38 +0200

    Added MSI-X configuration parsing. Added the BSD license to driver.* and pci.*

--- drivers/driver.cc
+++ drivers/driver.cc
@@ -1,3 +1,29 @@
+/* This header is BSD licensed so anyone can use the definitions to implement
+ * compatible drivers/servers.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ * 3. Neither the name of IBM nor the names of its contributors
+ *    may be used to endorse or promote products derived from this software
+ *    without specific prior written permission.
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS ``AS IS'' AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED.  IN NO EVENT SHALL IBM OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
+ * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+ * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
+ * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
+ * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE. */
+
 #include "drivers/driver.hh"
 #include "drivers/pci.hh"
 #include "debug.hh"
@@ -141,11 +167,12 @@ bool Driver::parse_pci_capabilities(void)
             return (false);
         }
 
+        // Read capability
         u8 capability = pci_readb(off + PCI_CAP_OFF_ID);
         switch (capability) {
         case PCI_CAP_MSIX:
             _have_msix = true;
-            debug(fmt("Have MSI-X!"));
+            parse_pci_msix(off);
             break;
         }
 
@@ -155,8 +182,31 @@ bool Driver::parse_pci_capabilities(void)
     return true;
 }
 
-bool Driver::parse_pci_msix(void)
+bool Driver::parse_pci_msix(u8 off)
 {
+    // Used for parsing MSI-x
+    u32 val = 0;
+
+    // Location within the configuration space
+    _msix.msix_location = off;
+    _msix.msix_ctrl = pci_readw(off + PCIR_MSIX_CTRL);
+    _msix.msix_msgnum = (_msix.msix_ctrl & PCIM_MSIXCTRL_TABLE_SIZE) + 1;
+    val = pci_readl(off + PCIR_MSIX_TABLE);
+    _msix.msix_table_bar = val & PCIM_MSIX_BIR_MASK;
+    _msix.msix_table_offset = val & ~PCIM_MSIX_BIR_MASK;
+    val = pci_readl(off + PCIR_MSIX_PBA);
+    _msix.msix_pba_bar = val & PCIM_MSIX_BIR_MASK;
+    _msix.msix_pba_offset = val & ~PCIM_MSIX_BIR_MASK;
+
+    debug(fmt("Have MSI-X!"));
+    debug(fmt("    msix_location: %1%") % (u16)_msix.msix_location);
+    debug(fmt("    msix_ctrl: %1%") % _msix.msix_ctrl);
+    debug(fmt("    msix_msgnum: %1%") % _msix.msix_msgnum);
+    debug(fmt("    msix_table_bar: %1%") % (u16)_msix.msix_table_bar);
+    debug(fmt("    msix_table_offset: %1%") % _msix.msix_table_offset);
+    debug(fmt("    msix_pba_bar: %1%") % (u16)_msix.msix_pba_bar);
+    debug(fmt("    msix_pba_offset: %1%") % _msix.msix_pba_offset);
+
     return true;
 }
 
