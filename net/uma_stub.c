a138517f143b5ab59fb06b1f8896b57f417cc11b
Guy Zana <guy@cloudius-systems.com>
2013-01-31 14:59:55 +0200

    Add trivial implementation for some UMA functions

--- net/uma_stub.c
+++ net/uma_stub.c
@@ -1,16 +1,41 @@
 #include <stdint.h>
+#include <assert.h>
 #include "param.h"
 #include "netport.h"
 #include "uma_stub.h"
 
 /* Global zone structures */
 int uma_zone_num = 0;
-struct uma_zone zones[16];
+struct uma_zone zones[16] = {0};
+
+/* Ref counts for shared data, save a ref-count per page.
+ * a hack until we have a slab allocator */
+u_int32_t uma_refcounts[1048576] = {0};
 
 
 void * uma_zalloc_arg(uma_zone_t zone, void *udata, int flags)
 {
-    return NULL;
+    void * ptr = malloc(zone->uz_size);
+
+    // Call init
+    if (zone->uz_init != NULL) {
+        if (zone->uz_init(ptr, zone->uz_size, flags) != 0) {
+            return (NULL);
+        }
+    }
+
+    // Call ctor
+    if (zone->uz_ctor != NULL) {
+        if (zone->uz_ctor(ptr, zone->uz_size, udata, flags) != 0) {
+            return (NULL);
+        }
+    }
+
+    if (flags & M_ZERO) {
+        bzero(ptr, zone->uz_size);
+    }
+
+    return (ptr);
 }
 
 void * uma_zalloc(uma_zone_t zone, int flags)
@@ -20,7 +45,11 @@ void * uma_zalloc(uma_zone_t zone, int flags)
 
 void uma_zfree_arg(uma_zone_t zone, void *item, void *udata)
 {
+    if (zone->uz_dtor) {
+        zone->uz_dtor(item, zone->uz_size, udata);
+    }
 
+    free(item);
 }
 
 void uma_zfree(uma_zone_t zone, void *item)
@@ -30,7 +59,7 @@ void uma_zfree(uma_zone_t zone, void *item)
 
 void zone_drain_wait(uma_zone_t zone, int waitok)
 {
-
+    /* Do nothing here */
 }
 
 void zone_drain(uma_zone_t zone)
@@ -40,7 +69,7 @@ void zone_drain(uma_zone_t zone)
 
 int uma_zone_set_max(uma_zone_t zone, int nitems)
 {
-    return 1;
+    return (nitems);
 }
 
 uma_zone_t uma_zcreate(const char *name, size_t size, uma_ctor ctor,
@@ -85,7 +114,7 @@ uma_zone_t uma_zsecond_create(char *name, uma_ctor ctor, uma_dtor dtor,
 
 void uma_zone_set_allocf(uma_zone_t zone, uma_alloc allocf)
 {
-
+    /* Do nothing */
 }
 
 int uma_zone_exhausted(uma_zone_t zone)
@@ -100,5 +129,14 @@ int uma_zone_exhausted_nolock(uma_zone_t zone)
 
 u_int32_t *uma_find_refcnt(uma_zone_t zone, void *item)
 {
-    return NULL;
+    /* OSv: Same ext data (Cluster) can be used by various mbuf structs, so
+     * use a refcnt on the data, the ctor of Cluster set this refcnt to 1
+     */
+
+    uint64_t addr = (uint64_t)item;
+    assert(addr < (1L<<32));
+
+    unsigned slot = (uint64_t)item >> 12;
+    /* Pray ref-count is smaller than 255 :) */
+    return ((u_int32_t *)&uma_refcounts[slot]);
 }
