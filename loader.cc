c83a16ceac0bf044676b722b73085c27c88b5284
Dor Laor <dor@cloudius-systems.com>
2013-02-01 23:39:46 +0200

    Register another virtio-blk device which isn't the ram disk. This isn't the right discovery mechnism and this should be simplified later on

--- loader.cc
+++ loader.cc
@@ -288,6 +288,7 @@ void* do_main_thread(void *_args)
     // Initialize all drivers
     hw::driver_manager* drvman = hw::driver_manager::instance();
     drvman->register_driver(new virtio::virtio_blk(0));
+    drvman->register_driver(new virtio::virtio_blk(1));
     drvman->register_driver(new virtio::virtio_net());
     drvman->load_all();
     drvman->list_drivers();
