8bfa1cd55fb7ebbcc6cf7a041a550cdad81d6968
Dor Laor <dor@cloudius-systems.com>
2013-02-07 17:30:34 +0200

    Move the thread test code into its own file to clear loader.cc

--- loader.cc
+++ loader.cc
@@ -83,54 +83,8 @@ static int test_ctr;
 using sched::thread;
 // since it contains actual code it needs to be after the 'using' declaration
 #include "tests/tst-malloc.hh"
+#include "tests/tst-threads.hh"
 
-
-struct test_threads_data {
-    thread* main;
-    thread* t1;
-    bool t1ok;
-    thread* t2;
-    bool t2ok;
-};
-
-void test_thread_1(test_threads_data& tt)
-{
-    while (test_ctr < 1000) {
-        thread::wait_until([&] { return (test_ctr % 2) == 0; });
-        ++test_ctr;
-        if (tt.t2ok) {
-            tt.t2->wake();
-        }
-    }
-    tt.t1ok = false;
-    tt.main->wake();
-}
-
-void test_thread_2(test_threads_data& tt)
-{
-    while (test_ctr < 1000) {
-        thread::wait_until([&] { return (test_ctr % 2) == 1; });
-        ++test_ctr;
-        if (tt.t1ok) {
-            tt.t1->wake();
-        }
-    }
-    tt.t2ok = false;
-    tt.main->wake();
-}
-
-void test_threads()
-{
-    test_threads_data tt;
-    tt.main = thread::current();
-    tt.t1ok = tt.t2ok = true;
-    tt.t1 = new thread([&] { test_thread_1(tt); });
-    tt.t2 = new thread([&] { test_thread_2(tt); });
-    thread::wait_until([&] { return test_ctr >= 1000; });
-    tt.t1->join();
-    tt.t2->join();
-    debug("threading test succeeded");
-}
 
 elf::program* prog;
 
