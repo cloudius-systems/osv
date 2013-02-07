127c5e9b78e05b4b336125da802e66bb2aa4b5aa
Christoph Hellwig <hch@cloudius-systems.com>
2013-02-07 14:41:40 +0100

    remove getenv debugging

--- libc/env/getenv.c
+++ libc/env/getenv.c
@@ -8,8 +8,6 @@ char *getenv(const char *name)
 	int i;
 	size_t l = strlen(name);
 
-	printf("getenv called for %s\n", name);
-
 	if (!__environ || !*name || strchr(name, '=')) return NULL;
 	for (i=0; __environ[i] && (strncmp(name, __environ[i], l)
 		|| __environ[i][l] != '='); i++);
