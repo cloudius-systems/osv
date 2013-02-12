d9a6cdd05cbd50d5d4d58b31fa37747464254f23
Guy Zana <guy@cloudius-systems.com>
2013-02-12 19:42:01 +0200

    Test rwlock from 2 threads, yield before releasing the rw_lock

--- tests/tst-rwlock.hh
+++ tests/tst-rwlock.hh
@@ -11,6 +11,14 @@ extern "C" {
     #include <bsd/porting/rwlock.h>
 }
 
+//
+// Test 1 - test lock recursively on the same thread
+// Test 2 - test sleep of rwlock from 2 threads, read/write equivalence
+//          The key is in the call to thread::current()->yield() before
+//          releasing the lock
+//
+
+
 class test_rwlock : public unit_tests::vtest {
 public:
 
@@ -18,7 +26,40 @@ public:
     thread* _main;
     bool _test1_finished;
 
-    // test a basic flow withing a single thread...
+    rwlock _test2_rwlock;
+    bool _test2_t1_finished;
+    bool _test2_t2_finished;
+
+    void rwlock_test2_t1(void)
+    {
+        for (int i=0; i<10; i++) {
+            debug("T1 Locking...");
+            rw_rlock(&_test2_rwlock);
+            debug("T1 Locked");
+            debug("  rwlock_test2_t1");
+            thread::current()->yield();
+            debug("T1 Unlocked");
+            rw_runlock(&_test2_rwlock);
+        }
+        _test2_t1_finished = true;
+        _main->wake();
+    }
+
+    void rwlock_test2_t2(void)
+    {
+        for (int i=0; i<5; i++) {
+            debug("T2 Locking...");
+            rw_wlock(&_test2_rwlock);
+            debug("T2 Locked");
+            debug("  rwlock_test2_t2");
+            debug("T2 Unlocked");
+            rw_wunlock(&_test2_rwlock);
+            thread::current()->yield();
+        }
+        _test2_t2_finished = true;
+        _main->wake();
+    }
+
     void rwlock_test1(void)
     {
         debug("rwlock_test1... ", false);
@@ -33,10 +74,15 @@ public:
         rw_wlock(&lock);
         rw_wunlock(&lock);
 
+        rw_rlock(&lock);
+        rw_runlock(&lock);
+
         rw_wlock(&lock);
         rw_wunlock(&lock);
 
         rw_runlock(&lock);
+
+        rw_destroy(&lock);
 
         debug("OK");
 
@@ -50,14 +96,22 @@ public:
 
         // Init
         _main = thread::current();
+
+        // Test 1
         _test1_finished = false;
-
-        // Run test1 in a thread
         thread* t1 = new thread([&] { rwlock_test1(); });
-
-        // Join...
         _main->wait_until([&] { return (_test1_finished); });
         delete t1;
+
+        // Test 2
+        _test2_t1_finished = false;
+        _test2_t2_finished = false;
+        rw_init(&_test2_rwlock, "tst2");
+        thread* t2_1 = new thread([&] { rwlock_test2_t1(); });
+        thread* t2_2 = new thread([&] { rwlock_test2_t2(); });
+        _main->wait_until([&] { return (_test2_t1_finished && _test2_t2_finished); });
+        delete t2_1;
+        delete t2_2;
     }
 };
 
