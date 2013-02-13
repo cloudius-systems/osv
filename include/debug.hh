8eb501daca71b0bcf31b24ba2b76afcb955f6678
Guy Zana <guy@cloudius-systems.com>
2013-02-13 11:35:40 +0200

    Added a logger that filters log messages using tags and severity (debug,info,warn,error)

--- include/debug.hh
+++ include/debug.hh
@@ -2,23 +2,58 @@
 #define DEBUG_H
 
 #include <iostream>
+#include <map>
+#include <string>
 #include "boost/format.hpp"
 
 typedef boost::format fmt;
 
 class IsaSerialConsole;
 
-class Debug {
+class logger {
 public:
-    //friend std::ostream& operator<<(std::ostream&, const aa&);
-    static Debug* Instance() {return (pinstance)? pinstance: (pinstance = new Debug);};
+
+   enum logger_severity {
+        logger_debug = 0,
+        logger_info = 1,
+        logger_warn = 2,
+        logger_error = 3,
+        // Suppress output, even errors
+        logger_none = 4
+    };
+
+    logger();
+    virtual ~logger();
+
+    static logger* instance() {
+        if (_instance == nullptr) {
+            _instance = new logger();
+        }
+        return (_instance);
+    }
+
+    //
+    // Interface for logging, these functions checks the filters and
+    // calls the underlying debug functions.
+    //
+    void log(const char* tag, logger_severity severity, const boost::format& _fmt);
+    void log(const char* tag, logger_severity severity, const char* _fmt, ...);
 
 private:
-   Debug() {Debug::pinstance = 0;};
-   Debug(const Debug& d) {};
-   Debug& operator=(const Debug& d) {pinstance = d.pinstance; return *pinstance;};
+   static logger* _instance;
 
-   static Debug* pinstance;
+   bool parse_configuration(void);
+
+   bool is_filtered(const char *tag, logger_severity severity);
+
+   // Adds a tag, if it exists then modify severity
+   void add_tag(const char* tag, logger_severity severity);
+
+   // Returns severity, per tag, if doesn't exist return error as default
+   logger_severity get_tag(const char* tag);
+
+   std::map<std::string, logger_severity> _log_level;
+
 };
 
 extern "C" {void debug(const char *msg); }
