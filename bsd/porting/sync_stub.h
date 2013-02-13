b7cd119bccc8c20e15fee218fbda436afcaedf4f
Guy Zana <guy@cloudius-systems.com>
2013-02-13 16:19:49 +0200

    Add a trivial implementation for mtx_* functions

--- bsd/porting/sync_stub.h
+++ bsd/porting/sync_stub.h
@@ -1,14 +1,18 @@
 #ifndef SYNC_STUB_H
 #define SYNC_STUB_H
 
+#include <osv/mutex.h>
+
 struct mtx {
-    int __unused;
+    mutex_t _mutex;
 };
 
 #define MTX_DEF     0x00000000  /* DEFAULT (sleep) lock */
 
-#define MA_OWNED    9
-
+#define MA_OWNED        (0x01)
+#define MA_NOTOWNED     (0x02)
+#define MA_RECURSED     (0x04)
+#define MA_NOTRECURSED  (0x08)
 
 void mtx_init(struct mtx *m, const char *name, const char *type, int opts);
 
@@ -20,7 +24,9 @@ void mtx_unlock(struct mtx *mp);
 
 void mtx_assert(struct mtx *mp, int flag);
 
-#define mtx_sleep(chan, mtx, pri, wmesg, timo) (void)0;
+// #define mtx_sleep(chan, mtx, pri, wmesg, timo) (void)0;
+
+
 
 void critical_enter(void);
 
