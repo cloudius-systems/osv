76c5866308342a2e772cbeb6a2a3e8db218a6165
Guy Zana <guy@cloudius-systems.com>
2013-02-19 18:10:50 +0200

    Call netisr_init()

--- bsd/net.cc
+++ bsd/net.cc
@@ -3,6 +3,7 @@
 extern "C" {
     #include <bsd/sys/sys/eventhandler.h>
     #include <bsd/sys/sys/mbuf.h>
+    #include <bsd/sys/net/netisr.h>
     #include <bsd/machine/param.h>
 }
 
@@ -11,5 +12,6 @@ void net_init(void)
     debug("Initializing network stack...");
     eventhandler_init(NULL);
     mbuf_init(NULL);
+    netisr_init(NULL);
     debug("Done!");
 }
