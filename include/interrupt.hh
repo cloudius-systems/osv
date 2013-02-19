6ad3016cc4475033422873e5d13fd932301b0ab7
Avi Kivity <avi@cloudius-systems.com>
2013-02-19 18:36:31 +0200

    interrupt: drop 'using namespace' from header
    The whole idea of namespaces is to prevent namespace pollution, using a
    "using namespace" in a header negates that.  Remove.

--- include/interrupt.hh
+++ include/interrupt.hh
@@ -10,17 +10,15 @@
 #include "drivers/pci-function.hh"
 #include <osv/types.h>
 
-using namespace pci;
-
 // max vectors per request
 const int max_vectors = 256;
 
 class msix_vector {
 public:
-    msix_vector(pci_function* dev);
+    msix_vector(pci::pci_function* dev);
     virtual ~msix_vector();
 
-    pci_function* get_pci_function(void);
+    pci::pci_function* get_pci_function(void);
     unsigned get_vector(void);
     void msix_unmask_entries(void);
     void msix_mask_entries(void);
@@ -33,7 +31,7 @@ private:
     // Handler to invoke...
     std::function<void ()> _handler;
     // The device that owns this vector
-    pci_function * _dev;
+    pci::pci_function * _dev;
     // Entry ids used by this vector
     std::list<unsigned> _entryids;
     unsigned _vector;
@@ -77,14 +75,14 @@ public:
     // 2. Allocate vectors and assign ISRs
     // 3. Setup entries
     // 4. Unmask interrupts
-    bool easy_register(pci_function* dev, msix_isr_list& isrs);
-    void easy_unregister(pci_function* dev);
+    bool easy_register(pci::pci_function* dev, msix_isr_list& isrs);
+    void easy_unregister(pci::pci_function* dev);
 
     /////////////////////
     // Multi Interface //
     /////////////////////
 
-    assigned_vectors request_vectors(pci_function* dev, unsigned num_vectors);
+    assigned_vectors request_vectors(pci::pci_function* dev, unsigned num_vectors);
     void free_vectors(const assigned_vectors& vectors);
     bool assign_isr(unsigned vector, std::function<void ()> handler);
     // Multiple entry can be assigned the same vector
@@ -96,7 +94,7 @@ private:
     static interrupt_manager* _instance;
     msix_vector* _vectors[max_vectors];
     // Used by the easy interface
-    std::map<pci_function *, assigned_vectors> _easy_dev2vectors;
+    std::map<pci::pci_function *, assigned_vectors> _easy_dev2vectors;
 };
 
 #endif /* INTERRUPT_HH_ */
