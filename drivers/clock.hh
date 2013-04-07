03cd7a7f86a846c882412dee83bada9899e2ee9e
Avi Kivity <avi@cloudius-systems.com>
2013-04-07 12:01:32 +0300

    clock: add suffixes for common time units

--- drivers/clock.hh
+++ drivers/clock.hh
@@ -13,5 +13,24 @@ private:
     static clock* _c;
 };
 
+inline constexpr unsigned long long operator"" _ns(unsigned long long t)
+{
+    return t;
+}
+
+inline constexpr unsigned long long operator"" _us(unsigned long long t)
+{
+    return t * 1000_ns;
+}
+
+inline constexpr unsigned long long operator"" _ms(unsigned long long t)
+{
+    return t * 1000_us;
+}
+
+inline constexpr unsigned long long operator"" _s(unsigned long long t)
+{
+    return t * 1000_ms;
+}
 
 #endif /* CLOCK_HH_ */
