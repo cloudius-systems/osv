71111a60390083c44c53318398363c65a6919f2d
Avi Kivity <avi@cloudius-systems.com>
2013-04-08 16:47:51 +0300

    trace: add test for tracepoint signature

--- tests/tst-tracepoint.cc
+++ tests/tst-tracepoint.cc
@@ -1,4 +1,5 @@
 #include <osv/trace.hh>
+#include "debug.hh"
 
 struct test_object {
     int i = 1;
@@ -15,10 +16,22 @@ tracepointv<storage_args<int, long>, runtime_args<test_object&>, test_object::un
     trace_2("tp2", "%d %d");
 
 
+std::string signature_string(u64 s)
+{
+    std::string ret;
+    while (s) {
+        ret.push_back(s & 255);
+        s >>= 8;
+    }
+    return ret;
+}
+
 int main(int ac, char** av)
 {
     test_object obj;
     trace_1(10, 20);
+    debug(boost::format("trace_1 signature: %s") % signature_string(trace_1.signature()));
+    assert(signature_string(trace_1.signature()) == "Iq");
     trace_2(obj);
 }
 
