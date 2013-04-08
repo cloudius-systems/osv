a5dc3aa5241e873f2feee1d6d99683201ea5f338
Avi Kivity <avi@cloudius-systems.com>
2013-04-08 12:49:30 +0300

    trace: add static tracepoint definitions
    Two styles are supported, a simple one and a flexible one.
    
    To declare and use a simple tracepoint, use:
    
      tracepoint<u32, u64, char> trace_my_event("my_event", "%d %x '%s'");
    
      ...
    
      void my_function(u32 a0, u64 a1)
      {
          trace_my_event(a0, a1, 'x');
      }
    
    where:
    
      u32/u64/char: the types of the parameters to be used in the tracepoint
      trace_my_event: an identifier (commonly prefixed by trace_)
      "my_event": how the tracepoint is shown in the logs
      "%d %x '%s'": printf/boost::format style format string (auto deduces
                    argument types)
      a0, a1, 'x': runtime arguments to be traced
    
    The flexible style allows different type lists used in the tracepoint
    function, and in storing.  This allows having the tracepoint snapshot fields
    from some object.  You will need to supply a function to convert from the
    runtime types to the storage types:
    
      std::tuple<u32, u64, char> snapshot_my_object(my_object& obj)
      {
        return std::make_tuple(obj.field1, obj.field2, obj.field3);
      }
    
      tracepointv<storage_args<u32, u64, char>,
                  runtime_args<my_object&>,
                  snapshot_my_object> trace_my_object("my_object", "%d %x '%s'");
    
      void my_function(my_object& obj)
      {
        trace_my_object(obj);
      }
    
    Using this style reduces the runtime overhead since 'snapshot_my_object' is
    only run if tracing is enabled.

--- include/osv/trace.hh
+++ include/osv/trace.hh
@@ -1,6 +1,99 @@
 #ifndef TRACE_HH_
 #define TRACE_HH_
 
+#include <iostream>
+#include <tuple>
+#include <boost/format.hpp>
+
 void enable_trace();
+
+template <class storage_args, class runtime_args>
+class assigner_type;
+
+template <class storage_args, class runtime_args,
+          typename assigner_type<storage_args, runtime_args>::type assign>
+class tracepointv;
+
+template <typename... args>
+struct storage_args
+{
+};
+
+template <typename... args>
+struct runtime_args
+{
+};
+
+template <typename... s_args, typename... r_args>
+struct assigner_type<storage_args<s_args...>, runtime_args<r_args...>> {
+    typedef std::tuple<s_args...> (*type)(r_args...);
+};
+
+template <size_t idx, size_t N, typename... args>
+struct tuple_formatter
+{
+    static boost::format& format(boost::format& fmt, std::tuple<args...> as) {
+        typedef tuple_formatter<idx + 1, N, args...> recurse;
+        return recurse::format(fmt % std::get<idx>(as), as);
+    }
+};
+
+template <size_t N, typename... args>
+struct tuple_formatter<N, N, args...>
+{
+    static boost::format& format(boost::format& fmt, std::tuple<args...> as) {
+        return fmt;
+    }
+};
+
+template <typename... args>
+inline
+boost::format& format_tuple(boost::format& fmt, std::tuple<args...> as)
+{
+    return tuple_formatter<size_t(0), sizeof...(args), args...>::format(fmt, as);
+}
+
+template <typename... args>
+inline
+boost::format format_tuple(const char* fmt, std::tuple<args...> as)
+{
+    boost::format format(fmt);
+    return format_tuple(format, as);
+}
+
+template <typename... s_args,
+          typename... r_args,
+          typename assigner_type<storage_args<s_args...>, runtime_args<r_args...>>::type assign>
+class tracepointv<storage_args<s_args...>, runtime_args<r_args...>, assign>
+{
+public:
+    explicit tracepointv(const char* _name,
+                 const char* _format)
+        : name(_name), format(_format) {}
+    void operator()(r_args... as) {
+        trace_slow_path(assign(as...));
+    }
+    void trace_slow_path(std::tuple<s_args...> as) __attribute__((cold)) {
+        if (enabled) {
+            std::cout << name << " " << format_tuple(format, as) << "\n";
+        }
+    }
+    const char* name;
+    const char* format;
+    bool enabled = true;
+};
+
+template <typename... args>
+inline
+std::tuple<args...> identity_assign(args... as)
+{
+    return std::make_tuple(as...);
+}
+
+template <typename... args>
+using tracepoint = tracepointv<storage_args<args...>,
+                               runtime_args<args...>,
+                               identity_assign<args...>>;
+
 
 #endif /* TRACE_HH_ */
