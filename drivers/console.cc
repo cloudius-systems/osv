ee9c42b74075404f4f02ac6dda9279a0504606f0
Avi Kivity <avi@cloudius-systems.com>
2013-04-14 16:53:56 +0300

    console: connect stdin to the console
    We use a polling thread to collect characters and place them in a queue.

--- drivers/console.cc
+++ drivers/console.cc
@@ -1,9 +1,13 @@
 
 #include <osv/prex.h>
 #include <osv/device.h>
+#include <sched.hh>
+#include <queue>
+#include <vector>
 
 #include "isa-serial.hh"
 #include "debug-console.hh"
+#include "drivers/clock.hh"
 
 namespace console {
 
@@ -28,6 +32,52 @@ void read(char *msg, size_t len)
     }
 }
 
+mutex console_mutex;
+std::queue<char> console_queue;
+std::list<sched::thread*> readers;
+
+// hack, until we have ISA interrupts
+void console_poll()
+{
+    sched::timer tmr(*sched::thread::current());
+    while (true) {
+        while (!with_lock(console_mutex, [] { return console.input_ready(); })) {
+            tmr.set(clock::get()->time() + 10_ms);
+            sched::thread::wait_until([&] { return tmr.expired(); });
+        }
+        with_lock(console_mutex, [] {
+            console_queue.push(console.readch());
+            for (auto t : readers) {
+                t->wake();
+            }
+        });
+    }
+}
+
+static int
+console_read(struct device *dev, struct uio *uio, int ioflag)
+{
+    return with_lock(console_mutex, [=] {
+        readers.push_back(sched::thread::current());
+        sched::thread::wait_until(console_mutex, [] { return !console_queue.empty(); });
+        readers.remove(sched::thread::current());
+        while (uio->uio_resid && !console_queue.empty()) {
+            struct iovec *iov = uio->uio_iov;
+            auto n = std::min(console_queue.size(), iov->iov_len);
+            for (size_t i = 0; i < n; ++i) {
+                static_cast<char*>(iov->iov_base)[i] = console_queue.front();
+                console_queue.pop();
+            }
+
+            uio->uio_resid -= n;
+            uio->uio_offset += n;
+            if (n == iov->iov_len) {
+                uio->uio_iov++;
+                uio->uio_iovcnt--;
+            }
+        }
+        return 0;
+    });
 }
 
 static int
@@ -36,9 +86,11 @@ console_write(struct device *dev, struct uio *uio, int ioflag)
     while (uio->uio_resid > 0) {
         struct iovec *iov = uio->uio_iov;
 
-	if (iov->iov_len) {
-            console::write(reinterpret_cast<const char *>(iov->iov_base),
-                           iov->iov_len, false);
+        if (iov->iov_len) {
+            with_lock(console_mutex, [=] {
+                console::write(reinterpret_cast<const char *>(iov->iov_base),
+                               iov->iov_len, false);
+            });
         }
 
         uio->uio_iov++;
@@ -61,10 +113,14 @@ console_ioctl(struct device *dev, u_long request, void *arg)
     }
 }
 
+}
+
+using namespace console;
+
 static struct devops console_devops = {
     .open	= no_open,
     .close	= no_close,
-    .read	= no_read,
+    .read	= console_read,
     .write	= console_write,
     .ioctl	= console_ioctl,
     .devctl	= no_devctl,
@@ -79,6 +135,7 @@ struct driver console_driver = {
 extern "C" int
 console_init(void)
 {
+    (new sched::thread(console_poll))->start();
     device_create(&console_driver, "console", D_CHR);
     return 0;
 }
