186ad05bdbf2ac49a04f9a348acf901654f4ab4c
Guy Zana <guy@cloudius-systems.com>
2013-04-17 14:21:57 +0300

    bsd: add implementation FIXME for sx_slock() empty stub

--- bsd/porting/netport.h
+++ bsd/porting/netport.h
@@ -87,11 +87,13 @@ extern int tick;
 #define ENOIOCTL    (-3)        /* ioctl not handled by this layer */
 #define EDIRIOCTL   (-4)        /* do direct ioctl in GEOM */
 
+/* FIXME: TODO - Implement... */
 #define sx_slock(...) do{}while(0)
 #define sx_sunlock(...) do{}while(0)
 #define sx_xlock(...) do{}while(0)
 #define sx_xunlock(...) do{}while(0)
 
+/* FIXME: TODO - Implement... */
 #ifndef time_uptime
 #define time_uptime (1)
 #endif
