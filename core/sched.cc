cloudius-systems/osv

commit 31572c85935df331544e53e9e2db9c5f88d82c6c

sched: fix handle_incoming_wakeups() called from preemptible context
handle_incoming_wakeups() may not be called from preemptible context,
since it manipulates per-cpu variables.

Fix by eliminating these calls.  In one call site, the call is removed,
since it will be called immediately afterwards with interrupts disabled.
In the other call site, push the call into an existing irq disabled region.

Fixes livelocks where a thread is placed into an incoming_wakeups queue
but has the wrong bit set in incoming_wakeups_mask.

core/sched.cc
@@ -50,8 +50,6 @@ cpu::cpu()
 
 void cpu::schedule(bool yield)
 {
-    // FIXME: drive by IPI
-    handle_incoming_wakeups();
 
     with_lock(irq_lock, [this] {
         reschedule_from_interrupt();
@@ -223,17 +221,21 @@ void thread::yield()
 {
     auto t = current();
     // FIXME: drive by IPI
-    t->_cpu->handle_incoming_wakeups();
-    // FIXME: what about other cpus?
-    if (t->_cpu->runqueue.empty()) {
-        return;
-    }
-    with_lock(irq_lock, [t] {
+    bool resched = with_lock(irq_lock, [t] {
+        t->_cpu->handle_incoming_wakeups();
+        preempt_enable();
+        // FIXME: what about other cpus?
+        if (t->_cpu->runqueue.empty()) {
+            return false;
+        }
         t->_cpu->runqueue.push_back(*t);
         assert(t->_status.load() == status::running);
         t->_status.store(status::queued);
+        return true;
     });
-    t->_cpu->schedule(true);
+    if (resched) {
+        t->_cpu->schedule(true);
+    }
 }
 
 thread::stack_info::stack_info()

commit 573684880396a320597c79c145aa5d2304a0157d

sched: allow wake() to reschedule
We want to reschedule if we woke a local thread, since it may have higher
priority than us.  However commit 31572c8593 broke this by changing the
preemption counter.

Fix by adjusting the check to take this into account.

Noted by Nadav.

core/sched.cc
@@ -323,7 +323,7 @@ void thread::wake()
     // FIXME: warrant an interruption
     if (_cpu != current()->tcpu()) {
         wakeup_ipi.send(_cpu);
-    } else if (arch::irq_enabled() && !preempt_counter) {
+    } else if (arch::irq_enabled() && preempt_counter == 1) { // ignore the preempt_disable() above
         _cpu->schedule();
         // We'll also reschedule at the end of an interrupt if needed
     }