df763e80218d97929178f02c33b593938ce06747
Guy Zana <guy@cloudius-systems.com>
2013-04-30 17:34:30 +0300

    java: add RhinoCLI.java
    instead of running rhino.jar -f <file>, we now embed rhino within our own class
    and completely control the context of the script engine.
    
    reuse Global() from org.mozilla.javascript.tools.shell to initialize
    our Context, this is done so we'll have functions such as importPackage()
    available to Javascript.
    
    this patch still doesn't replace rhino shell.

---
+++ java/src/com/cloudius/main/RhinoCLI.java
@@ -0,0 +1,34 @@
+package com.cloudius.main;
+
+import java.io.*;
+
+import sun.org.mozilla.javascript.*;
+import sun.org.mozilla.javascript.tools.shell.*;
+
+public class RhinoCLI {
+    
+    public static Global global = new Global();
+
+    //
+    // Invoke the cli.js file take care of exposing all scriptable objects
+    // such as the tests
+    //
+    public static void main(String[] args) {
+        Context cx=Context.enter();
+        try {
+            
+            global.init(cx);
+            Scriptable scope = ScriptableObject.getTopLevelScope(global);
+            
+            FileReader cli_js = new FileReader("/console/cli.js");
+            cx.evaluateReader(scope, cli_js, "cli.js", 1, null);
+            
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        } finally {
+            Context.exit();
+        }
+    }
+
+
+}
