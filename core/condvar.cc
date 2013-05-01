31f5f920b8bc29eb19a4d3ee5dd050064efe9521
Nadav Har'El <nyh@cloudius-systems.com>
2013-05-01 12:15:07 +0300

    New condition variable implementation
    Added a new condition variable implementation. The header file is
    <osv/condvar.h>, and it is usable both from C code and C++ code -
    in C++ code you will get some additional conveniences (like using
    methods instead of global functions).
    
    The condition variable type is "condvar_t" or "struct condvar"
    from C, or just "condvar" from C++.
    
    Besides working in C, this implementation has two additional
    benefits over the previous one (still in pthread.cc, will be
    removed in a followup patch):
    
    1. The condvar instance can be zero-initialized, and doesn't
       need special initialization or finalization functions.
    2. convar does not allocate any memory - the wait records
       that form a wait queue are kept on each waiting thread's
       stack.

---
+++ core/condvar.cc
@@ -0,0 +1,96 @@
+#include <osv/condvar.h>
+#include <sched.h>
+#include <errno.h>
+
+struct ccondvar_waiter {
+    struct ccondvar_waiter *newer;
+    sched::thread *t;
+};
+
+int condvar_wait(condvar_t *condvar, mutex_t* user_mutex, uint64_t expiration)
+{
+    if (expiration) {
+        sched::timer timer(*sched::thread::current());
+        timer.set(expiration);
+        return condvar_wait(condvar, user_mutex, &timer);
+    } else {
+        return condvar_wait(condvar, user_mutex, nullptr);
+    }
+}
+
+int condvar_wait(condvar_t *condvar, mutex_t* user_mutex, sched::timer* tmr)
+{
+    int ret = 0;
+    struct ccondvar_waiter wr = { nullptr, sched::thread::current() };
+
+    mutex_lock(&condvar->m);
+    if (!condvar->waiters_fifo.oldest) {
+        condvar->waiters_fifo.oldest = &wr;
+    } else {
+        condvar->waiters_fifo.newest->newer = &wr;
+    }
+    condvar->waiters_fifo.newest = &wr;
+    mutex_unlock(&condvar->m);
+
+    mutex_unlock(user_mutex);
+    // Wait until either the timer expires or condition variable signaled
+    sched::thread::wait_until([&] {
+        return (tmr && tmr->expired()) || !wr.t;
+    });
+    if (wr.t) {
+        // wr is still in the linked list (because of a timeout) so remove it:
+        mutex_lock(&condvar->m);
+        if (&wr == condvar->waiters_fifo.oldest) {
+            condvar->waiters_fifo.oldest = wr.newer;
+            if (!wr.newer) {
+                condvar->waiters_fifo.newest = nullptr;
+            }
+        } else {
+            struct ccondvar_waiter *p = condvar->waiters_fifo.oldest;
+            while (p) {
+                if (&wr == p->newer) {
+                    p->newer = p->newer->newer;
+                    if(!p->newer) {
+                        condvar->waiters_fifo.newest = p;
+                    }
+                    break;
+                }
+                p = p->newer;
+            }
+        }
+        mutex_unlock(&condvar->m);
+        ret = ETIMEDOUT;
+    }
+    mutex_lock(user_mutex);
+    return ret;
+}
+
+void condvar_wake_one(condvar_t *condvar)
+{
+    mutex_lock(&condvar->m);
+    struct ccondvar_waiter *wr = condvar->waiters_fifo.oldest;
+    if (wr) {
+        condvar->waiters_fifo.oldest = wr->newer;
+        if (wr->newer == nullptr) {
+            condvar->waiters_fifo.newest = nullptr;
+        }
+        sched::thread *t = wr->t;
+        wr->t = nullptr;
+        t->wake();
+    }
+    mutex_unlock(&condvar->m);
+}
+
+void condvar_wake_all(condvar_t *condvar)
+{
+    mutex_lock(&condvar->m);
+    struct ccondvar_waiter *wr = condvar->waiters_fifo.oldest;
+    while (wr) {
+        sched::thread *t = wr->t;
+        wr->t = nullptr;
+        t->wake();
+        wr = wr->newer;
+    }
+    condvar->waiters_fifo.oldest = condvar->waiters_fifo.newest = nullptr;
+    mutex_unlock(&condvar->m);
+}
