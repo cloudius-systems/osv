78a9b1d26f61c7d5272da6efe95a15bd7436f54f
Nadav Har'El <nyh@cloudius-systems.com>
2013-05-06 12:35:18 +0300

    Fix bug in include/lockfree/queue-mpsc.hh
    Fixed bug in the lock-free queue's push() implementation, which
    was reproduced in the already-committed test.
    
    The implementation was based on an incorrect understanding of the
    C++ compare_exchange operation. Normally, a CAS operation only tries to
    write to the variable we're trying to write too (in this case, the head
    of the pushlist). If the previous ("expected") value has already changed,
    nothing is done. But C++'s compare_exchange, to my complete surprise,
    when the write failed, copies the new (unexpected) value into the
    "expected" variable.
    
    So I had to fix the push() implementation to assume actual behavior of compare_exchange, not my make-believe one.
    
    This commit also includes various other code cleanups.

--- include/lockfree/queue-mpsc.hh
+++ include/lockfree/queue-mpsc.hh
@@ -1,27 +1,35 @@
-/* A lock-free multiple-producer single-consumer queue implementation.
- * "multiple-consumer" means that many push() may be in progress concurrently,
- * while "single-consumer" means we assume that only a single pop() attempt
- * may be in progress - potentially concurrently with push()s.
- *
- * Note that while there's just one concurrent consumer - it does not have
- * to always be the same thread or processor - it's just that the program
- * logic guarantees that in no case do to pop()s get called concurrently.
- */
+#ifndef INCLUDED_LOCKFREE_QUEUE_MPSC
+#define INCLUDED_LOCKFREE_QUEUE_MPSC
+// A lock-free multiple-producer single-consumer queue implementation.
+//
+// Multiple-Consumer means that many push()s may be in progress concurrently.
+// Single-Consumer means we assume that only a single pop() attempt may be in
+// progress - potentially concurrently with push()s.
+//
+// Note that while there's just one concurrent consumer - it does not have
+// to always be the same thread or processor - it's just that the program
+// logic guarantees that in no case do two pop()s get called concurrently.
+//
+// This is one of the simplest, and most well-known (and often
+// reinvented) lock-free algorithm, and we actually have another
+// implementation of the same algorithm - with a different API - in
+// include/osv/lockless-queue.hh. TODO: merge these two header files.
+// Boost also happens to use this algorithm as an example in their
+// documentation: see
+// http://www.boost.org/doc/libs/1_53_0/doc/html/atomic/usage_examples.html
+// #boost_atomic.usage_examples.mp_queue
 
 #include <atomic>
 
-using namespace std;
-
 namespace lockfree {
 
-// TODO: can we use boost::intrusive::list instead of our own?
 template <class T>
 class linked_item {
 public:
     T value;
     linked_item<T> *next;
-    linked_item<T>() : next(nullptr) { }
-    linked_item<T>(T val) : value(val), next(nullptr) { }
+    linked_item<T>() : value(), next(nullptr) { }
+    explicit linked_item<T>(T val) : value(val), next(nullptr) { }
  };
 
 template <class T>
@@ -29,13 +37,15 @@ class queue_mpsc {
 public:
     typedef linked_item<T> LT;
 private:
-    atomic<LT*> pushlist;
-    LT* poplist;
-    // currently, pop() when the queue is empty returns this "nothing".
-    // perhaps we should consider a different error mechanism.
-    T nothing;
+    std::atomic<LT*> pushlist;
+    // While poplist isn't used concurrently, we still want to allow it to
+    // be accessed from different threads, and when we read the pointer
+    // from poplist in one CPU we need the list it points to to also be
+    // visible (so-called "release/consume" memory ordering).
+    std::atomic<LT*> poplist;
 public:
-    queue_mpsc<T>(T nothing) : pushlist(nullptr), poplist(nullptr), nothing(nothing) { }
+    queue_mpsc<T>() : pushlist(nullptr), poplist(nullptr) { }
+
     inline void push(LT* item)
     {
         // We set up item to be the new head of the pushlist, pointing to the
@@ -43,50 +53,57 @@ public:
         // node, another concurrent push might have pushed its own items.
         // Therefore we can only replace the head of the pushlist with a CAS,
         // atomically checking that the head is still what we set in
-        // waiter->next, and changing the head.
-        // Note that while we have a loop here, it is lockfree - if one
+        // item->next, and changing the head.
+        // Note that while we have a loop here, it is lock-free - if one
         // competing pusher is paused, the other one can make progress.
+        LT *old = pushlist.load(std::memory_order_relaxed);
         do {
-            item->next = pushlist;
-        } while (!std::atomic_compare_exchange_weak(&pushlist, &item->next, item));
+            item->next = old;
+        } while (!pushlist.compare_exchange_weak(old, item, std::memory_order_release));
     }
 
-    inline T pop(void)
+    inline bool pop(T *result)
     {
-        if (poplist) {
+        LT *head = poplist.load(std::memory_order_consume);
+        if (head) {
             // The poplist (prepared by an earlier pop) is not empty, so pop
             // from it. We don't need any locking to access the poplist, as
             // it is only touched by pop operations, and our assumption (of a
-            // SCMP queue) is that pops cannot be concurrent.
-            LT *oldhead = poplist;
-            poplist = poplist->next;
-            return oldhead->value;
+            // single-consumer queue) is that pops cannot be concurrent.
+            *result = head->value;
+            poplist.store(head->next, std::memory_order_release);
+            return true;
         } else {
             // The poplist is empty. Atomically take the entire pushlist (pushers
             // may continue to to push concurrently, so the atomicity is imporant)
             // and then at our leasure, reverse this list (so oldest first) into
             // poplist. We can do this at our leasure because there are no competing
-            // pops.
-            LT *r = pushlist.exchange(nullptr);
+            // pops. Note we need memory_order_consume (because we only access memory
+            // via the pointer loaded from the atomic variable), but it appears gcc
+            // doesn't support memory_order_consume so we use memory_order_acquire.
+            LT *r = pushlist.exchange(nullptr, /*std::memory_order_consume*/std::memory_order_acquire);
+            if (!r)
+                return false; // the both poplist and poplist were empty
             // Reverse the previous pushlist (now in r) into poplist, and return
             // the last item (the oldest pushed item) as the result of the pop.
-            while(r){
-                if(!r->next)
-                    return r->value;
+            while (r->next) {
                 LT *next = r->next;
-                r->next = poplist;
-                poplist = r;
+                r->next = head;
+                head = r;
                 r = next;
             }
-            // if we're still here, the queue is empty. return "nothing"
-            return nothing;
+            *result = r->value;
+            poplist.store(head, std::memory_order_release);
+            return true;
         }
     }
 
-    inline bool isempty(void) const
+    inline bool empty(void) const
     {
-           return (!poplist && !pushlist.load());
+           return (!poplist.load(std::memory_order_relaxed) &&
+                   !pushlist.load(std::memory_order_relaxed));
     }
 };
 
 }
+#endif
