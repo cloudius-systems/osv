53fa57246733d8551f850d712993405e1d7095a1
Avi Kivity <avi@cloudius-systems.com>
2013-05-06 11:05:11 +0300

    boot: adjust boot loader for loaded payload size dynamically
    Since we're packing the entire file system into the boot image, it has
    overflowed the 128MB limit that was set for it.
    
    Adjust the boot loader during build time to account for the actual loaded size.
    
    Fixes wierd corruption during startup.

--- scripts/imgedit.py
+++ scripts/imgedit.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 
-import sys, os, optparse
+import sys, os, optparse, struct
 
 cmd = sys.argv[1]
 args = sys.argv[2:]
@@ -12,3 +12,12 @@ if cmd == 'setargs':
     with file(img, 'r+b') as f:
         f.seek(512)
         f.write(argstr)
+elif cmd == 'setsize':
+    img = args[0]
+    size = int(args[1])
+    block_size = 32 * 1024
+    blocks = (size + block_size - 1) / block_size
+    f = file(img, 'r+')
+    f.seek(512-4)
+    f.write(struct.pack('H', blocks))
+    f.close()
