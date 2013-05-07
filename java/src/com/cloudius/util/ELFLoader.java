8df4090272b53f7b0f554f691961a84a320131bb
Guy Zana <guy@cloudius-systems.com>
2013-05-07 11:17:17 +0300

    java: added ELFLoader class that invokes the jni wrapper for elf_runner()
    it translate the arguments recieved from javascript, invokes the jni method
    and return the exit code to javascript

---
+++ java/src/com/cloudius/util/ELFLoader.java
@@ -0,0 +1,51 @@
+package com.cloudius.util;
+
+import java.util.*;
+
+import sun.org.mozilla.javascript.NativeArray;
+import sun.org.mozilla.javascript.NativeGlobal;
+import sun.org.mozilla.javascript.ScriptableObject;
+import sun.org.mozilla.javascript.annotations.JSFunction;
+
+import com.cloudius.Config;
+import com.cloudius.main.RhinoCLI;
+
+public class ELFLoader extends ScriptableObject {
+
+    // Identifies the scriptable object
+    private static final long serialVersionUID = 87664098764510039L;
+
+    static {
+        Config.loadJNI("elf-loader.so");
+    }
+
+    public native static boolean run(String[] argv);
+
+    // The native function run() alters this member variable
+    public static int _exitcode;
+
+    @JSFunction
+    public boolean run()
+    {
+        NativeArray js_argv = (NativeArray)RhinoCLI._scope.get("_global_argv",
+                RhinoCLI._scope);
+        String[] argv = new String[(int)js_argv.getLength()];
+
+        for (int i=0; i < js_argv.getLength(); i++) {
+            argv[i] = (String)js_argv.get(i);
+        }
+
+        boolean success = run(argv);
+        if (success) {
+            RhinoCLI._scope.put("_exitcode", RhinoCLI._scope, _exitcode);
+        }
+
+        return (success);
+    }
+
+    @Override
+    public String getClassName() {
+        return "ELFLoader";
+    }
+
+}
