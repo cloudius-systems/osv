96dc275ac8e588e9594668d085817f25a1152f05
Guy Zana <guy@cloudius-systems.com>
2013-05-08 12:51:08 +0300

    java: make the context object publicly accessible in RhinoCLI

--- java/cli/com/cloudius/cli/main/RhinoCLI.java
+++ java/cli/com/cloudius/cli/main/RhinoCLI.java
@@ -13,22 +13,23 @@ public class RhinoCLI {
     public static Global global = new Global();
 
     public static Scriptable _scope;
+    public static Context _cx;
 
     //
     // Invoke the cli.js file take care of exposing all scriptable objects
     // such as the tests
     //
     public static void main(String[] args) {
-        Context cx=Context.enter();
+        _cx = Context.enter();
         try {
             
-            global.init(cx);
+            global.init(_cx);
             _scope = ScriptableObject.getTopLevelScope(global);
             ScriptableObject.defineClass(_scope, TCPEchoServerTest.class);
             ScriptableObject.defineClass(_scope, ELFLoader.class);
             
             FileReader cli_js = new FileReader("/console/cli.js");
-            cx.evaluateReader(_scope, cli_js, "cli.js", 1, null);
+            _cx.evaluateReader(_scope, cli_js, "cli.js", 1, null);
             
         } catch (Exception ex) {
             ex.printStackTrace();
