6db6cc591c7282e95014136dca86481e68cc4382
Avi Kivity <avi@cloudius-systems.com>
2013-05-13 12:50:31 +0300

    libc: make locale implementation match glibc's

--- libc/internal/locale_impl.h
+++ libc/internal/locale_impl.h
@@ -1,5 +1,18 @@
+#ifndef LOCALE_IMPL_H
+#define LOCALE_IMPL_H
+
 #include <locale.h>
 
-struct __locale {
-	int dummy;
+struct __locale_struct {
+    struct __locale_data *__locales[13];
+    const unsigned short int *__ctype_b;
+    const int* __ctype_tolower;
+    const int* __ctype_toupper;
+    const char *__names[13];
 };
+
+typedef struct __locale_struct *__locale_t;
+
+typedef __locale_t locale_t;
+
+#endif
