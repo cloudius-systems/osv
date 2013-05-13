6e4647bc60398423091fe349e621d7cbb1e1749c
Avi Kivity <avi@cloudius-systems.com>
2013-05-13 12:36:57 +0300

    include: fix warnings in endian.h
    gcc wants parentheses, let it have them.

--- include/api/endian.h
+++ include/api/endian.h
@@ -24,17 +24,17 @@
 
 static __inline uint16_t __bswap16(uint16_t __x)
 {
-	return __x<<8 | __x>>8;
+	return (__x<<8) | (__x>>8);
 }
 
 static __inline uint32_t __bswap32(uint32_t __x)
 {
-	return __x>>24 | __x>>8&0xff00 | __x<<8&0xff0000 | __x<<24;
+	return (__x>>24) | (__x>>8&0xff00) | (__x<<8&0xff0000) | (__x<<24);
 }
 
 static __inline uint64_t __bswap64(uint64_t __x)
 {
-	return __bswap32(__x)+0ULL<<32 | __bswap32(__x>>32);
+	return ((__bswap32(__x)+0ULL)<<32) | __bswap32(__x>>32);
 }
 
 #if __BYTE_ORDER == __LITTLE_ENDIAN
