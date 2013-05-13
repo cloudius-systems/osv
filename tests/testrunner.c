395eef6409b969c4bb6aa5f1e760a8a5abdd507a
Avi Kivity <avi@cloudius-systems.com>
2013-05-13 12:32:58 +0300

    tests: add missing includes
    musl include dependencies are different from glibcs, add missing includes
    to make up the difference.

--- tests/testrunner.c
+++ tests/testrunner.c
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <limits.h>
 
 extern void dlclose_by_path_np(const char* filename);
 
--- tests/tst-fat.c
+++ tests/tst-fat.c
@@ -8,6 +8,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
+#include <limits.h>
 
 #define BUF_SIZE	4096
 
