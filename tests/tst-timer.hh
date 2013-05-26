858f766661161de078246711c1db5af72e64eecc
Guy Zana <guy@cloudius-systems.com>
2013-05-26 16:41:27 +0300

    tests: extend timer test, make it more stressful
    noticed an assert in the download file test that was related to timers,
    this test reproduce the same bug.

--- tests/tst-timer.hh
+++ tests/tst-timer.hh
@@ -4,12 +4,13 @@
 #include "tst-hub.hh"
 #include "drivers/clock.hh"
 #include "debug.hh"
+#include "sched.hh"
 
 class test_timer : public unit_tests::vtest {
 
 public:
 
-    void run()
+    void test1(void)
     {
         auto t1 = clock::get()->time();
         auto t2 = clock::get()->time();
@@ -27,6 +28,48 @@ public:
         nanosleep(&ts, nullptr);
         t2 = clock::get()->time();
         debug("Timer test: nanosleep(100000) -> %d\n", t2 - t1);
+    }
+
+    static const int max_testers = 5000;
+    static const int tester_iteration = 10;
+
+    void stress_thread(void)
+    {
+        srandom(clock::get()->time());
+
+        for (int i=0; i<tester_iteration; i++) {
+            u64 ns = (random() % 1000000000) - 500000000; // -5 to +5 ms
+            sched::timer t(*sched::thread::current());
+            t.set(clock::get()->time() + ns);
+
+            sched::thread::wait_until([&] { return (t.expired()); });
+        }
+    }
+
+
+    // stress test the timer class
+    void test2(void)
+    {
+        debug("Starting stress test\n");
+        for (int i=0; i<max_testers; i++) {
+            _testers[i] = new sched::thread([&] { this->stress_thread(); });
+            _testers[i]->start();
+        }
+
+        // join
+        for (int i=0; i<max_testers; i++) {
+            _testers[i]->join();
+            delete _testers[i];
+        }
+        debug("End stress test\n");
+    }
+
+    sched::thread *_testers[max_testers];
+
+    void run()
+    {
+        //test1();
+        test2();
     }
 };
 
