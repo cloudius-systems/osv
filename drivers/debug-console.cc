4fd297124e1cc2cfdbe6a0912c3ff3db9e1a62d8
Glauber Costa <glommer@cloudius-systems.com>
2013-06-11 23:31:06 +0400

    console: dump early messages to the serial port
    We can use a very simple outb instruction to write data to the serial
    port in case we don't have a console implementation yet. We don't need
    to be fancy, and even limited functionality will already allow us to
    print messages early, (specially debug).
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

--- drivers/debug-console.cc
+++ drivers/debug-console.cc
@@ -1,5 +1,20 @@
 #include "debug-console.hh"
+#include "processor.hh"
 
+// Write to the serial port if the console is not yet initialized.  Because we
+// are just dumping output, no initialization is necessary.  We take advantage
+// of the fact that we are running on virtual hardware that probably does not
+// implement buffering like real UART does.  If it ever becomes a problem we
+// may need to improve this. But since we run only for a small amount of time
+// and very early - your real console is soon to be set up, I don't anticipate
+// any.
+static void simple_write(const char *str, size_t len)
+{
+    while (len > 0) {
+        processor::outb(*str++, 0x3f8);
+        len--;
+    }
+}
 void debug_console::set_impl(Console* impl)
 {
     with_lock(_lock, [=] { _impl = impl; });
@@ -7,14 +22,21 @@ void debug_console::set_impl(Console* impl)
 
 void debug_console::write(const char* str, size_t len)
 {
-    with_lock(_lock, [=] { if (_impl) { _impl->write(str, len); }});
+    with_lock(_lock, [=] {
+        if (_impl) {
+            _impl->write(str, len);
+        } else {
+            simple_write(str, len);
+        }
+    });
 }
 
 void debug_console::write_ll(const char *str, size_t len)
 {
     if (_impl) {
         _impl->write(str, len);
-    }
+    } else
+        simple_write(str, len);
 }
 
 void debug_console::newline()
