cloudius-systems/osv

commit 4bfe157bf07d5b26046a7969994e245c751ecfaf

cli: self-registering commands
Instead of defining a command object in one file and registering it in
another, do everything in one place.

console/cli.js
@@ -2,6 +2,12 @@ importPackage(java.io);
 importPackage(com.cloudius.util);
 importPackage(com.cloudius.cli.util);
 
+var _commands = {}
+
+var register_command = function(name, command) {
+    _commands[name] = command
+}
+
 load("/console/util.js");
 load("/console/autocomplete.js");
 load("/console/optparse.js");
@@ -19,22 +25,6 @@ load("/console/route.js");
 load("/console/java.js");
 load("/console/perf.js");
 
-// Commands
-var _commands = new Array();
-_commands["ls"] = ls;
-_commands["cd"] = cd;
-_commands["pwd"] = pwd;
-_commands["cat"] = cat;
-_commands["help"] = help_cmd;
-_commands["test"] = test_cmd;
-_commands["run"] = run_cmd;
-_commands["ifconfig"] = ifconfig_cmd;
-_commands["arp"] = arp_cmd;
-_commands["route"] = route_cmd;
-_commands["md5sum"] = md5sum_cmd;
-_commands["java"] = java_cmd;
-_commands["perf"] = perf_cmd;
-
 // Create interface to networking functions
 var networking_interface = new Networking();
 
@@ -51,6 +41,9 @@ var _c = 0;
 
 /* FIXME: Handle multi-line commands */
 var MAX_LENGTH = 80;
+
+var cd = _commands.cd
+var run_cmd = _commands.run
 
 // Prompt
 _prompt = "[osv]$ ";

console/arp.js
@@ -1,5 +1,5 @@
 
-var arp_cmd = {
+register_command('arp', {
    
     add: function(ifname, macaddr, ip) {
         networking_interface.arp_add(ifname, macaddr, ip);
@@ -19,6 +19,6 @@ var arp_cmd = {
         print("arp: manipulate the system ARP cache")
         print("usage: arp <ifname> <macaddr> <ip>")
     }
-};
+})
 
 

console/cat.js
@@ -1,5 +1,5 @@
 
-var cat = {
+register_command('cat', {
         
     invoke: function(inp) {
         if (inp.length != 2) {
@@ -41,6 +41,5 @@ var cat = {
             
         return (ls.ls());
     },
-};
-
+})
 

console/cd.js
@@ -1,5 +1,5 @@
 
-var cd = {
+register_command('cd', {
         
     init: function() {
         this._cwd = new File(".");
@@ -75,6 +75,6 @@ var cd = {
             _line_idx++;
         }
     }
-};
+})
 
 

console/help.js
@@ -1,5 +1,5 @@
 
-var help_cmd = {
+register_command('help', {
         
     invoke: function(inp) {
         if (inp.length != 2) {
@@ -23,6 +23,6 @@ var help_cmd = {
     help: function() {
         print("help: describe command");
     }
-};
+})
 
 

console/ifconfig.js
@@ -1,5 +1,5 @@
 
-var ifconfig_cmd = {
+register_command('ifconfig', {
         
     // returns a boolean indicating a successful operation
     set_ip: function(ifname, ip, netmask) {
@@ -47,6 +47,6 @@ var ifconfig_cmd = {
     help: function() {
         print("ifconfig <ifname> <ip> netmask <mask> up")
     }
-};
+})
 
 

console/java.js
@@ -1,7 +1,7 @@
-var java_cmd = {
+register_command('java', {
     invoke: function(argv) {
         argv.shift();  // Remove the command name ("java")
         Packages.RunJava.main(argv);
         return 0;
     },
-};
+})

console/ls.js
@@ -1,5 +1,5 @@
 
-var ls = {
+register_command('ls', {
         
     ls_dirs: function(subdir) {
         var dir;
@@ -39,4 +39,4 @@ var ls = {
         print("ls: list files in current directory");
     }
 
-};
+})

commit 79a89f95790ed456a4b5ab1fa3f23ac62399fd86

cli: fix tab completion
tab completion relies on a global 'ls' object, re-add it.

Broken by 4bfe157bf07d5b2.

console/cli.js
@@ -44,6 +44,7 @@ var MAX_LENGTH = 80;
 
 var cd = _commands.cd
 var run_cmd = _commands.run
+var ls = _commands.ls
 
 // Prompt
 _prompt = "[osv]$ ";