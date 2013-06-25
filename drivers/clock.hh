dae995899f4341db2fe64e715bbe9e995852b457
Glauber Costa <glommer@cloudius-systems.com>
2013-06-25 17:39:44 +0400

    buildfix: pvclock xen definitions

--- drivers/clock.hh
+++ drivers/clock.hh
@@ -3,6 +3,23 @@
 
 #include <osv/types.h>
 
+struct pvclock_wall_clock {
+        u32   version;
+        u32   sec;
+        u32   nsec;
+} __attribute__((__packed__));
+
+struct pvclock_vcpu_time_info {
+         u32   version;
+         u32   pad0;
+         u64   tsc_timestamp;
+         u64   system_time;
+         u32   tsc_to_system_mul;
+         s8    tsc_shift;
+         u8    flags;
+         u8    pad[2];
+} __attribute__((__packed__)); /* 32 bytes */
+
 class clock {
 public:
     virtual ~clock();
