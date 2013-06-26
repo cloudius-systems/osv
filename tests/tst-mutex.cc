bf5114ee01ac5b14e1095be49bc55b2d9edf5acc
Nadav Har'El <nyh@cloudius-systems.com>
2013-06-26 11:13:44 +0300

    Improved tst-mutex
    A bunch of improvements to tst-mutex. Add another mutual-exclusion
    test case, some asserts, and a benchmark of the "handoff" corner
    case.

--- tests/tst-mutex.cc
+++ tests/tst-mutex.cc
@@ -15,9 +15,10 @@
 template <typename T>
 static void increment_thread(int id, T *m, long len, volatile long *shared)
 {
-    //debug("Starting thread %d(%d)\n", id, sched::thread::current()->id());
     for(int i=0; i<len; i++){
         m->lock();
+        assert(m->getdepth()==1);
+        assert(m->owned());
         int val = *shared;
         *shared = val+1;
         m->unlock();
@@ -26,6 +27,30 @@ static void increment_thread(int id, T *m, long len, volatile long *shared)
         }
     }
 }
+
+template <typename T>
+using threadfunc =  decltype(increment_thread<T>);
+
+// checker_thread loops() sets a shared value to a known number, and
+// verifies that no other thread changes it. It checks the mutual-
+// exclusion capabilities of the mutex better than increment_thread.
+template <typename T>
+static void checker_thread(int id, T *m, long len, volatile long *shared)
+{
+    for (int i = 0; i < len; i++) {
+        m->lock();
+        assert(m->getdepth()==1);
+        assert(m->owned());
+        *shared = id;
+        for (int j = 0; j < 1000; j++)
+            assert(*shared == id);
+        sched::thread::yield();
+        for (int j = 0; j < 1000; j++)
+            assert(*shared == id);
+        m->unlock();
+    }
+}
+
 
 
 // Unfortunately, C++ lacks the trivial feature of converting a type's name,
@@ -49,20 +74,20 @@ template<typename T> const char *typeinfo<T>::_name = nullptr;
 
 // Test N concurrent threads using mutex, each pinned to a different cpu (N<=sched::cpus.size()).
 template <typename T>
-static void test(int N, long len, bool pinned)
+static void test(int N, long len, bool pinned, threadfunc<T> f)
 {
     debug("Contended mutex test, %s, %d %spinned threads\n",typeinfo<T>::name(), N,
             pinned ? "" : "non-");
-    //free(name);
     assert (!pinned || (unsigned int)N <= sched::cpus.size());
     long shared=0;
     T m;
     sched::thread *threads[N];
     for(int i = 0; i < N; i++) {
-        threads[i]= new sched::thread([i, len, &m, &shared] {
-            increment_thread(i, &m, len, &shared);
+        threads[i]= new sched::thread([i, len, &m, &shared, f] {
+            f(i, &m, len, &shared);
         }, pinned ? sched::thread::attr(sched::cpus[i]) : sched::thread::attr());
     }
+    auto t1 = clock::get()->time();
     for(int i = 0; i < N; i++) {
         threads[i]->start();
     }
@@ -70,8 +95,12 @@ static void test(int N, long len, bool pinned)
         threads[i]->join();
         delete threads[i];
     }
+    auto t2 = clock::get()->time();
     debug("\n");
-    assert(shared==len*N);
+    debug ("%d ns\n", (t2-t1)/len);
+    if (f == &increment_thread<T>) {
+        assert(shared==len*N);
+    }
 }
 
 // Test N concurrent threads using mutex, each pinned to a different cpu (N<=sched::cpus.size()).
@@ -95,28 +124,76 @@ static void show_size()
     debug("Size of %s: %d\n", typeinfo<T>::name(), sizeof(T));
 }
 
+// handoff_stressing_mutex is only for measure_uncontended<>. It stresses the
+// "handoff" case of the lockfree mutex - before calling unlock() it
+// always increments count (actually, sets it to 2, because we assume
+// it's 1), then calls unlock() which thinks it is racing with another
+// lock and uses the handoff protocol, and after unlock() finishes, it
+// decrements the count back.
+class handoff_stressing_mutex : public lockfree::mutex {
+public:
+    inline void unlock() {
+        // Note: we set count to 2 instead of count++ because it's faster
+        // and we want to measure the real unlock(), not to inflate the
+        // measurement with these extra instructions.
+        count.store(2, std::memory_order_relaxed);
+        lockfree::mutex::unlock();
+        count.store(0, std::memory_order_relaxed);
+    }
+};
+
 int main(int argc, char **argv)
 {
     debug("Running mutex tests\n");
     show_size<lockfree::mutex>();
+#ifndef LOCKFREE_MUTEX
     show_size<mutex>();
+#endif
     show_size<spinlock>();
 
     measure_uncontended<lockfree::mutex>(10000000);
+    measure_uncontended<handoff_stressing_mutex>(10000000);
+#ifndef LOCKFREE_MUTEX
     measure_uncontended<mutex>(10000000);
+#endif
     measure_uncontended<spinlock>(10000000);
 
-    test<lockfree::mutex>((int)sched::cpus.size(), 1000000, true);
-    test<lockfree::mutex>(2, 1000000, true);
-    test<lockfree::mutex>(20, 1000000, false);
 
-    test<mutex>((int)sched::cpus.size(), 1000000, true);
-    test<mutex>(2, 1000000, true);
-    test<mutex>(20, 1000000, false);
+    // The lockfree mutex's biggest challenge is what to do in unlock() when
+    // we want to wake a concurrent lock() but the wait queue is still empty
+    // (because the concurrent lock() didn't yet put itself there). The more
+    // concurrent locking threads we have, the less chance we have to find an
+    // empty queue, so somewhat counter-intuitively, the test with 2 threads
+    // stresses the "handoff" feature more than tests with more threads.
 
-    test<spinlock>((int)sched::cpus.size(), 1000000, true);
-    test<spinlock>(2, 1000000, true);
-    test<spinlock>(20, 1000000, false);
+
+    auto lff = increment_thread<lockfree::mutex>;
+    int n = 10000;
+    test<lockfree::mutex>(2, n, true, lff);
+    test<lockfree::mutex>((int)sched::cpus.size(), n, true, lff);
+    test<lockfree::mutex>(20, n, false, lff);
+
+    n = 1000000;
+    test<lockfree::mutex>(2, n, true, lff);
+    test<lockfree::mutex>((int)sched::cpus.size(), n, true, lff);
+    test<lockfree::mutex>(20, n, false, lff);
+
+    lff = checker_thread<lockfree::mutex>;
+    n = 100000;
+    test<lockfree::mutex>(2, n, true, lff);
+    test<lockfree::mutex>((int)sched::cpus.size(), n, true, lff);
+    test<lockfree::mutex>(20, n, false, lff);
+
+#ifndef LOCKFREE_MUTEX
+    auto f = increment_thread<mutex>;
+    test<mutex>((int)sched::cpus.size(), 1000000, true, f);
+    test<mutex>(2, 1000000, true, f);
+    test<mutex>(20, 1000000, false, f);
+#endif
+
+//    test<spinlock>((int)sched::cpus.size(), 1000000, true);
+//    test<spinlock>(2, 1000000, true);
+//    test<spinlock>(20, 1000000, false);
 
 
     debug("mutex tests succeeded\n");
