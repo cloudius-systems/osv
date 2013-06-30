120a19d92f43c06144daa063e88934158c42dfc6
Guy Zana <guy@cloudius-systems.com>
2013-06-30 14:40:57 +0300

    lockless: spsc ring buffer of fixed size
    single-producer / single-consumer lockless ring buffer of fixed size.
    
        1. _begin points to the head of the ring, _end points to the tail.
           both advance forward, adding items (_end++), consuming items (_begin++)
    
        2. all indexes of the ring are running, so the condition for empty ring is
           different than full ring.
                *) Empty -> (_begin == _end)
                *) Full -> (_end - _begin = MaxSize)
    
        3. uses only store and load operations, no rmw.

---
+++ include/lockfree/ring.hh
@@ -0,0 +1,62 @@
+//
+// single-producer / single-consumer lockless ring buffer of fixed size.
+//
+#ifndef __LF_RING_HH__
+#define __LF_RING_HH__
+
+#include <atomic>
+#include <sched.hh>
+#include <arch.hh>
+
+//
+// spsc ring of fixed size
+//
+template<class T, unsigned MaxSize>
+class ring_spsc {
+public:
+    ring_spsc(): _begin(0), _end(0) { }
+
+    bool push(const T& element)
+    {
+        unsigned end = _end.load(std::memory_order_relaxed);
+        unsigned beg = _begin.load(std::memory_order_relaxed);
+
+        if (end - beg >= MaxSize) {
+            return false;
+        }
+
+        _ring[end % MaxSize] = element;
+        _end.store(end + 1, std::memory_order_release);
+
+        return true;
+    }
+
+    bool pop(T& element)
+    {
+        unsigned beg = _begin.load(std::memory_order_relaxed);
+        unsigned end = _end.load(std::memory_order_acquire);
+
+        if (beg >= end) {
+            return false;
+        }
+
+        element = _ring[beg % MaxSize];
+        _begin.store(beg + 1, std::memory_order_relaxed);
+
+        return true;
+    }
+
+    unsigned size() {
+        unsigned end = _end.load(std::memory_order_relaxed);
+        unsigned beg = _begin.load(std::memory_order_relaxed);
+
+        return (end - beg);
+    }
+
+private:
+    std::atomic<unsigned> _begin CACHELINE_ALIGNED;
+    std::atomic<unsigned> _end CACHELINE_ALIGNED;
+    T _ring[MaxSize];
+};
+
+#endif // !__LF_RING_HH__
