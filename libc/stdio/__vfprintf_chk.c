2689d43de40c018e8de740aedb8b0499c2ee590b
Nadav Har'El <nyh@cloudius-systems.com>
2013-07-02 13:42:02 +0300

    Implement __vsnprintf_chk
    __vsnprintf_chk can be used by glibc when it knows the size of a buffer,
    to verify that the size parameter given to vsnprintf() doesn't overflow
    the buffer. Unfortunately, it happens to be used in libevent.so (used
    by memcached).

--- libc/stdio/__vfprintf_chk.c
+++ libc/stdio/__vfprintf_chk.c
@@ -1,6 +1,16 @@
 #include "stdio_impl.h"
+#include <stdlib.h>
 
 int __vfprintf_chk(FILE * f, int flag, const char *fmt, va_list ap)
 {
     return vfprintf(f, fmt, ap);
+}
+
+int __vsnprintf_chk(char *s, size_t maxlen, int flags, size_t slen,
+        const char *format, va_list args)
+{
+    if (slen < maxlen) {
+        abort();
+    }
+    return vsnprintf(s, slen, format, args);
 }
