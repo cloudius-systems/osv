f13e49c345a0c319f47a0f5f1cfa2b32b0941914
Guy Zana <guy@cloudius-systems.com>
2013-07-04 18:13:33 +0300

    percpu: map percpu variables to the same static elf section
    we don't want to use malloc() in the percpu framework since the allocator
    itself will have a percpu design.

--- core/percpu.cc
+++ core/percpu.cc
@@ -1,14 +1,17 @@
 #include <osv/percpu.hh>
 #include <string.h>
 
+extern char _percpu_start[], _percpu_end[], _percpu_sec_end[];
 std::vector<void*> percpu_base{64};  // FIXME: move to sched::cpu
-
-extern char _percpu_start[], _percpu_end[];
 
 void percpu_init(unsigned cpu)
 {
-    assert(!percpu_base[cpu]);
-    auto size = _percpu_end - _percpu_start;
-    percpu_base[cpu] = malloc(size);
-    memcpy(percpu_base[cpu], _percpu_start, size);
+    auto max_size = _percpu_sec_end - _percpu_start;
+    auto pcpu_size = _percpu_end - _percpu_start;
+    assert(pcpu_size * (cpu+1) < max_size);
+    percpu_base[cpu] = _percpu_start + cpu*pcpu_size;
+
+    if (cpu != 0) {
+        memcpy(percpu_base[cpu], _percpu_start, pcpu_size);
+    }
 }
