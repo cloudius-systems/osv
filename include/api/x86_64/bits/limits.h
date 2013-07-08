7ab8afa520a3c99f96788f74edb50d88a866c655
Glauber Costa <glommer@cloudius-systems.com>
2013-07-08 21:34:09 +0400

    add PAGE_SHIFT macro
    It is usually very useful, together with PAGE_SIZE

--- include/api/x86_64/bits/limits.h
+++ include/api/x86_64/bits/limits.h
@@ -1,5 +1,6 @@
 #if defined(_POSIX_SOURCE) || defined(_POSIX_C_SOURCE) \
  || defined(_XOPEN_SOURCE) || defined(_GNU_SOURCE) || defined(_BSD_SOURCE)
+#define PAGE_SHIFT 12
 #define PAGE_SIZE 4096
 #define LONG_BIT 64
 #endif
