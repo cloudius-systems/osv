44bd271f2dbbf302c0ed40b90687646590741421
Avi Kivity <avi@cloudius-systems.com>
2013-07-15 18:57:24 +0300

    percpu: introduce dynamic_percpu<>
    dynamic_percpu<T> allocates and initializes an object of type T on all cpus
    (if a cpu is later hotplugged, it will also get an instance).  Unlike ordinary
    percpu variables, dynamic_percpu objects can be used in a dynamic scope, that
    is, in objects that are not in static scope (one the stack or heap).

--- include/osv/percpu.hh
+++ include/osv/percpu.hh
@@ -2,6 +2,8 @@
 #define PERCPU_HH_
 
 #include <sched.hh>
+#include <type_traits>
+#include <memory>
 
 extern char _percpu_start[];
 
@@ -27,9 +29,43 @@ private:
     }
 private:
     T _var;
+    friend size_t dynamic_percpu_base();
 };
 
 #define PERCPU(type, var) percpu<type> var __attribute__((section(".percpu")))
+
+size_t dynamic_percpu_alloc(size_t size, size_t align);
+void dynamic_percpu_free(size_t offset, size_t size);
+
+template <typename T, size_t align = std::alignment_of<T>::value>
+class dynamic_percpu {
+public:
+    dynamic_percpu()
+        : _offset(dynamic_percpu_alloc(sizeof(T), align))
+        , _notifier(new sched::cpu::notifier([=] { new (addr()) T(); }))
+    {
+        for (auto c : sched::cpus) {
+            new (for_cpu(c)) T();
+        }
+    }
+    ~dynamic_percpu() {
+        for (auto c : sched::cpus) {
+            for_cpu(c)->~T();
+        }
+        dynamic_percpu_free(_offset, sizeof(T));
+    }
+    T* operator->() { return addr(); }
+    T& operator*() { return *addr(); }
+    T* for_cpu(sched::cpu* cpu) { return addr(cpu->percpu_base); }
+private:
+    T* addr(void* base = percpu_base) {
+        return static_cast<T*>(base + _offset);
+    }
+private:
+    size_t _offset;
+    std::unique_ptr<sched::cpu::notifier> _notifier;
+};
+
 
 void percpu_init(unsigned cpu);
 
