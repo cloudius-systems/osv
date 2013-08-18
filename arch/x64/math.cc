4bc96b95f0c7d69e66ee59f7523026b4d5b0e08f
Avi Kivity <avi@cloudius-systems.com>
2013-08-18 19:30:20 +0300

    libc: drop sse4.1 ceil(), floor()
    The dependency on sse4.1 crashes on older cpus, use the generic musl
    implementation.

--- arch/x64/math.cc
+++ arch/x64/math.cc
@@ -1,22 +1,6 @@
 #include <math.h>
 #include <osv/types.h>
 
-// FIXME: check for sse4.1
-
-double ceil(double v)
-{
-    double r;
-    asm("roundsd $2, %1, %0" : "=x"(r) : "x"(v));
-    return r;
-}
-
-double floor(double v)
-{
-    double r;
-    asm("roundsd $1, %1, %0" : "=x"(r) : "x"(v));
-    return r;
-}
-
 extern "C"
 int __isnan(double v)
 {
