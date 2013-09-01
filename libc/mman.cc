cloudius-systems/osv

commit a7d6b269ec6d416509a442e583848404cb5829d9

mman: Use libc_error() in mprotect()

libc/mman.cc
@@ -33,14 +33,12 @@ int mprotect(void *addr, size_t len, int prot)
 
     if ((reinterpret_cast<intptr_t>(addr) & 4095) || (len & 4095)) {
         // address not page aligned
-        errno = EINVAL;
-        return -1;
+        return libc_error(EINVAL);
     }
     if (!mmu::protect(addr, len, libc_prot_to_perm(prot))) {
         // NOTE: we return ENOMEM when part of the range was not mapped,
         // but nevertheless, set the protection on the rest!
-        errno = ENOMEM;
-        return -1;
+        return libc_error(ENOMEM);
     }
     return 0;
 }

commit 584c17374f657fdca55f1097c34ccc35d397f4e9

mman: Fix missing libc.hh include
Fixes the following build brekage caused by commit a7d6b26 ("mman: Use
libc_error() in mprotect()"):

../../libc/mman.cc: In function ‘int mprotect(void*, size_t, int)’:
../../libc/mman.cc:36:33: error: ‘libc_error’ was not declared in this scope
         return libc_error(EINVAL);
                                 ^
../../libc/mman.cc:41:33: error: ‘libc_error’ was not declared in this scope
         return libc_error(ENOMEM);
                                 ^
  CXX libc/pipe_buffer.o
  CXX libc/pipe.o
make[1]: *** [libc/mman.o] Error 1

libc/mman.cc
@@ -3,6 +3,7 @@
 #include "mmu.hh"
 #include "debug.hh"
 #include "osv/trace.hh"
+#include "libc/libc.hh"
 
 TRACEPOINT(trace_memory_mmap, "ret=%p, addr=%p, length=%d, prot=%d, flags=%d, fd=%d, offset=%d", void *, void *, size_t, int, int, int, off_t);
 TRACEPOINT(trace_memory_munmap, "addr=%p, length=%d", void *, size_t);