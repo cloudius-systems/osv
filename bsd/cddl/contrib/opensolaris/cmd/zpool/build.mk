24665da0a91aaedaab5d77aaf20346c108f627eb
Avi Kivity <avi@cloudius-systems.com>
2013-09-05 19:00:19 +0300

    build: wire up zpool-cmd build
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

---
+++ bsd/cddl/contrib/opensolaris/cmd/zpool/build.mk
@@ -0,0 +1,17 @@
+
+zpool-cmd-file-list = zpool_iter  zpool_main  zpool_util  zpool_vdev
+
+zpool-cmd-objects = $(foreach x, $(zpool-cmd-file-list), bsd/cddl/contrib/opensolaris/cmd/zpool/$x.o)
+
+cflags-zpool-cmd-includes = $(cflags-libzfs-include) -I$(src)/bsd/cddl/contrib/opensolaris/cmd/stat/common
+
+$(zpool-cmd-objects): kernel-defines =
+
+$(zpool-cmd-objects): local-includes += $(cflags-zpool-cmd-includes)
+
+$(zpool-cmd-objects): CFLAGS += -Wno-switch -D__va_list=__builtin_va_list '-DTEXT_DOMAIN=""' \
+			-Wno-maybe-uninitialized -Wno-unused-variable -Wno-unknown-pragmas -Wno-unused-function
+
+
+zpool.so: $(zpool-cmd-objects) libzfs.so
+	$(q-build-so)
