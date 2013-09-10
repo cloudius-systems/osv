cb8bf7f55fe6947ad23909b6be466c27fb31d7fd
Avi Kivity <avi@cloudius-systems.com>
2013-09-10 18:47:07 +0300

    zfs_context: build adjustments
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- bsd/cddl/contrib/opensolaris/lib/libzpool/common/sys/zfs_context.h
+++ bsd/cddl/contrib/opensolaris/lib/libzpool/common/sys/zfs_context.h
@@ -564,6 +564,14 @@ extern int zfs_secpolicy_rename_perms(const char *from, const char *to,
     cred_t *cr);
 extern int zfs_secpolicy_destroy_perms(const char *name, cred_t *cr);
 extern zoneid_t getzoneid(void);
+
+#ifdef __OSV__
+
+#define GLOBAL_ZONEID 0
+#define getzoneid() GLOBAL_ZONEID
+
+#endif
+
 /* Random compatibility stuff. */
 #define	lbolt	(gethrtime() >> 23)
 #define	lbolt64	(gethrtime() >> 23)
