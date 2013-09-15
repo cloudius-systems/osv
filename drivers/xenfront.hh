c0e0ebf2b1b2cdb4c0c5d3f1ac06eeec0d2d8183
Nadav Har'El <nyh@cloudius-systems.com>
2013-09-15 16:20:44 +0300

    Add copyright statement to drivers/*
    Add Cloudius copyright and license statement to drivers/*.
    
    A couple of header files were based on Linux's BSD-licensed header files
    (e.g., include/uapi/linux/virtio_net.h) so they included the BSD license,
    but not any copyright statement, so we can just replace that by our own
    statement of the BSD license.

--- drivers/xenfront.hh
+++ drivers/xenfront.hh
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2013 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
 #ifndef XENFRONT_DRIVER_H
 #define XENFRONT_DRIVER_H
 
