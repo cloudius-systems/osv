6e918b0c78615b6140a49e6fbca81e9115bd6591
Nadav Har'El <nyh@cloudius-systems.com>
2013-09-15 13:42:31 +0300

    Add Cloudius copyright to everything in arch/x64
    Add Cloudius copyright to everything in arch/x64. This includes C++ code, assembly
    code, and ld scripts.

--- arch/x64/bitops.h
+++ arch/x64/bitops.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2013 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
 #ifndef ARCH_BITOPS_H_
 #define ARCH_BITOPS_H_
 static inline unsigned int 
