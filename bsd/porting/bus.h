fe9e6a8272cbef0a0a05d3523ed9c453f2d254db
Nadav Har'El <nyh@cloudius-systems.com>
2013-09-15 14:22:32 +0300

    Add copyright statements in bsd/
    Added our copyright statements to some of the files in the top bsd/
    directory, and in bsd/porting.
    
    I only added our copyright to files which were completely by us - I did
    not attempt to hunt which bsd or solaris files we modified to add our
    copyright to them, I don't think this is important (or, we can do this
    later).
    
    I also found one header file (uma_stub.h) that had large chunks copied from
    freebsd, so I added both the freebsd copyright and ours.

--- bsd/porting/bus.h
+++ bsd/porting/bus.h
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2013 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
 #ifndef _OSV_BSD_BUS_H
 #define _OSV_BSD_BUS_H
 #include "mmu.h"
--- bsd/porting/bus_dma.cc
+++ bsd/porting/bus_dma.cc
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2013 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
 // Simplified implementation of BSD's bus_dma interfaces
 #include <bsd/porting/netport.h>
 #include <bsd/porting/bus.h>
