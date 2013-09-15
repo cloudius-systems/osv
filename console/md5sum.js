62f3e58d555b6a8d86bdade098e269f3ae80dbe2
Nadav Har'El <nyh@cloudius-systems.com>
2013-09-15 14:53:22 +0300

    Add copyright statement to console/*
    Added copyright statement to all Javascript commands for our old CLI, in
    console/*. We should eventually remove this directory, but while it still
    exists, let's add copyright statements.

--- console/md5sum.js
+++ console/md5sum.js
@@ -1,3 +1,10 @@
+/*
+ * Copyright (C) 2013 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
 register_command('md5sum', {
     init: function() {
         this._md5 = new MD5();
