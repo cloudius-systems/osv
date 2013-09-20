ade5bc3e31a32678f3e0beea8a932814adbb28f5
Nadav Har'El <nyh@cloudius-systems.com>
2013-09-20 12:26:17 +0300

    Tests: add trivial sleep test
    Add a trivial sleep() test, which sleep()s for 2 seconds, and verifies
    that this finishes and has slept for roughly 2 seconds.
    
    I used this for debugging issue #26 - the attempts there ruined timers,
    and in particular this trivial test hangs, as sleep() never returns.
    
    (A note to our future automatic testing implementor: We need to allow
    for the possibility that a test doesn't cleanly fail, but rather hangs,
    and consider this a failure too).
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>

--- build.mk
+++ build.mk
@@ -142,6 +142,7 @@ tests += tests/tst-wake.so
 tests += tests/tst-epoll.so
 tests += tests/tst-lfring.so
 tests += tests/tst-fsx.so
+tests += tests/tst-sleep.so
 tests += tests/tst-resolve.so
 tests += tests/tst-except.so
 tests += tests/tst-tcp-sendonly.so
---
+++ tests/tst-sleep.cc
@@ -0,0 +1,25 @@
+#include <debug.hh>
+
+#include <chrono>
+
+int tests = 0, fails = 0;
+
+static void report(bool ok, const char* msg)
+{
+    ++tests;
+    fails += !ok;
+    debug("%s: %s\n", (ok ? "PASS" : "FAIL"), msg);
+}
+
+int main(int ac, char** av)
+{
+    debug("starting sleep test\n");
+    auto start = std::chrono::system_clock::now();
+    int r = sleep(2);
+    auto end = std::chrono::system_clock::now();
+    double sec = ((std::chrono::duration<double>)(end - start)).count();
+    report(r == 0, "sleep 2 seconds finished successfully");
+    report(sec >= 1.5 && sec < 2.5, "and slept for roughly 2 seconds");
+
+    debug("SUMMARY: %d tests, %d failures\n", tests, fails);
+}
