c995c41f7d71860323cf1219af9984b39f3141c5
Raphael S. Carvalho <raphael.scarv@gmail.com>
2013-09-23 08:57:10 -0300

    tst-fs-link: Add extra checks
    Add a new test to see if the content from one link would be the same in
    another.
    
    Signed-off-by: Raphael S. Carvalho <raphael.scarv@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- tests/tst-fs-link.cc
+++ tests/tst-fs-link.cc
@@ -26,26 +26,42 @@ static void report(bool ok, const char* msg)
 int main(int argc, char *argv[])
 {
     struct stat st[2];
+    char buf[4] = { 0 };
+    char buf2[4] = { 0 };
+
     // Create a temporary file that's used in testing.
-    auto fd = open("foo", O_CREAT|O_TRUNC|O_RDWR, 0666);
+    auto fd = open("/usr/foo", O_CREAT|O_TRUNC|O_RDWR, 0666);
+    write(fd, "test", 4);
+    lseek(fd, 0, SEEK_SET);
+    read(fd, buf, 4);
     report(fd > 0, "create a file");
     report(close(fd) == 0, "close the file");
+
     // Create a hard link
-    report(link("foo", "foo2") == 0, "create a hard link");
+    report(link("/usr/foo", "/usr/foo2") == 0, "create a hard link");
+
+    auto fd2 = open("/usr/foo2", O_RDONLY);
+    lseek(fd2, 0, SEEK_SET);
+    read(fd2, buf2, 4);
+    close(fd2);
+
+    // Check that it's possible to read the content from another link
+    // of the same inode.
+    report(strncmp(buf, buf2, 4) == 0, "read content from another link");
     // Check that hard link count is increased
-    report(stat("foo", &st[0]) == 0, "stat the file");
+    report(stat("/usr/foo", &st[0]) == 0, "stat the file");
     report(st[0].st_nlink == 2, "hard link count is increased");
     // Check that hard link points to the same inode
-    report(stat("foo2", &st[1]) == 0, "stat the hard link");
+    report(stat("/usr/foo2", &st[1]) == 0, "stat the hard link");
     report(st[0].st_dev == st[1].st_dev, "stat device IDs match");
     report(st[0].st_ino == st[1].st_ino, "stat inode numbers match");
     // Remove the hard link
-    report(unlink("foo2") == 0, "remove the hard link");
+    report(unlink("/usr/foo2") == 0, "remove the hard link");
     // Check that hard link count is decreased
-    report(stat("foo", &st[0]) == 0, "stat the file");
+    report(stat("/usr/foo", &st[0]) == 0, "stat the file");
     report(st[0].st_nlink == 1, "hard link count is decreased");
     // Clean up the temporary file.
-    report(unlink("foo") == 0, "remove the file");
+    report(unlink("/usr/foo") == 0, "remove the file");
     // Report results.
     printf("SUMMARY: %d tests, %d failures\n", tests, fails);
     return 0;
