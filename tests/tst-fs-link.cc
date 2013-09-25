b94793ee4fe6c9295410616d913a7c206a773056
Pekka Enberg <penberg@cloudius-systems.com>
2013-09-25 12:32:45 +0300

    tst-fs-link.so: Make paths configurable
    Make the link source and target paths configurable so the test can be
    run on Linux:
    
      $ g++ -std=c++11 tst-fs-link.cc && ./a.out foo foo2
    
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- tests/tst-fs-link.cc
+++ tests/tst-fs-link.cc
@@ -25,12 +25,21 @@ static void report(bool ok, const char* msg)
 
 int main(int argc, char *argv[])
 {
+    const char *oldpath, *newpath;
     struct stat st[2];
     char buf[4] = { 0 };
     char buf2[4] = { 0 };
 
+    if (argc > 2) {
+        oldpath = argv[1];
+        newpath = argv[2];
+    } else {
+        oldpath = "/usr/foo";
+        newpath = "/usr/foo2";
+    }
+
     // Create a temporary file that's used in testing.
-    auto fd = open("/usr/foo", O_CREAT|O_TRUNC|O_RDWR, 0666);
+    auto fd = open(oldpath, O_CREAT|O_TRUNC|O_RDWR, 0666);
     write(fd, "test", 4);
     lseek(fd, 0, SEEK_SET);
     read(fd, buf, 4);
@@ -38,9 +47,9 @@ int main(int argc, char *argv[])
     report(close(fd) == 0, "close the file");
 
     // Create a hard link
-    report(link("/usr/foo", "/usr/foo2") == 0, "create a hard link");
+    report(link(oldpath, newpath) == 0, "create a hard link");
 
-    auto fd2 = open("/usr/foo2", O_RDONLY);
+    auto fd2 = open(newpath, O_RDONLY);
     lseek(fd2, 0, SEEK_SET);
     read(fd2, buf2, 4);
     close(fd2);
@@ -49,19 +58,19 @@ int main(int argc, char *argv[])
     // of the same inode.
     report(strncmp(buf, buf2, 4) == 0, "read content from another link");
     // Check that hard link count is increased
-    report(stat("/usr/foo", &st[0]) == 0, "stat the file");
+    report(stat(oldpath, &st[0]) == 0, "stat the file");
     report(st[0].st_nlink == 2, "hard link count is increased");
     // Check that hard link points to the same inode
-    report(stat("/usr/foo2", &st[1]) == 0, "stat the hard link");
+    report(stat(newpath, &st[1]) == 0, "stat the hard link");
     report(st[0].st_dev == st[1].st_dev, "stat device IDs match");
     report(st[0].st_ino == st[1].st_ino, "stat inode numbers match");
     // Remove the hard link
-    report(unlink("/usr/foo2") == 0, "remove the hard link");
+    report(unlink(newpath) == 0, "remove the hard link");
     // Check that hard link count is decreased
-    report(stat("/usr/foo", &st[0]) == 0, "stat the file");
+    report(stat(oldpath, &st[0]) == 0, "stat the file");
     report(st[0].st_nlink == 1, "hard link count is decreased");
     // Clean up the temporary file.
-    report(unlink("/usr/foo") == 0, "remove the file");
+    report(unlink(oldpath) == 0, "remove the file");
     // Report results.
     printf("SUMMARY: %d tests, %d failures\n", tests, fails);
     return 0;
