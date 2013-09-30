e4ceea63b2072f7ca94a7444e4133602d3b4df72
Venkatesh Srinivas <venkateshs@google.com>
2013-09-30 11:36:39 -0700

    kvmclock: Implement support for old kvmclock MSRs.
    Older versions of KVM and user VMMs expose kvmclock MSRs at different
    MSR offsets. Detect the old flag in kvmclock::probe() and use the old
    MSRs if they are the only ones available.
    
    Signed-off-by: Venkatesh Srinivas <venkateshs@google.com>
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

--- drivers/kvmclock.cc
+++ drivers/kvmclock.cc
@@ -20,12 +20,14 @@ class kvmclock : public clock {
 public:
     kvmclock();
     virtual s64 time() __attribute__((no_instrument_function));
+    static bool probe();
 private:
     u64 wall_clock_boot();
     u64 system_time();
     static void setup_cpu();
 private:
     static bool _smp_init;
+    static bool _new_kvmclock_msrs;
     pvclock_wall_clock* _wall;
     u64  _wall_ns;
     static PERCPU(pvclock_vcpu_time_info, _sys);
@@ -33,22 +35,42 @@ private:
 };
 
 bool kvmclock::_smp_init = false;
+bool kvmclock::_new_kvmclock_msrs = true;
 PERCPU(pvclock_vcpu_time_info, kvmclock::_sys);
 
 kvmclock::kvmclock()
     : cpu_notifier(&kvmclock::setup_cpu)
 {
+    auto wall_time_msr = (_new_kvmclock_msrs) ?
+                         msr::KVM_WALL_CLOCK_NEW : msr::KVM_WALL_CLOCK;
     _wall = new pvclock_wall_clock;
     memset(_wall, 0, sizeof(*_wall));
-    processor::wrmsr(msr::KVM_WALL_CLOCK_NEW, mmu::virt_to_phys(_wall));
+    processor::wrmsr(wall_time_msr, mmu::virt_to_phys(_wall));
     _wall_ns = wall_clock_boot();
 }
 
 void kvmclock::setup_cpu()
 {
+    auto system_time_msr = (_new_kvmclock_msrs) ?
+                           msr::KVM_SYSTEM_TIME_NEW : msr::KVM_SYSTEM_TIME;
     memset(&*_sys, 0, sizeof(*_sys));
-    processor::wrmsr(msr::KVM_SYSTEM_TIME_NEW, mmu::virt_to_phys(&*_sys) | 1);
+    processor::wrmsr(system_time_msr, mmu::virt_to_phys(&*_sys) | 1);
     _smp_init = true;
+}
+
+bool kvmclock::probe()
+{
+    if (!processor::features().kvm_clocksource2 &&
+        !processor::features().kvm_clocksource) {
+        return false;
+    }
+
+    if (!processor::features().kvm_clocksource2 &&
+        processor::features().kvm_clocksource) {
+        _new_kvmclock_msrs = false;
+    }
+
+    return true;
 }
 
 s64 kvmclock::time()
@@ -80,8 +102,7 @@ u64 kvmclock::system_time()
 
 static __attribute__((constructor(CLOCK_INIT_PRIO))) void setup_kvmclock()
 {
-    // FIXME: old clocksource too?
-    if (processor::features().kvm_clocksource2) {
+    if (kvmclock::probe()) {
         clock::register_clock(new kvmclock);
     }
 }
