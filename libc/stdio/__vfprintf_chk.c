e990aa2aaeeba2efa2564d3a7acc27f2444c6205
Nadav Har'El <nyh@cloudius-systems.com>
2013-10-03 14:15:36 +0300

    Yet another silly variant of sprintf
    Used somewhere within libcurses, so provide an implementation.
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>

--- libc/stdio/__vfprintf_chk.c
+++ libc/stdio/__vfprintf_chk.c
@@ -14,3 +14,16 @@ int __vsnprintf_chk(char *s, size_t maxlen, int flags, size_t slen,
     }
     return vsnprintf(s, slen, format, args);
 }
+
+int __sprintf_chk(char *s, int flags, size_t slen, const char *format, ...)
+{
+    va_list args;
+    int ret;
+    va_start(args, format);
+    ret = vsnprintf(s, slen, format, args);
+    if (ret > slen) {
+        abort();
+    }
+    va_end(args);
+    return ret;
+}
