bc420c7ace88279786c698da8483ae18effa990e
Nadav Har'El <nyh@cloudius-systems.com>
2013-10-07 16:13:24 +0300

    Start using doxygen for OSv's API documention
    This patch begins to document OSv's native (non-Linux-compatible) C++ APIs
    using Doxygen. With doxygen, functions, classes, and other APIs are
    documented using comments inside the source code, similarly to the way
    Javadoc is used in the Java world.
    
    After this patch, running "doxygen" generates the documentation in HTML
    form (and also ugly ones in LaTeX...) in the "doxyout" directory.
    After running "doxygen" in the OSv top-level directory, open your browser to
    file://.../doxyout/html/index.html to view this documentation.
    
    This patch adds a doxygen configuration file, "Doxyfile", and documents
    three functions in the osv namespace: osv::halt(), osv::poweroff(),
    osv::halt().
    
    The format of the resulting documentation is not perfect, but it's not
    as bad as I feared, and also the documentation doesn't uglify the code
    too much (it mainly impacts the header files), so I now believe it will
    better for us than writing man pages (an avenue which I explored
    previously).
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>

--- include/osv/power.hh
+++ include/osv/power.hh
@@ -8,12 +8,68 @@
 #ifndef INCLUDED_OSV_POWER_H
 #define INCLUDED_OSV_POWER_H
 
+/**
+ * OSv namespace
+ */
 namespace osv {
 
+/** @name #include <osv/power.hh>
+ * Functions for turning off or rebooting the virtual machine
+ */
+
+/**@{*/
+/**
+ * Abort the system, leaving all vCPUs in hung state.
+ *
+ * The running system is aborted immediately, without attempting any sort of
+ * clean shutdown (e.g., files are not flushed to disk). This makes halt()
+ * useful even when OSv's state is known to be broken, e.g., when detecting
+ * a bug.
+ *
+ * After a halt, the virtual machine will be hung (in an endless loop of HLT
+ * instructions). If you want to instruct the hypervisor to terminate this
+ * guest, use osv::poweroff() instead of halt().
+ *
+ * \return This function can never return.
+ */
 void halt() __attribute__((noreturn));
+
+/**
+ * Abort the system and terminate the virtual machine.
+ *
+ * The running system is aborted immediately, without attempting any sort of
+ * clean shutdown (e.g., files are not flushed to disk). This makes poweroff()
+ * useful even when OSv's state is known to be broken, e.g., when detecting
+ * a bug.
+ *
+ * poweroff() instructs the hypervisor to stop running the virtual machine.
+ * If for some reason the hypervisor cannot do this, the VM remains hung
+ * as in osv::halt().
+ *
+ * If you want to leave the virtual machine running after aborting the
+ * system, e.g., to attach a debugger, use osv::halt() instead of poweroff().
+ *
+ * \return This function can never return.
+ */
 void poweroff() __attribute__((noreturn));
+
+/**
+ * Reboot the virtual machine.
+ *
+ * The system is rebooted immediately, without attempting any sort of
+ * clean shutdown (e.g., files are not flushed to disk). This makes reboot()
+ * useful even when OSv's state is known to be broken, e.g., when detecting
+ * a bug.
+ *
+ * After the reboot, the system - OSv and its application - normally comes
+ * up again.
+ *
+ * \return Usually, this function does not return. But it may return in the
+ * unlikely case that the hypervisor refuses the reboot command.
+ */
 void reboot();
 
+/**@}*/
 }
 
 #endif /* INCLUDED_OSV_POWER_H */
