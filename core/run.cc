03f032f18bd8ba097b43172c08401f241c68c1f2
Avi Kivity <avi@cloudius-systems.com>
2013-10-14 16:32:54 +0300

    run: add helper for running with a vector<string> argv
    Much easier than constructing a traditional argc/argv.
    
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- core/run.cc
+++ core/run.cc
@@ -1,9 +1,15 @@
 #include <string.h>
+#include <algorithm>
+#include <vector>
+#include <boost/range/algorithm/transform.hpp>
 
 #include <elf.hh>
 #include <osv/run.hh>
 
 #include <debug.hh>
+
+using namespace std;
+using namespace boost::range;
 
 namespace osv {
 
@@ -24,6 +30,20 @@ bool run(std::string path, int argc, char** argv, int *return_code)
         *return_code = rc;
     }
     return true;
+}
+
+bool run(string path, vector<string> args, int* return_code)
+{
+    // C main wants mutable arguments, so we have can't use strings directly
+    vector<vector<char>> mut_args;
+    transform(args, back_inserter(mut_args),
+            [](string s) { return vector<char>(s.data(), s.data() + s.size() + 1); });
+    vector<char*> argv;
+    transform(mut_args.begin(), mut_args.end(), back_inserter(argv),
+            [](vector<char>& s) { return s.data(); });
+    auto argc = argv.size();
+    argv.push_back(nullptr);
+    return run(path, argc, argv.data(), return_code);
 }
 
 }
--- include/osv/run.hh
+++ include/osv/run.hh
@@ -10,6 +10,7 @@
 
 #include <elf.hh>
 #include <string>
+#include <vector>
 
 
 /**
@@ -74,6 +75,59 @@ namespace osv {
  * \return \c true if run, \c false if couldn't run
  */
 bool run(std::string path, int argc, char** argv, int *return_code);
+
+/**
+ * Run the given executable.
+ *
+ * Currently, The executable is assumed to be a shared object with a \c main()
+ * function as in Unix (i.e., a \c main function with C linkage, taking argc
+ * and argv and returning an int).
+ * This \c main function is run in the current thread.
+ *
+ * run() loads the given shared object into memory, and unloads it when not
+ * needed any more. In particular, if the same object is run concurrently, it
+ * will only be unloaded after the last of the concurrent runs has finished.
+ *
+ * It is worth noting how run() of a shared object differs Unix's traditional
+ * exec() of an executable:
+ *
+ * \li run() runs the \c main function in the current thread, but other treads
+ *     continue to run normally. run() returns when the \c main function
+ *     returns.
+ * \li When a Unix executable exits, its resources are all cleaned up. But our
+ *     shared objects need to clean up after themselves. If main() completes
+ *     with memory allocated, with open files, etc., then these resources will
+ *     never be freed. If main() returns while threads are still running its
+ *     code, or pointers to its memory are somewhere in the kernel's
+ *     structures, a crash will likely soon follow.
+ *     A convenient way to ensure that certain cleanups happen when the object
+ *     is unloaded is to use static objects with destructors. These
+ *     destructors are guaranteed to be run when the object is unloaded.
+ *     To run a main() which returns without waiting for all its threads to
+ *     finish, you must use elf::program::get_library() directly.
+ * \li When several instances of the same object run concurrently, they share
+ *     a single copy of static and global variables. Therefore main() cannot
+ *     assume that when it starts, static variables have their default values.
+ *     Moreover, a shared-object which is to be run concurrently on multiple
+ *     threads needs to access its global variables in a thread-safe manner
+ *     (e.g., using mutexes), or avoid globals, even if each main() call itself
+ *     is single-threaded. Some non-reentrant library functions (such as
+ *     getopt() with its \c optind global) are especially problematic in this
+ *     regard.
+ * \li The main() must complete with "return", not with exit().
+ *
+ * \param[in]  path        The pathname of the shared object to run
+ * \param[in]  args        The argument array <TT>argv</TT>. Traditionally,
+ *                         <TT>args[0]</TT> is the name of the
+ *                         program, often same as <TT>object</TT>.
+ * \param[out] return_code Where to write main()'s return code. If
+ *                         <TT>return_code == nulltr</TT>, main()'s return
+ *                         code is ignored.
+ *
+ * \return \c true if run, \c false if couldn't run
+ */
+bool run(std::string path, std::vector<std::string> args, int *return_code);
+
 /**@}*/
 
 }
