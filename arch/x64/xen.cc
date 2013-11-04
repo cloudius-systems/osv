cloudius-systems/osv

commit 0c77d3c2675dc1f06a3526c8079eae5442ed1a59

x64/xen: Fix assignment to xen_features.
The code wouldn't work if XENFEAT_NR_SUBMAPS > 1. It's currently 1.
The assignment to xen_features must take i as well as j into consideration.

Signed-off-by: Raphael S. Carvalho <raphael.scarv@gmail.com>
Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

arch/x64/xen.cc
@@ -181,7 +181,7 @@ void xen_init(processor::features_type &features, unsigned base)
             if (version_hypercall(XENVER_get_features, &info) < 0)
                 assert(0);
             for (int j = 0; j < 32; j++)
-                xen_features[j] = !!(info.submap & 1<<j);
+                xen_features[i * j] = !!(info.submap & 1<<j);
         }
         features.xen_clocksource = xen_features[9] & 1;
         features.xen_vector_callback = xen_features[8] & 1;

commit 0bdd4bc6e5211b8d7010c809989439d4d3df4619

xen: Fix xen_features initialization
Ironically, my commit 0c77d3c broke xen_features initialization.
This patch fixes it.

Reported-by: Avi Kivity <avi@cloudius-systems.com>
Signed-off-by: Raphael S. Carvalho <raphael.scarv@gmail.com>
Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

arch/x64/xen.cc
@@ -181,7 +181,7 @@ void xen_init(processor::features_type &features, unsigned base)
             if (version_hypercall(XENVER_get_features, &info) < 0)
                 assert(0);
             for (int j = 0; j < 32; j++)
-                xen_features[i * j] = !!(info.submap & 1<<j);
+                xen_features[i * 32 + j] = !!(info.submap & 1<<j);
         }
         features.xen_clocksource = xen_features[9] & 1;
         features.xen_vector_callback = xen_features[8] & 1;