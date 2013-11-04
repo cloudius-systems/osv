cloudius-systems/osv

commit c98b928e7e0503d9d7f3cf897612b8b25ec6a8bf

Explicitly use python version number
It is needed for systems where python points to python3

Signed-off-by: Anatol Pomozov <anatol.pomozov@gmail.com>
Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

scripts/loader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import gdb
 import re
@@ -493,7 +493,7 @@ gdb.events.cont.connect(continue_handler)
 def setup_libstdcxx():
     gcc = external + '/gcc.bin'
     sys.path += [gcc + '/usr/share/gdb/auto-load/usr/lib64',
-                 glob(gcc + '/usr/share/gcc-*/python')[0],
+                 glob(gcc + '/usr/share/gcc-*/python2')[0],
                  ]
     main = glob(gcc + '/usr/share/gdb/auto-load/usr/lib64/libstdc++.so.*.py')[0]
     execfile(main)

bsd/sys/xen/interface/foreign/Makefile
@@ -25,13 +25,13 @@ check-headers:
 endif
 
 x86_32.h: ../arch-x86/xen-x86_32.h ../arch-x86/xen.h ../xen.h $(scripts)
-	python mkheader.py $* $@ $(filter %.h,$^)
+	python2 mkheader.py $* $@ $(filter %.h,$^)
 
 x86_64.h: ../arch-x86/xen-x86_64.h ../arch-x86/xen.h ../xen.h $(scripts)
-	python mkheader.py $* $@ $(filter %.h,$^)
+	python2 mkheader.py $* $@ $(filter %.h,$^)
 
 ia64.h: ../arch-ia64.h ../xen.h $(scripts)
-	python mkheader.py $* $@ $(filter %.h,$^)
+	python2 mkheader.py $* $@ $(filter %.h,$^)
 
 checker.c: $(scripts)
-	python mkchecker.py $(XEN_TARGET_ARCH) $@ $(architectures)
+	python2 mkchecker.py $(XEN_TARGET_ARCH) $@ $(architectures)

bsd/sys/xen/interface/foreign/mkchecker.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import sys;
 from structs import structs;

bsd/sys/xen/interface/foreign/mkheader.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import sys, re;
 from structs import unions, structs, defines;

scripts/imgedit.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import sys, os, optparse, struct
 

scripts/mkbootfs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import os, sys, struct, optparse, StringIO, ConfigParser
 

scripts/mkzfs.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+#!/usr/bin/python2
 
 import os, sys, struct, optparse, StringIO, ConfigParser, subprocess, shutil, socket, time, threading
 

scripts/run.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 import subprocess
 import sys
 import argparse

commit 3337580d01acf576455b0754935f0d638036e75a

Fix setup_libstdcxx() in scripts/loader.py
Fix the following breakage caused by commit c98b928 ("Explicitly use python
version number") when starting GDB:

  Traceback (most recent call last):
    File "/home/penberg/osv/scripts/loader.py", line 833, in <module>
      setup_libstdcxx()
    File "/home/penberg/osv/scripts/loader.py", line 496, in setup_libstdcxx

Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

scripts/loader.py
@@ -493,7 +493,7 @@ gdb.events.cont.connect(continue_handler)
 def setup_libstdcxx():
     gcc = external + '/gcc.bin'
     sys.path += [gcc + '/usr/share/gdb/auto-load/usr/lib64',
-                 glob(gcc + '/usr/share/gcc-*/python2')[0],
+                 glob(gcc + '/usr/share/gcc-*/python')[0],
                  ]
     main = glob(gcc + '/usr/share/gdb/auto-load/usr/lib64/libstdc++.so.*.py')[0]
     execfile(main)