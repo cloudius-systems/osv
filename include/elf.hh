c02d5388e65e58fac5b9dc8404c7cb20a4979ef8
dleifker@gmail.com <dleifker@gmail.com>
2013-11-10 05:53:36 -0800

    elf: Ignore PT_PAX_FLAGS
    Fixed the following abort when build was running mkzfs.py.
    
    Done!
    eth0: ethernet address: 52:54:0:12:34:56
    [I/20 dhcp]: Waiting for IP...
    [I/26 dhcp]: Server acknowledged IP for interface eth0
    [I/26 dhcp]: Configuring eth0: ip 192.168.122.15 subnet mask 255.255.255.0
    gateway 192.168.122.1
    Aborted
    
    Signed-off-by: David Leifker <dlei...@gmail.com>
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- include/elf.hh
+++ include/elf.hh
@@ -91,6 +91,7 @@ enum {
     PT_GNU_EH_FRAME = 0x6474e550, // Exception handling records
     PT_GNU_STACK = 0x6474e551, // Stack permissions record
     PT_GNU_RELRO = 0x6474e552, // Read-only after relocations
+    PT_PAX_FLAGS = 0x65041580,
 };
 
 struct Elf64_Phdr {
