6bad4121d781e427c6983ef81dda6ca28c13119e
Pekka Enberg <penberg@cloudius-systems.com>
2013-11-29 14:14:45 +0200

    test.py: Stop test execution on error
    Add error scanning to test.py based on known patterns in stdout and
    stderr and stop test execution on error to make broken test cases stand
    out.
    
    Example output looks as follows:
    
      PASS: link returns ENOENT if source path does not exists
      PASS: create a file
      PASS: close the file
      PASS: create a hard link
      PASS: link returns EEXIST if destination path exists
      PASS: read content from another link
      PASS: stat the file
      PASS: hard link count is increased
      PASS: stat the hard link
      PASS: stat device IDs match
      PASS: stat inode numbers match
      PASS: remove the hard link
      PASS: stat the file
      FAIL: hard link count is decreased
      PASS: remove the file
      SUMMARY: 15 tests, 1 failures
      Powering off.
    
      Test '/tests/tst-fs-link.so' FAILED
      make: *** [check] Error 1
    
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- scripts/test.py
+++ scripts/test.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python2
 import subprocess
+import sys
+import re
 
 tests = [
     "/tests/tst-except.so",
@@ -15,14 +17,42 @@ tests = [
     "/tests/tst-fs-link.so",
 ]
 
+def scan_errors(s):
+    if not s:
+        return False
+    patterns = [
+        "failures detected in test suite",
+        "Assertion failed",
+        "FAIL"
+    ]
+    for pattern in patterns:
+        if re.findall(pattern, s):
+            return True
+    return False
+
 def run_test(name):
     print("Running '%s'..." % name)
     args = ["-g", "-e", name]
-    subprocess.call(["./scripts/run.py"] + args)
+    process = subprocess.Popen(["./scripts/run.py"] + args, stdout=subprocess.PIPE)
+    out = ""
+    while True:
+        ch = process.stdout.read(1)
+        if ch == '' and process.poll() != None:
+            break
+        out += ch
+        if ch != '':
+            sys.stdout.write(ch)
+            sys.stdout.flush()
+
+    if scan_errors(out) or process.returncode:
+        print("Test '%s' FAILED" % name)
+        exit(1)
 
 def run_tests():
     for test in tests:
         run_test(test)
+
+    print("OK (%d tests run)" % (len(tests)))
 
 def main():
     run_tests()
