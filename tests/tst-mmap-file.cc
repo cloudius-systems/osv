c58c79d68228e260d27a129d5964b651f0d1a319
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2013-12-03 14:05:50 -0200

    tests: Add new tests into tst-mmap-file.so
    The main purpose is to exercise the new parameter checks added to
    mmap to handle wrong combination of parameters.
    Let's intentionally force errors and catch them with the respective errno codes.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- tests/tst-mmap-file.cc
+++ tests/tst-mmap-file.cc
@@ -13,6 +13,7 @@
 #include <unistd.h>
 #include <fcntl.h>
 #include <stdio.h>
+#include <errno.h>
 
 static int tests = 0, fails = 0;
 
@@ -58,6 +59,28 @@ static int verify_pattern(int fd, size_t size, unsigned char pattern, int mmap_f
     return 0;
 }
 
+static int check_mapping(void *addr, size_t size, unsigned flags, int fd,
+                         size_t offset, int expected_errno)
+{
+    void *ret = mmap(addr, size, PROT_READ|PROT_WRITE, flags, fd, offset);
+
+    if (ret == MAP_FAILED && errno == expected_errno) {
+        return 0;
+    }
+
+    if (ret != addr && ret == MAP_FAILED) {
+        perror("mmap");
+        return -1;
+    }
+
+    if (munmap(ret, size) < 0) {
+        perror("munmap");
+        return -1;
+    }
+
+    return 0;
+}
+
 int main(int argc, char *argv[])
 {
     auto fd = open("/tmp/mmap-file-test", O_CREAT|O_TRUNC|O_RDWR, 0666);
@@ -71,7 +94,45 @@ int main(int argc, char *argv[])
     report(write_pattern(fd, size/2, 0x0f, MAP_SHARED, size/2) == 0, "write pattern to partial MAP_SHARED");
     report(verify_pattern(fd, size/2, 0xfe, MAP_PRIVATE, 0) == 0, "verify pattern didn't change in unmapped part");
     report(verify_pattern(fd, size/2, 0x0f, MAP_PRIVATE, size/2) == 0, "verify pattern changed in mapped part");
+
+    report(check_mapping(NULL, size, MAP_SHARED | MAP_PRIVATE, fd, 0, EINVAL) == 0,
+        "force EINVAL by not passing neither MAP_PRIVATE nor MAP_SHARED.");
+    report(check_mapping(NULL, size, 0, fd, 0, EINVAL) == 0,
+        "force EINVAL by passing both MAP_PRIVATE and MAP_SHARED.");
+    // if MAP_ANONYMOUS isn't set, then we must check the validity of the fd.
+    report(check_mapping(NULL, size, MAP_SHARED, -1, 0, EBADF) == 0,
+        "force EBADF by passing an invalid file descriptor.");
+    report(check_mapping(NULL, size, MAP_SHARED, fd, 4095, EINVAL) == 0,
+        "force EINVAL by passing an unaligned offset.");
+    report(check_mapping(NULL, 0, MAP_SHARED, fd, 0, EINVAL) == 0,
+        "force EINVAL by passing length equals to zero.");
+
+    constexpr void *unaligned_addr = reinterpret_cast<void*>(0xefff1001); // unaligned 4k-sized page.
+    constexpr void *aligned_addr   = reinterpret_cast<void*>(0xefff1000); // aligned 4k-sized page.
+    // if MAP_FIXED was specified, then mmap should return EINVAL to unaligned addresses.
+    report(check_mapping(unaligned_addr, size, MAP_SHARED | MAP_FIXED, fd, 0, EINVAL) == 0,
+        "force EINVAL by passing an unaligned addr and the flag MAP_FIXED.");
+    // if MAP_FIXED was specified and an aligned addr was passed, then mmap should proceed correctly.
+    report(check_mapping(aligned_addr, size, MAP_SHARED | MAP_FIXED, fd, 0, 0) == 0,
+        "passed an aligned addr and MAP_FIXED, then mmap should return exactly the specified addr.");
+
     report(close(fd) == 0, "close");
+
+    fd = open("/tmp/mmap-file-test", O_WRONLY);
+    report(fd > 0, "open file again: O_WRONLY");
+    report(check_mapping(NULL, size, MAP_PRIVATE, fd, 0, EACCES) == 0,
+        "force EACCES by private mapping a file that is not opened for reading");
+    report(close(fd) == 0, "close again");
+
+    fd = open("/tmp/mmap-file-test", O_WRONLY);
+    report(fd > 0, "open file again: O_WRONLY");
+    report(check_mapping(NULL, size, MAP_SHARED, fd, 0, EACCES) == 0,
+        "force EACCES by shared mapping a file that is not read-write (and PROT_WRITE is set)");
+    report(close(fd) == 0, "close again");
+
+    // TODO: map an append-only file with prot asking for PROT_WRITE, mmap should return EACCES.
+    // TODO: map a file under a fs mounted with the flag NO_EXEC and prot asked for PROT_EXEC (expect EPERM).
+
     printf("SUMMARY: %d tests, %d failures\n", tests, fails);
     return 0;
 }
