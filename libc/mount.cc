2050ce8c74c06872deac488f76a7bec7661edb81
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2013-12-06 14:41:13 -0200

    libc/mount: Change umount2 and add umount
    umount2 should call sys_umount2 instead.  Add umount that calls sys_umount.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- libc/mount.cc
+++ libc/mount.cc
@@ -3,11 +3,21 @@
 #include <sys/mount.h>
 #include <fs/vfs/vfs.h>
 
-int umount2(const char *path, int flags)
+int umount(const char *path)
 {
     auto r = sys_umount(path);
     if (r == 0) {
-	return 0;
+        return 0;
+    } else {
+        return libc_error(r);
+    }
+}
+
+int umount2(const char *path, int flags)
+{
+    auto r = sys_umount2(path, flags);
+    if (r == 0) {
+        return 0;
     } else {
         return libc_error(r);
     }
