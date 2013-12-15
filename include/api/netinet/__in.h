4c5a10d5b0926fe07b79b6bfb13fac1afc30c7ab
Avi Kivity <avi@cloudius-systems.com>
2013-12-15 20:39:49 +0200

    net: reconcile bsd and api IPPROTO_* macros
    Move them to a common include file.
    
    Since they're defined externally, there is no real conflict.
    
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- include/api/netinet/__in.h
+++ include/api/netinet/__in.h
@@ -9,6 +9,37 @@
 #ifndef OSV__IN_H_
 #define OSV__IN_H_
 
+#define IPPROTO_IP       0
+#define IPPROTO_HOPOPTS  0
+#define IPPROTO_ICMP     1
+#define IPPROTO_IGMP     2
+#define IPPROTO_IPIP     4
+#define IPPROTO_TCP      6
+#define IPPROTO_EGP      8
+#define IPPROTO_PUP      12
+#define IPPROTO_UDP      17
+#define IPPROTO_IDP      22
+#define IPPROTO_TP       29
+#define IPPROTO_DCCP     33
+#define IPPROTO_IPV6     41
+#define IPPROTO_ROUTING  43
+#define IPPROTO_FRAGMENT 44
+#define IPPROTO_RSVP     46
+#define IPPROTO_GRE      47
+#define IPPROTO_ESP      50
+#define IPPROTO_AH       51
+#define IPPROTO_ICMPV6   58
+#define IPPROTO_NONE     59
+#define IPPROTO_DSTOPTS  60
+#define IPPROTO_MTP      92
+#define IPPROTO_ENCAP    98
+#define IPPROTO_PIM      103
+#define IPPROTO_COMP     108
+#define IPPROTO_SCTP     132
+#define IPPROTO_UDPLITE  136
+#define IPPROTO_RAW      255
+#define IPPROTO_MAX      256
+
 struct ip_mreq {
         struct  in_addr imr_multiaddr;  /* IP multicast address of group */
         struct  in_addr imr_interface;  /* local IP address of interface */
--- include/api/netinet/in.h
+++ include/api/netinet/in.h
@@ -75,37 +75,6 @@ uint32_t ntohl(uint32_t);
 uint16_t ntohs(uint16_t);
 #endif
 
-#define IPPROTO_IP       0
-#define IPPROTO_HOPOPTS  0
-#define IPPROTO_ICMP     1
-#define IPPROTO_IGMP     2
-#define IPPROTO_IPIP     4
-#define IPPROTO_TCP      6
-#define IPPROTO_EGP      8
-#define IPPROTO_PUP      12
-#define IPPROTO_UDP      17
-#define IPPROTO_IDP      22
-#define IPPROTO_TP       29
-#define IPPROTO_DCCP     33
-#define IPPROTO_IPV6     41
-#define IPPROTO_ROUTING  43
-#define IPPROTO_FRAGMENT 44
-#define IPPROTO_RSVP     46
-#define IPPROTO_GRE      47
-#define IPPROTO_ESP      50
-#define IPPROTO_AH       51
-#define IPPROTO_ICMPV6   58
-#define IPPROTO_NONE     59
-#define IPPROTO_DSTOPTS  60
-#define IPPROTO_MTP      92
-#define IPPROTO_ENCAP    98
-#define IPPROTO_PIM      103
-#define IPPROTO_COMP     108
-#define IPPROTO_SCTP     132
-#define IPPROTO_UDPLITE  136
-#define IPPROTO_RAW      255
-#define IPPROTO_MAX      256
-
 #define IN6_IS_ADDR_UNSPECIFIED(a) \
         (((uint32_t *) (a))[0] == 0 && ((uint32_t *) (a))[1] == 0 && \
          ((uint32_t *) (a))[2] == 0 && ((uint32_t *) (a))[3] == 0)
