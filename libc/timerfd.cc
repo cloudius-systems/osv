cb19cf98155a4509ffda11d07c7be1ee56c425c5
Nadav Har'El <nyh@cloudius-systems.com>
2014-01-07 23:42:05 +0200

    Refactor timerfd.cc
    In his review of timerfd.cc, Avi asked that I simplify the implementation
    by having a single "timerfd" object (instead of two I had - timerfd_file
    and timerfd_object), and by using a single mutex instead of the complex
    combination of mutexes and atomic variable.
    
    This new version indeed does this. It should be easier to understand this
    code, and it is 30 lines shorter.
    
    The performance of this code is slightly inferior to the previous one -
    in particular poll() now locks and unlocks a mutex - but this should be
    negligible in practice.
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- libc/timerfd.cc
+++ libc/timerfd.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Cloudius Systems, Ltd.
+ * Copyright (C) 2013-2014 Cloudius Systems, Ltd.
  *
  * This work is open source software, licensed under the terms of the
  * BSD license as described in the LICENSE file in the top-level directory.
@@ -19,13 +19,19 @@
 
 #include <atomic>
 
-class timerfd_object {
+class timerfd final : public special_file {
+public:
+    explicit timerfd(int clockid, int flags);
+    virtual ~timerfd() override;
+
+    virtual int read(uio* data, int flags) override;
+    virtual int poll(int events) override;
+    virtual int close() override;
+    void set(s64 expiration, s64 interval);
+    void get(s64 &expiration, s64 &interval) const;
+
 private:
-    // Whom to wake when the timerfd becomes readable. This is a thread
-    // waiting on read(), or a null thread if nobody is read()ing.
-    sched::thread_handle &_h;
-    // Which fd's poll_wake() to call when the timerfd becomes readable.
-    file *_f;
+    mutable mutex _mutex;
 
     // _expiration is the time when the timerfd becomes readable and read()
     // will return 1. After this time, the value to be returned by read()
@@ -33,77 +39,72 @@ private:
     s64 _expiration = 0;
     s64 _interval = 0;
 
-    // Whether _expiration has already passed, we woke _h and _f already,
-    // and new readers or pollers will not block.
-    std::atomic<bool> _expired {false};
-
-    // Each timerfd keeps a timer for wakeup of _h and _f (as explained above)
+    // Each timerfd keeps a timer for wakeup of sleeping read() or poll()
     // in a dedicated thread. We could have used a timer_base::client instead
     // of a real thread, but things get really complicated when trying to
     // support set() which cancels on one CPU the timer set on another CPU.
     sched::thread _wakeup_thread;
-    mutex _mutex;
     s64 _wakeup_due = 0;
     condvar _wakeup_change_cond;
     bool _wakeup_thread_exit = false;
-    void wakeup_thread_func()
-    {
-        sched::timer tmr(*sched::thread::current());
-        while (true) {
-            WITH_LOCK(_mutex) {
-                if (_wakeup_thread_exit) {
-                    return;
-                }
-                if (_wakeup_due != 0) {
-                    tmr.set(_wakeup_due);
-                    _wakeup_change_cond.wait(_mutex, &tmr);
-                    if (tmr.expired()) {
-                        _expired.store(true, std::memory_order_relaxed);
-                        _wakeup_due = 0;
-                        // Possibly wake one ongoing read()
-                        _h.wake();
-                        // Or wake anybody polling on the fd containing this
-                        poll_wake(_f, POLLIN);
-                    } else {
-                        tmr.cancel();
-                    }
+    condvar _blocked_reader;
+    void wakeup_thread_func();
+};
+
+timerfd::timerfd(int clockid, int oflags)
+    : special_file(FREAD | oflags, DTYPE_UNSPEC),
+      _wakeup_thread(
+            [&] { wakeup_thread_func(); }, sched::thread::attr().stack(4096))
+{
+    _wakeup_thread.start();
+}
+
+timerfd::~timerfd() {
+    WITH_LOCK(_mutex) {
+        _wakeup_thread_exit = true;
+        _wakeup_change_cond.wake_one();
+    }
+    _wakeup_thread.join();
+}
+
+void timerfd::wakeup_thread_func()
+{
+    sched::timer tmr(*sched::thread::current());
+    WITH_LOCK(_mutex) {
+        while (!_wakeup_thread_exit) {
+            if (_wakeup_due != 0) {
+                tmr.set(_wakeup_due);
+                _wakeup_change_cond.wait(_mutex, &tmr);
+                if (tmr.expired()) {
+                    _wakeup_due = 0;
+                    // Wake blocked read() or poll() on this fd
+                    _blocked_reader.wake_one();
+                    poll_wake(this, POLLIN);
                 } else {
-                    _wakeup_change_cond.wait(_mutex);
+                    tmr.cancel();
                 }
+            } else {
+                _wakeup_change_cond.wait(_mutex);
             }
         }
     }
+}
 
-
-public:
-    timerfd_object(sched::thread_handle &h, file *f)
-        : _h(h), _f(f),  _wakeup_thread(
-                [&] { wakeup_thread_func(); }, sched::thread::attr().stack(4096))
-    {
-        _wakeup_thread.start();
+void timerfd::set(s64 expiration, s64 interval)
+{
+    WITH_LOCK(_mutex) {
+        _expiration = expiration;
+        _interval = interval;
+        _wakeup_due = expiration;
+        _wakeup_change_cond.wake_one();
+        _blocked_reader.wake_one();
     }
+}
 
-    ~timerfd_object() {
-        // Ask the wakeup_thread_func to end, so it can be joined without hang
-        WITH_LOCK(_mutex) {
-            _wakeup_thread_exit = true;
-            _wakeup_change_cond.wake_one();
-        }
-        _wakeup_thread.join();
-    }
-
-    void set(s64 expiration, s64 interval) {
-        WITH_LOCK(_mutex) {
-            _expired.store(false, std::memory_order_relaxed);
-            _expiration = expiration;
-            _interval = interval;
-            _wakeup_due = expiration;
-            _wakeup_change_cond.wake_one();
-        }
-    }
-
-    void get(s64 &expiration, s64 &interval) const {
-        if (is_readable()) {
+void timerfd::get(s64 &expiration, s64 &interval) const
+{
+    WITH_LOCK(_mutex) {
+        if (_expiration && !_wakeup_due) {
             // already expired, calculate the time of the next expiration
             if (!_interval) {
                 expiration = 0;
@@ -117,55 +118,6 @@ public:
         }
         interval = _interval;
     }
-
-    // Returns true if a poll on the timerfd will return POLLIN, i.e, an
-    // expiration has occurred since the last set() or read_and_zero_counter().
-    bool is_readable() const {
-        return _expired.load(std::memory_order_relaxed);
-    }
-
-    // Read the timerfd's current count of expirations since the last read()
-    // or set(). May also set a timer until is_readable() becomes true again.
-    u64 read_and_zero_counter() {
-        if (!_expired.load(std::memory_order_relaxed)) {
-            return 0;
-        }
-        WITH_LOCK(_mutex) {
-            // After a read, is_readable() no longer true until next timer.
-            _expired.store(false, std::memory_order_relaxed);
-            if (!_interval) {
-                return 1;
-            }
-            auto now = nanotime();
-            // set next wakeup for the next multiple of interval from
-            // _expiration which is after "now".
-            assert (now >= _expiration);
-            u64 count = (now - _expiration) / _interval;
-            _expiration = _expiration + (count+1) * _interval;
-            _wakeup_due = _expiration;
-            _wakeup_change_cond.wake_one();
-            return 1 + count;
-        }
-    }
-};
-
-
-class timerfd_file final : public special_file {
-public:
-    explicit timerfd_file(int clockid, int flags);
-    virtual ~timerfd_file() {}
-    virtual int read(uio* data, int flags) override;
-    virtual int poll(int events) override;
-    virtual int close() override;
-
-    sched::thread_handle _h;
-    timerfd_object _obj;
-    mutex _read_mutex;
-};
-
-timerfd_file::timerfd_file(int clockid, int oflags)
-    : special_file(FREAD | oflags, DTYPE_UNSPEC), _h(), _obj(_h, this)
-{
 }
 
 // Copy from the byte array into the given iovec array, stopping when either
@@ -183,33 +135,55 @@ static void copy_to_uio(const char *q, size_t qlen, uio *uio)
     }
 }
 
-int timerfd_file::read(uio *data, int flags)
+int timerfd::read(uio *data, int flags)
 {
-    WITH_LOCK(_read_mutex) {
-        if (data->uio_resid < (ssize_t) sizeof(decltype(_obj.read_and_zero_counter()))) {
-            return EINVAL;
-        }
-        if (!_obj.is_readable()) {
+    u64 ret;
+
+    if (data->uio_resid < (ssize_t) sizeof(ret)) {
+        return EINVAL;
+    }
+
+    WITH_LOCK(_mutex) {
+        while (!_expiration || _wakeup_due) {
             if (f_flags & O_NONBLOCK) {
                 return EAGAIN;
             }
-            // Wait until the timer expires
-            _h.reset(*sched::thread::current());
-            sched::thread::wait_until([&] { return _obj.is_readable(); });
-            _h.clear();
+            _blocked_reader.wait(_mutex);
         }
-        auto c = _obj.read_and_zero_counter();
-        copy_to_uio((const char *)&c, sizeof(c), data);
+        // Read the timerfd's current count of expirations since the last
+        // read() or set(). If an interval is set, also set a timer until
+        // the next expiration.
+        if (!_interval) {
+            ret = 1;
+            _expiration = 0;
+        } else {
+            auto now = nanotime();
+            // set next wakeup for the next multiple of interval from
+            // _expiration which is after "now".
+            assert (now >= _expiration);
+            u64 count = (now - _expiration) / _interval;
+            _expiration = _expiration + (count+1) * _interval;
+            _wakeup_due = _expiration;
+            _wakeup_change_cond.wake_one();
+            ret = 1 + count;
+        }
+        copy_to_uio((const char *)&ret, sizeof(ret), data);
         return 0;
     }
 }
 
-int timerfd_file::poll(int events)
+int timerfd::poll(int events)
 {
-    return _obj.is_readable() ? POLLIN : 0;
+    WITH_LOCK(_mutex) {
+        if (!_expiration || _wakeup_due) {
+            return 0;
+        } else {
+            return POLLIN;
+        }
+    }
 }
 
-int timerfd_file::close()
+int timerfd::close()
 {
     // No need to do anything explicit - close will delete this object,
     // causing all its fields to be properly destructed.
@@ -235,7 +209,7 @@ int timerfd_create(int clockid, int flags) {
     }
     try {
         int oflags = (flags & TFD_NONBLOCK) ? O_NONBLOCK : 0;
-        fileref f = make_file<timerfd_file>(clockid, oflags);
+        fileref f = make_file<timerfd>(clockid, oflags);
         fdesc fd(f);
         return fd.release();
     } catch (int error) {
@@ -251,7 +225,7 @@ int timerfd_settime(int fd, int flags, const itimerspec *newval,
         errno = EBADF;
         return -1;
     }
-    auto tf = dynamic_cast<timerfd_file*>(f.get());
+    auto tf = dynamic_cast<timerfd*>(f.get());
     if (!tf) {
         errno = EINVAL;
         return -1;
@@ -260,7 +234,7 @@ int timerfd_settime(int fd, int flags, const itimerspec *newval,
     s64 expiration, interval;
     s64 now = nanotime();
     if (oldval) {
-        tf->_obj.get(expiration, interval);
+        tf->get(expiration, interval);
         if (expiration) {
             // oldval is always returned in relative time
             expiration -= now;
@@ -277,7 +251,7 @@ int timerfd_settime(int fd, int flags, const itimerspec *newval,
         expiration += now;
     }
 
-    tf->_obj.set(expiration, interval);
+    tf->set(expiration, interval);
     return 0;
 }
 
@@ -288,7 +262,7 @@ int timerfd_gettime(int fd, itimerspec *val)
         errno = EBADF;
         return -1;
     }
-    auto tf = dynamic_cast<timerfd_file*>(f.get());
+    auto tf = dynamic_cast<timerfd*>(f.get());
     if (!tf) {
         errno = EINVAL;
         return -1;
@@ -296,7 +270,7 @@ int timerfd_gettime(int fd, itimerspec *val)
 
     s64 expiration, interval;
     s64 now = nanotime();
-    tf->_obj.get(expiration, interval);
+    tf->get(expiration, interval);
     if (expiration) {
         // timerfd_gettime() wants relative time
         expiration -= now;
