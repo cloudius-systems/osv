84f139e31078c046c60bf091949aaa3550c0cf0a
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2014-01-13 13:08:21 +0100

    trace: support persisting trace stream to file
    Having traces persisted in a file has the follwing advantages:
    
     - trace collection is decoupled from trace analysis. trace analysis
       tools no longer have to be part of GDB scripts. This is important
       for EC2 instances, to which we cannot connect with GDB. In this
       case trace data can be downloaded using other channels, but tools
       remain unchanged.
    
     - trace analysis doesn't have to be performed in the same OSv session.
       After trace is collected OSv can be shut down or left running without
       further disturbance.
    
    The trace is saved in a binary format of the following structure:
    
      <format-version>
      <N_tp>
      <tracepoint 1>
      ...
      <tracepoint N_tp>
      <trace 1>
      <trace 2>
      ...
      <trace N_t>
    
    Where:
    
      <tracepoint X> = <tp_key><name><signature><format>
      <trace X> = <tp_key><thread><time><cpu>(<backtrace_addr>*)<0><data>
    
    Signed-off-by: Tomasz Grabiec <tgrabiec@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- scripts/osv/trace.py
+++ scripts/osv/trace.py
@@ -3,6 +3,9 @@ import mmap
 import struct
 import sys
 
+
+_format_version = 1
+
 def nanos_to_millis(nanos):
     return float(nanos) / 1000000
 
@@ -72,3 +75,112 @@ class Trace:
 
     def __str__(self):
         return self.format()
+
+
+class SlidingUnpacker:
+    def __init__(self, buffer):
+        self.buffer = buffer
+        self.offset = 0
+
+    def unpack_str(self):
+        len, = struct.unpack_from('H', self.buffer, offset=self.offset)
+        self.offset += struct.calcsize('H')
+        string = ''.join(struct.unpack_from('c' * len, self.buffer, offset=self.offset))
+        self.offset += len
+        return string
+
+    def unpack(self, format):
+        values = struct.unpack_from(format, self.buffer, offset=self.offset)
+        self.offset += struct.calcsize(format)
+        return values
+
+    def __nonzero__(self):
+        return self.offset < len(self.buffer)
+
+class WritingPacker:
+    def __init__(self, writer):
+        self.writer = writer
+        self.offset = 0
+
+    def pack(self, format, *data):
+        self.writer(struct.pack(format, *data))
+        self.offset += struct.calcsize(format)
+
+    def pack_str(self, *args):
+        for arg in args:
+            if not type(arg) == str:
+                raise Exception('Should be string but is %s' % type(arg))
+
+            count = len(arg)
+            self.writer(struct.pack('H', count))
+            self.writer(struct.pack('c' * count, *arg))
+
+def read(buffer_view):
+    unpacker = SlidingUnpacker(buffer_view)
+    version, = unpacker.unpack('i')
+
+    if version != _format_version:
+        raise Exception('Version mismatch, current is %d got %d' % (_format_version, version))
+
+    tracepoints = {}
+    n_tracepoints, = unpacker.unpack('Q')
+    for i in range(n_tracepoints):
+        key, = unpacker.unpack('Q')
+        tracepoints[key] = TracePoint(key, unpacker.unpack_str(),
+            unpacker.unpack_str(), unpacker.unpack_str())
+
+    while unpacker:
+        tp_key, thread, time, cpu = unpacker.unpack('QQQI')
+        tp = tracepoints[tp_key]
+
+        backtrace = []
+        while True:
+            frame, = unpacker.unpack('Q')
+            if not frame:
+                break
+            backtrace.append(frame)
+
+        data = unpacker.unpack(tp.signature)
+        yield Trace(tp, thread, time, cpu, data, backtrace=backtrace)
+
+def write(traces, writer):
+    packer = WritingPacker(writer)
+    packer.pack('i', _format_version)
+
+    tracepoints = set(trace.tp for trace in traces)
+    packer.pack('Q', len(tracepoints))
+    for tp in tracepoints:
+        packer.pack('Q', tp.key)
+        packer.pack_str(tp.name, tp.signature, tp.format)
+
+    for trace in traces:
+        packer.pack('QQQI', trace.tp.key, trace.thread, trace.time, trace.cpu)
+
+        if trace.backtrace:
+            for frame in filter(None, trace.backtrace):
+                packer.pack('Q', frame)
+        packer.pack('Q', 0)
+
+        packer.pack(trace.tp.signature, *trace.data)
+
+class read_file:
+    def __init__(self, filename):
+        self.filename = filename
+        self.map = None
+
+    def __enter__(self):
+        self.file = open(self.filename, 'r+b')
+        self.map = mmap.mmap(self.file.fileno(), 0)
+        return self
+
+    def __exit__(self, *args):
+        if self.map:
+            self.map.close()
+        self.file.close()
+
+    def get_traces(self):
+        return read(self.map)
+
+def write_to_file(filename, traces):
+    with open(filename, 'wb') as file:
+        return write(traces, file.write)
