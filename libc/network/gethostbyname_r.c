f5f0501df639b8b828bde6d2691d897991914aa4
Vlad Zolotarov <vladz@cloudius-systems.com>
2014-01-19 14:14:20 +0200

    libc: added gethostbyname()
    Reviewed-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- libc/network/gethostbyname_r.c
+++ libc/network/gethostbyname_r.c
@@ -2,10 +2,35 @@
 
 #include <sys/socket.h>
 #include <netdb.h>
+#include <errno.h>
+#include <assert.h>
+#include <stdlib.h>
 
 int gethostbyname_r(const char *name,
 	struct hostent *h, char *buf, size_t buflen,
 	struct hostent **res, int *err)
 {
 	return gethostbyname2_r(name, AF_INET, h, buf, buflen, res, err);
+}
+
+
+struct hostent *gethostbyname(const char *name)
+{
+    /* We estimate that this should be enough for the sane host names */
+#define GETHOSTBYNAME_TMP_BUF_LEN  256
+    static struct hostent ghbn_ret_buf;
+    static char tmp_buf[GETHOSTBYNAME_TMP_BUF_LEN];
+
+    struct hostent *tmp;
+    int err, rc;
+
+    rc = gethostbyname_r(name, &ghbn_ret_buf, tmp_buf, sizeof(tmp_buf),
+                         &tmp, &err);
+    assert(rc != ERANGE);
+
+    if (rc) {
+        return NULL;
+    } else {
+        return tmp;
+    }
 }
