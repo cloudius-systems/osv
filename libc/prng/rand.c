4b29d7f7e1ed39b97ed517345aee490127ff8bcc
Vlad Zolotarov <vladz@cloudius-systems.com>
2014-01-19 14:14:22 +0200

    libc: added rand_r()
    Reviewed-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- libc/prng/rand.c
+++ libc/prng/rand.c
@@ -7,7 +7,17 @@ void srand(unsigned s)
 	seed = s-1;
 }
 
+static inline int _rand_r(unsigned int *seedp)
+{
+	return (*seedp = (*seedp + 1) * 1103515245 + 12345 - 1) + 1 & 0x7fffffff;
+}
+
+int rand_r(unsigned int *seedp)
+{
+    return _rand_r(seedp);
+}
+
 int rand(void)
 {
-	return (seed = (seed+1) * 1103515245 + 12345 - 1)+1 & 0x7fffffff;
+	return _rand_r(&seed);
 }
