9950d3f18b1cca29d75e6036511584171bc1451d
Pekka Enberg <penberg@cloudius-systems.com>
2014-01-20 14:01:19 +0200

    java: Disable JVM balloon
    Dor Laor writes:
    
      - Ballooning causes to issues and we like to disable it:
    
        1. Parallel GC
            2 GC threads are trying to move memory around and they
            do it optimistically without locking. This fools our memmov logic.
            Gleb and Glauber have a solution (to use GCmonitor) but will take
            time to implement.
    
        2. Logger clash
            The existing GCmonitor bean access the logger early and we don't
            have the tomcat logger handy. Tomek is working on a solution but
            it will take few days to complete.
    
    Therefore, disable the JVM balloon for v0.05.
    
    Acked-by: Glauber Costa <glommer@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- java/java.cc
+++ java/java.cc
@@ -109,6 +109,7 @@ int main(int argc, char **argv)
         }
     }
 
+#if 0
     if (!has_xmx) {
         // FIXME: We should estimate how much memory the JVM itself is going to use
         auto heap_size = memory::stats::free() >> 20;
@@ -118,6 +119,7 @@ int main(int argc, char **argv)
         }
         debugf("Autotuning JVM heap size to %d MB\n", heap_size);
     }
+#endif
 
     vm_args.nOptions = options.size();
     vm_args.options = options.data();
@@ -157,11 +159,13 @@ int main(int argc, char **argv)
         env->SetObjectArrayElement(args, index++, env->NewStringUTF(argv[i]));
     }
 
+#if 0
     // Manually setting the heap size is viewed as a declaration of intent. In
     // that case, we'll leave the user alone. This may be revisited in the
     // future, but it is certainly the safest option.
     std::unique_ptr<jvm_balloon_shrinker>
         balloon(has_xmx ? nullptr : new jvm_balloon_shrinker(jvm));
+#endif
 
     env->CallStaticVoidMethod(mainclass, mainmethod, args);
 
