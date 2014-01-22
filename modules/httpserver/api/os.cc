2257350d7de0f83293589c1931fb9f1c57b223dd
Amnon Heiman <amnon@cloudius-systems.com>
2014-01-22 13:06:59 +0200

    Http Server: Implement the os api
    The swagger os.json api defines a set of os related operations.
    The self explenatory list that will be supported is:
    
    The following would work with GET:
    http://192.168.122.89:8000/os/manufacturer
    http://192.168.122.89:8000/os/uptime
    http://192.168.122.89:8000/os/date
    http://192.168.122.89:8000/os/memory/total
    http://192.168.122.89:8000/os/memory/free
    
    The following would work with POST
    http://192.168.122.89:8000/os/shutdown
    
    Signed-off-by: Amnon Heiman <amnon@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- modules/httpserver/api/os.cc
+++ modules/httpserver/api/os.cc
@@ -10,6 +10,9 @@
 #include "osv/version.hh"
 #include "json/formatter.hh"
 #include "autogen/os.json.hh"
+#include <sys/sysinfo.h>
+#include <time.h>
+#include <osv/shutdown.hh>
 
 namespace httpserver {
 
@@ -26,10 +29,56 @@ void init(routes& routes)
 
     function_handler* os_version = new function_handler([](const_req req)
     {
-        return json::formatter::to_json(osv::version());
+        return formatter::to_json(osv::version());
     }, "json");
-
     routes.add_path("getOSversion", os_version);
+
+    function_handler* manufacturer = new function_handler([](const_req req)
+    {
+        return formatter::to_json("cloudius-systems");
+    }, "json");
+    routes.add_path("getOSmanufacturer", manufacturer);
+
+    function_handler* bootup = new function_handler([](const_req req)
+    {
+        struct sysinfo info;
+        sysinfo(&info);
+        return formatter::to_json(info.uptime);
+    }, "json");
+    routes.add_path("getLastBootUpTime", bootup);
+
+    function_handler* get_date = new function_handler([](const_req req)
+    {
+        time_t t;
+        time(&t);
+        date_time result;
+        localtime_r(&t,&result);
+        return formatter::to_json(result);
+    }, "json");
+    routes.add_path("getDate", get_date);
+
+    function_handler* total_mem = new function_handler([](const_req req)
+    {
+        struct sysinfo info;
+        sysinfo(&info);
+        return formatter::to_json(info.totalram);
+    }, "json");
+    routes.add_path("getTotalVirtualMemorySize", total_mem);
+
+    function_handler* free_mem = new function_handler([](const_req req)
+    {
+        struct sysinfo info;
+        sysinfo(&info);
+        return formatter::to_json(info.freeram);
+    }, "json");
+    routes.add_path("getFreeVirtualMemory", free_mem);
+
+    function_handler* shutdown = new function_handler([](const_req req)
+    {
+        osv::shutdown();
+        return formatter::to_json("");
+    }, "json");
+    routes.add_path("shutdown", shutdown);
 }
 
 }
