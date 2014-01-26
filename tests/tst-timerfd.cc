2c47a392544fd23249e82cc4f8e80c4589b6538b
Nadav Har'El <nyh@cloudius-systems.com>
2014-01-26 15:34:46 +0200

    clock: Test timerfd's support for monotonic clock
    In tst-timerfd, test timerfd with monotonic clock, in addition to the
    existing test with the realtime clock.
    
    This patch also changes this test to only use Linux APIs, not anything
    OSv-specific, so it can also be run on Linux.
    
    Reviewed-by: Glauber Costa <glommer@cloudius-systems.com>
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- tests/tst-timerfd.cc
+++ tests/tst-timerfd.cc
@@ -4,14 +4,14 @@
  * This work is open source software, licensed under the terms of the
  * BSD license as described in the LICENSE file in the top-level directory.
  */
+// To compile on Linux, use: g++ -g -pthread -std=c++11 tests/tst-timerfd.cc
 
 #include <sys/timerfd.h>
 #include <poll.h>
 #include <unistd.h>
-
-#include <osv/debug.hh>
-#include <drivers/clock.hh>
-#include <osv/sched.hh>
+#include <iostream>
+#include <chrono>
+#include <thread>
 
 #include <errno.h>
 int tests = 0, fails = 0;
@@ -22,10 +22,12 @@ bool do_expect(T actual, T expected, const char *actuals, const char *expecteds,
     ++tests;
     if (actual != expected) {
         fails++;
-        debug("FAIL: %s:%d:  For %s expected %s (%s), saw %s.\n", file, line, actuals, expecteds, expected, actual);
+        std::cout << "FAIL: " << file << ":" << line << ": For " << actuals
+                << " expected " << expecteds << "(" << expected << "), saw "
+                << actual << ".\n";
         return false;
     }
-    debug("OK: %s:%d.\n", file, line);
+    std::cout << "OK: " << file << ":" << line << ".\n";
     return true;
 }
 template<typename T>
@@ -34,10 +36,11 @@ bool do_expectge(T actual, T expected, const char *actuals, const char *expected
     ++tests;
     if (actual < expected) {
         fails++;
-        debug("FAIL: %s:%d:  For %s expected >=%s, saw %s.\n", file, line, actuals, expecteds, actual);
+        std::cout << "FAIL: " << file << ":" << line << ": For " << actuals
+                << " expected >=" << expecteds << ", saw " << actual << ".\n";
         return false;
     }
-    debug("OK: %s:%d.\n", file, line);
+    std::cout << "OK: " << file << ":" << line << ".\n";
     return true;
 }
 #define expect(actual, expected) do_expect(actual, expected, #actual, #expected, __FILE__, __LINE__)
@@ -51,10 +54,17 @@ bool do_expectge(T actual, T expected, const char *actuals, const char *expected
         do_expectge(var, 0, #call, "0", __FILE__, __LINE__); \
         do_expect(errno, 0, #call " errno",  "0", __FILE__, __LINE__);
 
-int main(int argc, char **argv)
+inline constexpr long long operator"" _ms(unsigned long long t)
+{
+    return t * 1000000;
+}
+
+using u64 = uint64_t;
+
+void dotest(int clockid)
 {
     int fd;
-    expect_success(fd, timerfd_create(CLOCK_REALTIME, TFD_NONBLOCK));
+    expect_success(fd, timerfd_create(clockid, TFD_NONBLOCK));
     int junk;
 
     // Before we set a timer, timerfd has no events for polling or reading
@@ -71,7 +81,7 @@ int main(int argc, char **argv)
     // If we check now, the timer should have not yet expired
     expect_errno(read(fd, buf, sizeof(buf)), EAGAIN);
     // But if we sleep for a whole second, the timer would have expired and read returns a counter of 1
-    sched::thread::sleep_until(nanotime()+1000_ms);
+    sleep(1);
     u64 counter;
     expect(read(fd, &counter, sizeof(counter)), (ssize_t)8);
     expect(counter, (u64)1);
@@ -83,10 +93,10 @@ int main(int argc, char **argv)
     itimerspec t2 {{0,200_ms},{0,300_ms}};
     expect_success(junk, timerfd_settime(fd, 0, &t2, nullptr));
     expect_errno(read(fd, buf, sizeof(buf)), EAGAIN);
-    sched::thread::sleep_until(nanotime()+400_ms);
+    usleep(400000);
     expect(poll(&pfd, 1, 0), 1);
     expect((int)pfd.revents, POLLIN);
-    sched::thread::sleep_until(nanotime()+400_ms);
+    usleep(400000);
     expect(poll(&pfd, 1, 0), 1);
     expect((int)pfd.revents, POLLIN);
     expect(read(fd, &counter, sizeof(counter)), (ssize_t)8);
@@ -95,20 +105,26 @@ int main(int argc, char **argv)
 
     // If sleep (so counter becomes nonzero again) and then cancel the
     // timer, the counter remains zero
-    sched::thread::sleep_until(nanotime()+400_ms);
+    usleep(400000);
     expect(poll(&pfd, 1, 0), 1); // counter is nonzero
     itimerspec t3 {{0,0},{0,0}};
     expect_success(junk, timerfd_settime(fd, 0, &t3, nullptr));
     expect(poll(&pfd, 1, 0), 0); // counter is back to zero
-    sched::thread::sleep_until(nanotime()+400_ms);
+    usleep(400000);
     expect(poll(&pfd, 1, 0), 0); // and timer was really canceled
 
     // Check absolute time setting
     // Set a timer to expire in 300ms from now (using absolute time)
-    itimerspec t4 {{0,0},{0,nanotime()+300_ms}};
+    itimerspec t4 {};
+    clock_gettime(clockid, &(t4.it_value));
+    t4.it_value.tv_nsec += 300_ms;
+    if (t4.it_value.tv_nsec >= 1000_ms) {
+        t4.it_value.tv_nsec -= 1000_ms;
+        t4.it_value.tv_sec++;
+    }
     expect_success(junk, timerfd_settime(fd, TFD_TIMER_ABSTIME, &t4, nullptr));
     expect_errno(read(fd, buf, sizeof(buf)), EAGAIN);
-    sched::thread::sleep_until(nanotime()+500_ms);
+    usleep(500000);
     expect(read(fd, &counter, sizeof(counter)), (ssize_t)8);
     expect(counter, (u64)1);
     expect_errno(read(fd, &counter, sizeof(counter)), EAGAIN);
@@ -117,12 +133,12 @@ int main(int argc, char **argv)
     // cases in blocking read tests below).
     itimerspec t45 {{0,0},{0,400_ms}};
     expect_success(junk, timerfd_settime(fd, 0, &t45, nullptr));
-    s64 before = nanotime();
+    auto before = std::chrono::high_resolution_clock::now();
     expect(poll(&pfd, 1, 0), 0);
     expect(poll(&pfd, 1, 100/*ms*/), 0);
     expect(poll(&pfd, 1, 1000/*ms*/), 1);
-    s64 after = nanotime();
-    expectge(after-before, (s64)300_ms);
+    auto after = std::chrono::high_resolution_clock::now();
+    expectge((int64_t)std::chrono::duration_cast<std::chrono::nanoseconds> (after-before).count(), (int64_t)300_ms);
 
     // Check close
     expect_success(junk, close(fd));
@@ -143,23 +159,22 @@ int main(int argc, char **argv)
     // Check blocking read, no interval, timer set before read blocks
     itimerspec t5 {{0,0},{0,500_ms}};
     expect_success(junk, timerfd_settime(fd, 0, &t5, nullptr));
-    before = nanotime();
+    before = std::chrono::high_resolution_clock::now();
     expect(read(fd, &counter, sizeof(counter)), (ssize_t)8);
     expect(counter, (u64)1);
-    after = nanotime();
-    expectge(after-before, (s64)400_ms);
+    after = std::chrono::high_resolution_clock::now();
+    expectge((int64_t)std::chrono::duration_cast<std::chrono::nanoseconds> (after-before).count(), (int64_t)400_ms);
 
     // Check blocking read, no interval, timer set after read blocks
-    sched::thread th2([&] {
+    std::thread th2([&] {
         u64 counter2;
-        s64 before = nanotime();
+        before = std::chrono::high_resolution_clock::now();
         expect(read(fd, &counter2, sizeof(counter2)), (ssize_t)8);
         expect(counter2, (u64)1);
-        s64 after = nanotime();
-        expectge(after-before, (s64)500_ms);
+        after = std::chrono::high_resolution_clock::now();
+        expectge((int64_t)std::chrono::duration_cast<std::chrono::nanoseconds> (after-before).count(), (int64_t)500_ms);
     });
-    th2.start();
-    sched::thread::sleep_until(nanotime()+400_ms); // read will likely block by now
+    usleep(400000);
     itimerspec t6 {{0,0},{0,200_ms}};
     expect_success(junk, timerfd_settime(fd, 0, &t6, nullptr));
     th2.join();
@@ -167,13 +182,13 @@ int main(int argc, char **argv)
     // Check blocking read, with interval
     itimerspec t7 {{0,200_ms},{0,500_ms}};
     expect_success(junk, timerfd_settime(fd, 0, &t7, nullptr));
-    before = nanotime();
+    before = std::chrono::high_resolution_clock::now();
     expect(read(fd, &counter, sizeof(counter)), (ssize_t)8);
     expect(counter, (u64)1);
     expect(read(fd, &counter, sizeof(counter)), (ssize_t)8);
     expect(counter, (u64)1);
-    after = nanotime();
-    expectge(after-before, (s64)600_ms);
+    after = std::chrono::high_resolution_clock::now();
+    expectge((int64_t)std::chrono::duration_cast<std::chrono::nanoseconds> (after-before).count(), (int64_t)600_ms);
 
     // Check timerfd_gettime():
     itimerspec t8 {{0,400_ms},{0,300_ms}};
@@ -187,7 +202,7 @@ int main(int argc, char **argv)
     expectge(t8.it_value.tv_nsec, tout.it_value.tv_nsec);
     expectge(tout.it_value.tv_nsec, t8.it_value.tv_nsec-(long)100_ms);
     // After a while but before expiration:
-    sched::thread::sleep_until(nanotime()+100_ms);
+    usleep(100000);
     expect_success(junk, timerfd_gettime(fd, &tout));
     expect(tout.it_interval.tv_sec, t8.it_interval.tv_sec);
     expect(tout.it_interval.tv_nsec, t8.it_interval.tv_nsec);
@@ -195,7 +210,7 @@ int main(int argc, char **argv)
     expectge(t8.it_value.tv_nsec-(long)100_ms, tout.it_value.tv_nsec);
     expectge(tout.it_value.tv_nsec, t8.it_value.tv_nsec-(long)200_ms);
     // After expiration, we have the interval
-    sched::thread::sleep_until(nanotime()+300_ms);
+    usleep(300000);
     expect_success(junk, timerfd_gettime(fd, &tout));
     expect(tout.it_interval.tv_sec, t8.it_interval.tv_sec);
     expect(tout.it_interval.tv_nsec, t8.it_interval.tv_nsec);
@@ -206,7 +221,7 @@ int main(int argc, char **argv)
     // Check timerfd_gettime() after expiration of a single-time timer:
     itimerspec t9 {{0,0},{0,100_ms}};
     expect_success(junk, timerfd_settime(fd, 0, &t9, nullptr));
-    sched::thread::sleep_until(nanotime()+200_ms);
+    usleep(200000);
     expect_success(junk, timerfd_gettime(fd, &tout));
     expect(tout.it_interval.tv_sec, (long)0);
     expect(tout.it_interval.tv_nsec, (long)0);
@@ -215,6 +230,12 @@ int main(int argc, char **argv)
     expect(tout.it_value.tv_nsec, (long)0);
 
 
-    debug("SUMMARY: %d tests, %d failures\n", tests, fails);
+}
+
+int main(int argc, char **argv)
+{
+    dotest(CLOCK_REALTIME);
+    dotest(CLOCK_MONOTONIC);
+    std::cout << "SUMMARY: " << tests << " tests, " << fails << " failures\n";
     return fails == 0 ? 0 : 1;
 }
