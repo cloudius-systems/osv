9ac6a0cc7e01bc7eba0b6457d924e46857556486
Avi Kivity <avi@cloudius-systems.com>
2014-02-02 12:30:59 +0200

    libc: provide pthread_setname_np()
    Useful for debugging.
    
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- libc/pthread.cc
+++ libc/pthread.cc
@@ -598,3 +598,12 @@ int pthread_kill(pthread_t thread, int sig)
 
     return EINVAL;
 }
+
+int pthread_setname_np(pthread_t p, const char* name)
+{
+    if (strlen(name) > 16) {
+        return ERANGE;
+    }
+    pthread::from_libc(p)->_thread.set_name(name);
+    return 0;
+}
