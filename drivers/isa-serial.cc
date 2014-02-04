9787aa705708cc61e9a0979c0c844c79a9ae7fc0
Takuya ASADA <syuu@cloudius-systems.com>
2014-02-05 08:22:49 +0900

    Coding style fix for isa-serial.cc
    Signed-off-by: Takuya ASADA <syuu@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- drivers/isa-serial.cc
+++ drivers/isa-serial.cc
@@ -27,7 +27,7 @@ void IsaSerialConsole::write(const char *str, size_t len)
 bool IsaSerialConsole::input_ready()
 {
     u8 lsr = pci::inb(ioport + LSR_ADDRESS);
-    return (lsr & LSR_RECEIVE_DATA_READY);
+    return lsr & LSR_RECEIVE_DATA_READY;
 }
 
 char IsaSerialConsole::readch()
