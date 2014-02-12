b16df0f917eddc5c85529dda97d1701c84661f26
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2014-02-12 14:52:45 +0100

    java: add test checking that extension classes are visible
    Signed-off-by: Tomasz Grabiec <tgrabiec@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

---
+++ java/tests-jre-extension/src/main/java/tests/SomeExtensionClass.java
@@ -0,0 +1,10 @@
+package tests;
+
+/*
+ * Copyright (C) 2014 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+public class SomeExtensionClass {
+}
