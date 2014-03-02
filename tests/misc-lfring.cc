31750ff07b8e386f7599c26b87fda59f725ca08b
Vlad Zolotarov <vladz@cloudius-systems.com>
2014-03-02 14:52:28 +0200

    tests/misc-lfring.cc: Delete the test for ring_mpsc.
    We are about to kill ring_mpsc - delete the corresponding tests.
    
    Reviewed-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Vlad Zolotarov <vladz@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- tests/misc-lfring.cc
+++ tests/misc-lfring.cc
@@ -91,91 +91,7 @@ private:
     }
 };
 
-//
-// Create 4 threads on different CPUs which perform concurrent push/pop
-// Testing mpsc ring
-//
-class test_mpsc_ring {
-public:
 
-    static const int max_random = 25;
-    static const u64 elements_to_process = 100000000;
-
-    bool run()
-    {
-        assert (sched::cpus.size() >= 4);
-
-        sched::thread * thread1 = new sched::thread([&] { thread_push(0); },
-            sched::thread::attr().pin(sched::cpus[0]));
-        sched::thread * thread2 = new sched::thread([&] { thread_push(1); },
-            sched::thread::attr().pin(sched::cpus[1]));
-        sched::thread * thread3 = new sched::thread([&] { thread_push(2); },
-            sched::thread::attr().pin(sched::cpus[2]));
-        sched::thread * thread4 = new sched::thread([&] { thread_pop(3); },
-            sched::thread::attr().pin(sched::cpus[3]));
-
-        thread1->start();
-        thread2->start();
-        thread3->start();
-        thread4->start();
-
-        thread1->join();
-        thread2->join();
-        thread3->join();
-        thread4->join();
-
-        delete thread1;
-        delete thread2;
-        delete thread3;
-        delete thread4;
-
-        bool success = true;
-        debug("Results:\n");
-        for (int i=0; i < max_random; i++) {
-            unsigned pushed = _stats[0][i] + _stats[1][i] + _stats[2][i];
-            unsigned popped = _stats[3][i];
-
-            debug("    value=%-08d pushed=%-08d popped=%-08d\n", i,
-                pushed, popped);
-
-            if (pushed != popped) {
-                success = false;
-            }
-        }
-
-        return success;
-    }
-
-private:
-
-    ring_mpsc<int, 4096> _ring;
-
-    int _stats[4][max_random] = {};
-
-    void thread_push(int cpu_id)
-    {
-        std::srand(std::time(0));
-        for (u64 ctr=0; ctr < elements_to_process; ctr++)
-        {
-            int element = std::rand() % max_random + 6;
-            unsigned in_idx = _ring.push(element);
-            if (in_idx != 0)
-                while (!_ring.push_to(element, in_idx));
-            _stats[cpu_id][element-6]++;
-        }
-    }
-
-    void thread_pop(int cpu_id)
-    {
-        std::srand(std::time(0));
-        for (u64 ctr=0; ctr < elements_to_process*3; ctr++)
-        {
-            int element = 0;
-            while (!_ring.pop(element));
-            _stats[cpu_id][element-6]++;
-        }
-    }
-};
 
 
 class test_mpsc_queue {
@@ -307,21 +223,6 @@ int main(int argc, char **argv)
         debug("[+] throughput: %.0f ops/s\n", (double)(test_spsc_ring::elements_to_process*2)/dT);
     } else {
         debug("[-] spsc test failed\n");
-        return 1;
-    }
-
-    debug("[~] Testing mpsc ringbuffer:\n");
-    test_mpsc_ring t2;
-    beg = nanotime();
-    rc = t2.run();
-    end = nanotime();
-    if (rc) {
-        double dT = (double)(end-beg)/1000000000.0;
-        debug("[+] mpsc test passed:\n");
-        debug("[+] duration: %.6fs\n", dT);
-        debug("[+] throughput: %.0f ops/s\n", (double)(test_mpsc_ring::elements_to_process*6)/dT);
-    } else {
-        debug("[-] mpsc test failed\n");
         return 1;
     }
 
