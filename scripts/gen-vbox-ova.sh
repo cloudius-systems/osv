ab8eb942cbdb951b43e2721e619b1d52ee3320a6
Asias He <asias@cloudius-systems.com>
2014-03-07 11:08:38 +0800

    scripts: Add script to generate OSv VirtualBox Appliance
    * Run scripts/gen-vbox-ova.sh
    
    A build/release/osv.ova will be created
    
    * Import OSv VirtualBox Appliance
    
    Click File -> Import Appliance, Choose osv.ova to import
    
    Signed-off-by: Asias He <asias@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

---
+++ scripts/gen-vbox-ova.sh
@@ -0,0 +1,46 @@
+#!/bin/sh
+
+# Setup name and dir
+name=osv
+vmdir=~/VirtualBox\ VMs/$name
+origin_img=build/release/usr.img
+ova_img=build/release/osv.ova
+
+# Stop vm
+VBoxManage controlvm $name poweroff  >/dev/null 2>&1
+# Unreigster vm
+VBoxManage unregistervm $name --delete  >/dev/null 2>&1
+# Delete old ova
+rm -f $ova_img
+
+# Create and register the vm
+VBoxManage createvm  --name $name -ostype Linux26_64
+VBoxManage registervm "$vmdir/$name.vbox"
+
+# Setup mem
+VBoxManage modifyvm osv --memory 1024
+
+# Setup SATA controller
+qemu-img convert $origin_img -O vdi "$vmdir/$name.vdi"
+VBoxManage storagectl  $name  --name SATA --add sata --controller IntelAHCI
+VBoxManage storageattach  $name --storagectl SATA --port 0 --type hdd --medium "$vmdir/$name.vdi"
+
+# Setup Network hostonly
+VBoxManage modifyvm $name --nic1 hostonly
+VBoxManage modifyvm $name --nictype1 virtio
+VBoxManage modifyvm $name --hostonlyadapter1 vboxnet0
+
+# Turn on HPET timer
+VBoxManage modifyvm $name --hpet on
+
+# Use ICH9 instead of piix
+#VBoxManage modifyvm $name --chipset ich9
+
+# Setup serial
+VBoxManage modifyvm $name  --uart1 0x3f8 4
+#VBoxManage modifyvm $name  --uartmode1 file "$vmdir/$name.log"
+
+# Export ova
+VBoxManage export osv -o build/release/osv.ova --ovf10 --vsys 0 --product "OSv" --vendor "Cloudius Systems"
+
+echo "$PWD/$ova_img is created"
