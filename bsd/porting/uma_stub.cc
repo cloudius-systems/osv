cloudius-systems/osv

commit 08835b4e608eddcbd0ce17e3ef7a813d7b239731

bsd: don't cache uma allocations if CONF_debug_memory
Caching allocations can hide use-after-free bugs.

Signed-off-by: Avi Kivity <avi@cloudius-systems.com>
Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

bsd/porting/uma_stub.cc
@@ -14,7 +14,7 @@
 
 void* uma_zone::cache::alloc()
 {
-    if (len) {
+    if (len && CONF_debug_memory) {
         return a[--len];
     }
     return nullptr;
@@ -22,7 +22,7 @@ void* uma_zone::cache::alloc()
 
 bool uma_zone::cache::free(void* obj)
 {
-    if (len < max_size) {
+    if (len < max_size && CONF_debug_memory) {
         a[len++] = obj;
         return true;
     }

commit 319cf42ddcce0db62fb1d8118e8454dcbcd5a7ea

Fix uma_zone cache regression
In commit 08835b4e608eddcbd0ce17e3ef7a813d7b239731, we turned off uma
allocation caching when CONF_debug_memory is on because it hid bugs.
Or at least, this is what was *intended*. What the patch actually did
was the opposite - it turned off the cache in the normal compilation!

This caused a deterioration of UDP memcached performance, from 159,000
to 131,000 req/sec. This was the second half of the recent memcached
performance deterioration (see issue #227).

Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>
Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

bsd/porting/uma_stub.cc
@@ -14,7 +14,7 @@
 
 void* uma_zone::cache::alloc()
 {
-    if (len && CONF_debug_memory) {
+    if (len && !CONF_debug_memory) {
         return a[--len];
     }
     return nullptr;
@@ -22,7 +22,7 @@ void* uma_zone::cache::alloc()
 
 bool uma_zone::cache::free(void* obj)
 {
-    if (len < max_size && CONF_debug_memory) {
+    if (len < max_size && !CONF_debug_memory) {
         a[len++] = obj;
         return true;
     }