2266917b0f5c3af6677ba54a4f83a7a6c4404e22
Glauber Costa <glommer@cloudius-systems.com>
2014-03-14 11:20:48 -0300

    OSvMonitor: change criteria for removing balloons
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>

--- java/runjava/src/main/java/io/osv/OSvGCMonitor.java
+++ java/runjava/src/main/java/io/osv/OSvGCMonitor.java
@@ -7,6 +7,7 @@ import javax.management.openmbean.CompositeData;
 import java.lang.management.GarbageCollectorMXBean;
 import java.lang.management.ManagementFactory;
 import java.util.List;
+import java.lang.Runtime;
 
 public class OSvGCMonitor {
 
@@ -14,7 +15,7 @@ public class OSvGCMonitor {
         System.load("/usr/lib/jni/monitor.so");
     }
 
-    private native static void NotifyOSv(long h);
+    private native static void NotifyOSv(long h, long qty);
 
     static long handle;
 
@@ -36,17 +37,12 @@ public class OSvGCMonitor {
                         if (notification.getType().equals(GarbageCollectionNotificationInfo.GARBAGE_COLLECTION_NOTIFICATION)) {
                             CompositeData ndata = (CompositeData) notification.getUserData();
                             GarbageCollectionNotificationInfo info = GarbageCollectionNotificationInfo.from(ndata);
-                            // FIXME: This will relieve pressure, but only
-                            // after the GC is run. Ideally, we should be able
-                            // to flush our balloons *before* the GC is run,
-                            // and potentially avoid it. But there is no
-                            // indication from the JVM that a full GC is about
-                            // to happen, only that it has just happened. In
-                            // the future, we may be able to do better by
-                            // tracking minor GCs and trying to predict if a
-                            // major GC is eminent.
-                            if ("end of major GC".equals(info.getGcAction())) {
-                                NotifyOSv(handle); // FIXME: Pass parameters here to better inform OSv.
+                            boolean major = "end of major GC".equals(info.getGcAction());
+                            long free = Runtime.getRuntime().freeMemory();
+                            long total = Runtime.getRuntime().totalMemory();
+                            long qty = (15 * total) - (free * 100);
+                            if (qty > 0) {
+                                NotifyOSv(handle, qty);
                             }
                         }
                     }
