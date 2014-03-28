1e6cd8f71e934087f68d11fc622ea1d26710947e
Zifei Tong <zifeitong@gmail.com>
2014-03-28 16:20:14 +0800

    scripts: Python3 support for scripts/silentant.py
    Add explicit conversion form bytes array to string which returned by
    Popen.communicate().
    
    Use Python3 style print function.
    
    Signed-off-by: Zifei Tong <zifeitong@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- scripts/silentant.py
+++ scripts/silentant.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python2
+#!/usr/bin/python
 
 # ant insists of being not very quiet, with output like
 #
@@ -7,6 +7,7 @@
 #
 # This script runs ant but detects this output and trims it.
 
+from __future__ import print_function
 import os, sys, subprocess
 
 try:
@@ -14,13 +15,13 @@ try:
                            stdout = subprocess.PIPE,
                            stderr = subprocess.PIPE)
 except OSError:
-     print "Apache Ant not found. Please install ant package."
+     print("Apache Ant not found. Please install ant package.")
      sys.exit(1)
 
-out = ant.communicate()[0]
+out = ant.communicate()[0].decode()
 
 lines = out.splitlines()
 if not (len(lines) == 3 and lines[0] == '' and lines[1] == 'BUILD SUCCESSFUL'):
-    print out,
+    print(out, end='')
 
 sys.exit(ant.returncode)
