6d68d1ab6ecab3e927b6261f70eb951c3b719589
Avi Kivity <avi@cloudius-systems.com>
2014-04-01 20:24:04 +0300

    Revert "rcu: Per-CPU rcu_defer()"
    This reverts commit d24cda2c394f94e603eaae7cb6f02edc41c2ff47.  It wants
    migration_lock to be merged first.
    
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- core/rcu.cc
+++ core/rcu.cc
@@ -11,10 +11,6 @@
 #include <vector>
 #include <boost/algorithm/cxx11/all_of.hpp>
 #include <osv/debug.hh>
-#include <osv/percpu.hh>
-#include <osv/preempt-lock.hh>
-#include <osv/migration-lock.hh>
-#include <osv/wait_record.hh>
 
 namespace osv {
 
@@ -25,13 +21,9 @@ rcu_lock_in_preempt_type rcu_read_lock_in_preempt_disabled;
 namespace rcu {
 
 mutex mtx;
-
-struct rcu_defer_queue {
-    int buf; // double-buffer: 0 or 1
-    std::array<std::function<void ()>, 2000> callbacks[2];
-    unsigned int ncallbacks[2];
-};
-static PERCPU(rcu_defer_queue, percpu_callbacks);
+std::vector<std::function<void ()>> callbacks;
+void collect_garbage();
+sched::thread* garbage_collector_thread;
 
 class cpu_quiescent_state_thread {
 public:
@@ -40,20 +32,13 @@ public:
     bool check(uint64_t generation);
 private:
     void work();
-    void set_generation(uint64_t generation);
 private:
-    static std::atomic<uint64_t> next_generation;
     sched::thread _t;
     std::atomic<uint64_t> _generation = { 0 };
     std::atomic<uint64_t> _request = { 0 };
-    std::atomic<bool> _requested { false };
 };
 
-std::atomic<uint64_t> cpu_quiescent_state_thread::next_generation { 0 };
-
 std::vector<cpu_quiescent_state_thread*> cpu_quiescent_state_threads;
-static PERCPU(sched::thread_handle, percpu_quiescent_state_thread);
-static PERCPU(wait_record*, percpu_waiting_defers);
 
 // FIXME: hot-remove cpus
 // FIXME: locking for the vector
@@ -64,7 +49,6 @@ sched::cpu::notifier cpu_notifier([] {
 cpu_quiescent_state_thread::cpu_quiescent_state_thread(sched::cpu* cpu)
     : _t([=] { work(); }, sched::thread::attr().pin(cpu).name(osv::sprintf("rcu%d", cpu->id)))
 {
-    (*percpu_quiescent_state_thread).reset(_t);
     _t.start();
 }
 
@@ -82,15 +66,15 @@ bool cpu_quiescent_state_thread::check(uint64_t generation)
     return _generation.load(std::memory_order_relaxed) >= generation;
 }
 
-void cpu_quiescent_state_thread::set_generation(uint64_t generation)
+void cpu_quiescent_state_thread::work()
 {
-    _generation.store(generation, std::memory_order_relaxed);
-    // Wake the quiescent threads who might be interested in my _generation.
-    for (auto cqst : cpu_quiescent_state_threads) {
-        if (cqst != this &&
-                cqst->_requested.load(std::memory_order_relaxed)) {
-            cqst->_t.wake();
-        }
+    while (true) {
+        sched::thread::wait_until([&] {
+            return _generation.load(std::memory_order_relaxed) <  _request.load(std::memory_order_relaxed);
+        });
+        auto r = _request.load(std::memory_order_relaxed);
+        _generation.store(r, std::memory_order_relaxed);
+        garbage_collector_thread->wake();
     }
 }
 
@@ -105,74 +89,32 @@ bool all_at_generation(decltype(cpu_quiescent_state_threads)& cqsts,
     return true;
 }
 
-void cpu_quiescent_state_thread::work()
+void await_grace_period()
+{
+    static uint64_t generation = 0;
+    ++generation;
+    // copy cpu_quiescent_state_threads to prevent a hotplugged cpu
+    // from changing the number of cpus we request a new generation on,
+    // and the number of cpus we wait on
+    // FIXME: better locking
+    auto cqsts = cpu_quiescent_state_threads;
+    for (auto cqst : cqsts) {
+        cqst->request(generation);
+    }
+    sched::thread::wait_until([&cqsts] { return all_at_generation(cqsts, generation); });
+}
+
+void collect_garbage()
 {
     while (true) {
-        bool toclean = false;
-        WITH_LOCK(preempt_lock) {
-            auto p = &*percpu_callbacks;
-            if (p->ncallbacks[p->buf]) {
-                toclean = true;
-                p->buf = !p->buf;
-            }
-            // If an rcu_defer() is waiting for buffer room, let it know.
-            auto q = *percpu_waiting_defers;
-            while (q) {
-                auto next = q->next;
-                q->wake();
-                q = next;
-            }
-            *percpu_waiting_defers = nullptr;
+        std::vector<std::function<void ()>> now;
+        WITH_LOCK(mtx) {
+            sched::thread::wait_until(mtx, [] { return !callbacks.empty(); });
+            now = std::move(callbacks);
         }
-        if (toclean) {
-            auto g = next_generation.fetch_add(1, std::memory_order_relaxed) + 1;
-            _requested.store(true, std::memory_order_relaxed);
-            // copy cpu_quiescent_state_threads to prevent a hotplugged cpu
-            // from changing the number of cpus we request a new generation on,
-            // and the number of cpus we wait on
-            // FIXME: better locking
-            auto cqsts = cpu_quiescent_state_threads;
-            for (auto cqst : cqsts) {
-                if (cqst != this) {
-                    cqst->request(g);
-                }
-            }
-            set_generation(g);
-            // Wait until desired generation g is reached, but while waiting
-            // also service generation requests from other cpus' threads.
-            while (true) {
-                sched::thread::wait_until([&cqsts, &g, this] {
-                    return ( (_generation.load(std::memory_order_relaxed) <
-                                _request.load(std::memory_order_relaxed))
-                             || all_at_generation(cqsts, g)); });
-                auto r = _request.load(std::memory_order_relaxed);
-                if (_generation.load(std::memory_order_relaxed) < r) {
-                    set_generation(r);
-                } else {
-                    break;
-                }
-            }
-            // Finally all_at_generation(cqsts, g), so can clean up
-            _requested.store(false, std::memory_order_relaxed);
-            auto p = &*percpu_callbacks;
-            auto b = !p->buf;
-            auto &callbacks = p->callbacks[b];
-            auto ncallbacks = p->ncallbacks[b];
-            p->ncallbacks[b] = 0;
-            for (unsigned i = 0; i < ncallbacks; i++) {
-                (callbacks[i])();
-            }
-        } else {
-            // Wait until we have a generation request from another CPU who
-            // wants to clean up, or we are woken to clean up our callbacks
-            sched::thread::wait_until([=] {
-                return (_generation.load(std::memory_order_relaxed) <
-                        _request.load(std::memory_order_relaxed)) ||
-                        percpu_callbacks->ncallbacks[percpu_callbacks->buf]; });
-            auto r = _request.load(std::memory_order_relaxed);
-            if (_generation.load(std::memory_order_relaxed) < r) {
-                set_generation(r);
-            }
+        await_grace_period();
+        for (auto& c : now) {
+            c();
         }
     }
 }
@@ -183,39 +125,23 @@ using namespace rcu;
 
 void rcu_defer(std::function<void ()>&& func)
 {
-    WITH_LOCK(preempt_lock) {
-        auto p = &*percpu_callbacks;
-        while (p->ncallbacks[p->buf] == p->callbacks[p->buf].size()) {
-            // We're out of room. Wait for the cleanup on this CPU to switch
-            // buffers. Make sure to re-awake on the same CPU.
-            // FIXME: We have a starvation possibility: another thread looping
-            // on rcu_defer() can cause us to always find a full queue.
-            wait_record wr(sched::thread::current());
-            wr.next = *percpu_waiting_defers;
-            *percpu_waiting_defers = &wr;
-            WITH_LOCK(migration_lock) {
-                DROP_LOCK(preempt_lock) {
-                    (*percpu_quiescent_state_thread).wake();
-                    wr.wait();
-                }
-            }
-            assert (p == &*percpu_callbacks);
-        }
-        auto b = p->buf;
-        p->callbacks[b][p->ncallbacks[b]++] = std::move(func);
+    WITH_LOCK(mtx) {
+        callbacks.push_back(func);
     }
+    garbage_collector_thread->wake();
 }
 
 void rcu_synchronize()
 {
     semaphore s{0};
-    WITH_LOCK(migration_lock) {
-        rcu_defer([](semaphore* s) { s->post(); }, &s);
-        // rcu_defer() might not wake the cleanup thread until enough deferred
-        // callbacks have accumulated, so wake it up now.
-        (*percpu_quiescent_state_thread).wake();
-    }
+    rcu_defer([](semaphore* s) { s->post(); }, &s);
     s.wait();
+}
+
+void rcu_init()
+{
+    garbage_collector_thread = new sched::thread(collect_garbage, sched::thread::attr().name("rcu_gc"));
+    garbage_collector_thread->start();
 }
 
 }
