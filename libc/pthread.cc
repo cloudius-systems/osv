c86c915b047e93798aca4ccb5aafba4782fa2c56
Glauber Costa <glommer@cloudius-systems.com>
2014-04-09 12:11:26 +0400

    pthread: implement mutexattr functions
    redis (jemalloc to be precise) actually expects that functions to succeed.
    Returning -1 here means it will not initialize correctly its memory allocator.
    
    This simple implementation just stores the desired value, but does not change
    anything in the underlying mutex.
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- libc/pthread.cc
+++ libc/pthread.cc
@@ -560,26 +560,25 @@ int pthread_equal(pthread_t t1, pthread_t t2)
 
 int pthread_mutexattr_init(pthread_mutexattr_t *attr)
 {
-    WARN_STUBBED();
-    return ENOMEM;
+    *(attr) = PTHREAD_MUTEX_DEFAULT;
+    return 0;
 }
 
 int pthread_mutexattr_destroy(pthread_mutexattr_t *attr)
 {
-    WARN_STUBBED();
-    return EINVAL;
+    return 0;
 }
 
 int pthread_mutexattr_gettype(const pthread_mutexattr_t *attr, int *type)
 {
-    WARN_STUBBED();
-    return EINVAL;
+    *(type) = *(attr);
+    return 0;
 }
 
 int pthread_mutexattr_settype(pthread_mutexattr_t *attr, int type)
 {
-    WARN_STUBBED();
-    return EINVAL;
+    *(attr) = type;
+    return 0;
 }
 
 int pthread_condattr_init(pthread_condattr_t *attr)
