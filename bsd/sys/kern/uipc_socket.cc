43a7292aaf509981477f81d86ec506da3f6858d8
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2014-04-11 09:55:48 +0200

    net: extract common code
    Signed-off-by: Tomasz Grabiec <tgrabiec@cloudius-systems.com>
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- bsd/sys/kern/uipc_socket.cc
+++ bsd/sys/kern/uipc_socket.cc
@@ -582,6 +582,13 @@ sofree(struct socket *so)
 	sodealloc(so);
 }
 
+static void flush_net_channel(struct socket *so)
+{
+	if (so->so_nc) {
+		so->so_nc->process_queue();
+	}
+}
+
 /*
  * Close a socket on last file table reference removal.  Initiate disconnect
  * if connected.  Free socket when disconnect complete.
@@ -970,9 +977,7 @@ sosend_generic(struct socket *so, struct bsd_sockaddr *addr, struct uio *uio,
 		goto out;
 
 restart:
-	if (so->so_nc) {
-		so->so_nc->process_queue();
-	}
+	flush_net_channel(so);
 	do {
 		if (so->so_snd.sb_state & SBS_CANTSENDMORE) {
 			error = EPIPE;
@@ -1235,9 +1240,7 @@ soreceive_generic(struct socket *so, struct bsd_sockaddr **psa, struct uio *uio,
 		goto out;
 
 restart:
-	if (so->so_nc) {
-		so->so_nc->process_queue();
-	}
+	flush_net_channel(so);
 	m = so->so_rcv.sb_mb;
 	/*
 	 * If we have less data than requested, block awaiting more (subject
