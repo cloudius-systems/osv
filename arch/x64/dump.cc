4a1a240cb99e310757289846cec536b2f7cb7037
Glauber Costa <glommer@cloudius-systems.com>
2014-04-16 16:22:03 +0400

    x86: fix dump_registers
    We are usign %x instead of %lx for x86_64. This has the effect of trimming all
    64 bits to 32 bits creating a very, very confusing situation for people
    debugging OSv.
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- arch/x64/dump.cc
+++ arch/x64/dump.cc
@@ -12,9 +12,9 @@
 void dump_registers(exception_frame* ef)
 {
     debug_ll("registers:\n");
-    debug_ll("RIP: 0x%016x  RFL: 0x%016x  CS:  0x%016x  SS:  0x%016x\n", ef->rip, ef->rflags, ef->cs, ef->ss);
-    debug_ll("RAX: 0x%016x  RBX: 0x%016x  RCX: 0x%016x  RDX: 0x%016x\n", ef->rax, ef->rbx, ef->rcx, ef->rdx);
-    debug_ll("RSI: 0x%016x  RDI: 0x%016x  RBP: 0x%016x  R8:  0x%016x\n", ef->rsi, ef->rdi, ef->rbp, ef->r8);
-    debug_ll("R9:  0x%016x  R10: 0x%016x  R11: 0x%016x  R12: 0x%016x\n", ef->r9, ef->r10, ef->r11, ef->r12);
-    debug_ll("R13: 0x%016x  R14: 0x%016x  R15: 0x%016x  RSP: 0x%016x\n", ef->r9, ef->r10, ef->r11, ef->rsp);
+    debug_ll("RIP: 0x%016lx  RFL: 0x%016lx  CS:  0x%016lx  SS:  0x%016lx\n", ef->rip, ef->rflags, ef->cs, ef->ss);
+    debug_ll("RAX: 0x%016lx  RBX: 0x%016lx  RCX: 0x%016lx  RDX: 0x%016lx\n", ef->rax, ef->rbx, ef->rcx, ef->rdx);
+    debug_ll("RSI: 0x%016lx  RDI: 0x%016lx  RBP: 0x%016lx  R8:  0x%016lx\n", ef->rsi, ef->rdi, ef->rbp, ef->r8);
+    debug_ll("R9:  0x%016lx  R10: 0x%016lx  R11: 0x%016lx  R12: 0x%016lx\n", ef->r9, ef->r10, ef->r11, ef->r12);
+    debug_ll("R13: 0x%016lx  R14: 0x%016lx  R15: 0x%016lx  RSP: 0x%016lx\n", ef->r9, ef->r10, ef->r11, ef->rsp);
 }
