ca1299623ac083459728b9e3a749e6000a8c8467
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2014-04-29 20:45:47 -0300

    drivers: Change random to use Yarrow CPRNG and add urandom as an alias
    This patch changes the device behind /dev/random to start using FreeBSD Yarrow
    CPRNG algorithm. Similar to FreeBSD, our /dev/urandom was created as an alias
    to /dev/random.
    The device operations are linked to Yarrow through the random adaptor API.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- drivers/random.cc
+++ drivers/random.cc
@@ -1,5 +1,33 @@
+/*-
+ * Copyright (c) 2000-2013 Mark R V Murray
+ * Copyright (c) 2013 Arthur Mesh <arthurmesh@gmail.com>
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions
+ * are met:
+ * 1. Redistributions of source code must retain the above copyright
+ *    notice, this list of conditions and the following disclaimer
+ *    in this position and unchanged.
+ * 2. Redistributions in binary form must reproduce the above copyright
+ *    notice, this list of conditions and the following disclaimer in the
+ *    documentation and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE AUTHOR ``AS IS'' AND ANY EXPRESS OR
+ * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
+ * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED.
+ * IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY DIRECT, INDIRECT,
+ * INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT
+ * NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+ * THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
+ */
+
 /*
- * Copyright (C) 2013 Cloudius Systems, Ltd.
+ * Copyright (C) 2014 Cloudius Systems, Ltd.
  *
  * This work is open source software, licensed under the terms of the
  * BSD license as described in the LICENSE file in the top-level directory.
@@ -8,6 +36,16 @@
 #include "drivers/random.hh"
 
 #include <osv/device.h>
+#include <osv/uio.h>
+#include <osv/debug.hh>
+
+#include <sys/selinfo.h>
+#include <sys/random.h>
+#include <sys/param.h>
+
+#include <dev/random/randomdev.h>
+#include <dev/random/randomdev_soft.h>
+#include <dev/random/random_adaptors.h>
 
 namespace randomdev {
 
@@ -23,18 +61,42 @@ static random_device_priv *to_priv(device *dev)
 static int
 random_read(struct device *dev, struct uio *uio, int ioflags)
 {
-    auto prv = to_priv(dev);
-    for (auto i = 0; i < uio->uio_iovcnt; i++) {
-        auto *iov = &uio->uio_iov[i];
-        auto nr = prv->drv->get_random_bytes(static_cast<char*>(iov->iov_base), iov->iov_len);
+    int c, error = 0;
+    char *random_buf;
 
-        uio->uio_resid  -= nr;
-        uio->uio_offset += nr;
-
-        if (nr < iov->iov_len) {
-            break;
-        }
+    // Blocking logic
+    if (!random_adaptor->seeded) {
+        error = (*random_adaptor->block)(ioflags);
     }
+
+    if (!error) {
+        random_buf = new char[PAGE_SIZE];
+
+        while (uio->uio_resid > 0 && !error) {
+            c = std::min(uio->uio_resid, static_cast<long int>(PAGE_SIZE));
+            c = (*random_adaptor->read)(static_cast<void *>(random_buf), c);
+            error = uiomove(random_buf, c, uio);
+        }
+
+        // Finished reading; let the source know so it can do some
+        // optional housekeeping */
+        (*random_adaptor->read)(nullptr, 0);
+
+        delete [] random_buf;
+    }
+
+    return error;
+}
+
+static int
+random_write(struct device *dev, struct uio *uio, int ioflags)
+{
+    // We used to allow this to insert userland entropy.
+    // We don't any more because (1) this so-called entropy
+    // is usually lousy and (b) its vaguely possible to
+    // mess with entropy harvesting by overdoing a write.
+    // Now we just ignore input like /dev/null does.
+    uio->uio_resid = 0;
 
     return 0;
 }
@@ -43,7 +105,7 @@ static struct devops random_device_devops {
     no_open,
     no_close,
     random_read,
-    no_write,
+    random_write,
     no_ioctl,
     no_devctl,
 };
@@ -58,14 +120,25 @@ random_device::random_device()
 {
     struct random_device_priv *prv;
 
+    (random_adaptor->init)();
+
+    // Create random
     _random_dev = device_create(&random_device_driver, "random", D_CHR);
     prv = to_priv(_random_dev);
+    prv->drv = this;
+
+    // Create urandom as a sort of alias to random
+    _urandom_dev = device_create(&random_device_driver, "urandom", D_CHR);
+    prv = to_priv(_urandom_dev);
     prv->drv = this;
 }
 
 random_device::~random_device()
 {
+    (random_adaptor->deinit)();
+
     device_destroy(_random_dev);
+    device_destroy(_urandom_dev);
 }
 
 static hw_rng* s_hwrng;
@@ -75,18 +148,11 @@ void random_device::register_source(hw_rng* hwrng)
     s_hwrng = hwrng;
 }
 
-size_t random_device::get_random_bytes(char *buf, size_t size)
-{
-    if (s_hwrng) {
-        return s_hwrng->get_random_bytes(buf, size);
-    }
-    return 0;
-}
-
 void randomdev_init()
 {
     if (s_hwrng) {
         new random_device();
+        debug("random: <%s> initialized\n", random_adaptor->ident);
     }
 }
 
--- drivers/random.hh
+++ drivers/random.hh
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2013 Cloudius Systems, Ltd.
+ * Copyright (C) 2014 Cloudius Systems, Ltd.
  *
  * This work is open source software, licensed under the terms of the
  * BSD license as described in the LICENSE file in the top-level directory.
@@ -22,13 +22,12 @@ public:
     random_device();
     virtual ~random_device();
 
-    size_t get_random_bytes(char *buf, size_t size);
-
     static void register_source(hw_rng* hwrng);
 
 private:
 
     device* _random_dev;
+    device* _urandom_dev;
 };
 
 class hw_rng {
