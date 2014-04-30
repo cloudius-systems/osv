4db42e4f675169b0f44148b916269ea517ee2cd5
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2014-04-30 17:33:38 +0200

    tests: add test exploiting client RST problem
    The test fails when the problem described in issue #283 exists.
    
    Signed-off-by: Tomasz Grabiec <tgrabiec@cloudius-systems.com>
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- tests/tst-tcp-listen.cc
+++ tests/tst-tcp-listen.cc
@@ -8,15 +8,38 @@
 #define BOOST_TEST_MODULE tst-tcp-listen
 
 #include <vector>
+#include <thread>
+#include <chrono>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <osv/latch.hh>
 #include <boost/test/unit_test.hpp>
+#include <boost/interprocess/sync/interprocess_semaphore.hpp>
 
 #define LISTEN_PORT 7777
 
+using _clock = std::chrono::high_resolution_clock;
+
+static int accept_with_timeout(int listening_socket, int timeout_in_seconds)
+{
+    fd_set rfds;
+    FD_ZERO(&rfds);
+    FD_SET(listening_socket, &rfds);
+
+    struct timeval tv = { .tv_sec = timeout_in_seconds, .tv_usec = 0 };
+    int retval = select(listening_socket + 1, &rfds, NULL, NULL, &tv);
+    BOOST_REQUIRE_EQUAL(1, retval);
+
+    int client_socket = accept(listening_socket, NULL, NULL);
+    BOOST_REQUIRE(client_socket > 0);
+    return client_socket;
+}
+
 BOOST_AUTO_TEST_CASE(test_connections_get_accepted_even_when_backlog_gets_overflowed)
 {
+    std::vector<int> sockets_to_close;
+
     constexpr int n_connections = 7;
     constexpr int backlog_size = 2;
 
@@ -29,6 +52,8 @@ BOOST_AUTO_TEST_CASE(test_connections_get_accepted_even_when_backlog_gets_overfl
     auto listen_s = socket(AF_INET, SOCK_STREAM, 0);
     BOOST_REQUIRE(listen_s > 0);
 
+    sockets_to_close.push_back(listen_s);
+
     struct sockaddr_in laddr = {};
     laddr.sin_family = AF_INET;
     laddr.sin_addr.s_addr = htonl(INADDR_ANY);
@@ -38,8 +63,6 @@ BOOST_AUTO_TEST_CASE(test_connections_get_accepted_even_when_backlog_gets_overfl
     BOOST_REQUIRE(listen(listen_s, backlog_size) == 0);
 
     BOOST_MESSAGE("listening...");
-
-    std::vector<int> sockets_to_close;
 
     for (int i = 0; i < n_connections; i++) {
         int s = socket(AF_INET, SOCK_STREAM, 0);
@@ -59,16 +82,7 @@ BOOST_AUTO_TEST_CASE(test_connections_get_accepted_even_when_backlog_gets_overfl
     BOOST_MESSAGE("starting to accept...");
 
     for (int i = 0; i < n_connections; i++) {
-        fd_set rfds;
-        FD_ZERO(&rfds);
-        FD_SET(listen_s, &rfds);
-
-        // Wait 3 seconds before failing the test
-        struct timeval tv = { .tv_sec = 3, .tv_usec = 0 };
-        int retval = select(listen_s + 1, &rfds, NULL, NULL, &tv);
-        BOOST_REQUIRE_EQUAL(1, retval);
-
-        int client_s = accept(listen_s, NULL, NULL);
+        int client_s = accept_with_timeout(listen_s, 3);
         BOOST_REQUIRE(client_s >= 0);
         BOOST_MESSAGE("accepted");
 
@@ -80,4 +94,94 @@ BOOST_AUTO_TEST_CASE(test_connections_get_accepted_even_when_backlog_gets_overfl
     for (auto& fd : sockets_to_close) {
         close(fd);
     }
+}
+
+BOOST_AUTO_TEST_CASE(test_clients_are_not_reset_when_backlog_is_full_and_they_write)
+{
+    constexpr int backlog_size = 5;
+    constexpr int n_connections = backlog_size * 3;
+
+    auto listen_s = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_s < 0) {
+        perror("socket");
+        exit(1);
+    }
+
+    struct sockaddr_in laddr = {};
+    laddr.sin_family = AF_INET;
+    laddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    laddr.sin_port = htons(LISTEN_PORT);
+
+    if (bind(listen_s, (struct sockaddr *) &laddr, sizeof(laddr)) < 0) {
+        perror("bind");
+        exit(1);
+    }
+
+    BOOST_REQUIRE(listen(listen_s, backlog_size) == 0);
+
+    BOOST_MESSAGE("listening...");
+
+    std::vector<std::thread*> threads;
+
+    latch _latch(n_connections);
+
+    for (int i = 0; i < n_connections; i++) {
+        threads.push_back(new std::thread([i, &_latch] {
+            int s = socket(AF_INET, SOCK_STREAM, 0);
+            if (s < 0) {
+                perror("socket");
+                exit(1);
+            }
+
+            struct sockaddr_in raddr = {};
+            raddr.sin_family = AF_INET;
+            inet_aton("127.0.0.1", &raddr.sin_addr);
+            raddr.sin_port = htons(LISTEN_PORT);
+
+            _latch.count_down();
+
+            if (connect(s, (struct sockaddr *)&raddr, sizeof(raddr))) {
+                perror("connect");
+                exit(1);
+            }
+
+            while (true) {
+                const char* msg = "hello";
+                int bytes = write(s, msg, strlen(msg) + 1);
+                if (bytes < 0) {
+                    break;
+                }
+            }
+        }));
+    }
+
+    // Start accepting after all clients are lined up
+    // to create a thundering storm effect.
+    _latch.await();
+
+    for (int i = 0; i < n_connections; i++) {
+        int client_s = accept_with_timeout(listen_s, 3);
+        BOOST_MESSAGE("accepted");
+
+        threads.push_back(new std::thread([client_s] {
+            auto close_at = _clock::now() + std::chrono::milliseconds(50);
+            while (_clock::now() < close_at) {
+                char buf[1];
+                int bytes = read(client_s, &buf, sizeof(buf));
+                if (bytes < 0) {
+                    perror("read");
+                    exit(1);
+                }
+            }
+
+            close(client_s);
+        }));
+    }
+
+    for (auto& thread : threads) {
+        thread->join();
+        delete thread;
+    }
+
+    close(listen_s);
 }
