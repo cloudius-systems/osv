17244715bf69823f99bb1c73be3ba8c318b73763
Takuya ASADA <syuu@cloudius-systems.com>
2014-05-01 12:13:54 +0900

    Add symbol name on dump_registers
    Resolve and demangle symbol name of RIP, show it.
    
    Signed-off-by: Takuya ASADA <syuu@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- arch/x64/dump.cc
+++ arch/x64/dump.cc
@@ -7,12 +7,18 @@
 
 #include "exceptions.hh"
 #include <osv/debug.hh>
+#include <osv/demangle.hh>
 #include "dump.hh"
 
 void dump_registers(exception_frame* ef)
 {
-    debug_ll("registers:\n");
-    debug_ll("RIP: 0x%016lx  RFL: 0x%016lx  CS:  0x%016lx  SS:  0x%016lx\n", ef->rip, ef->rflags, ef->cs, ef->ss);
+    char name[1024];
+
+    osv::lookup_name_demangled(
+        reinterpret_cast<void *>(ef->rip), name, sizeof(name));
+    debug_ll("[registers]\n");
+    debug_ll("RIP: 0x%016x <%s>\n", ef->rip, name);
+    debug_ll("RFL: 0x%016x  CS:  0x%016x  SS:  0x%016x\n", ef->rflags, ef->cs, ef->ss);
     debug_ll("RAX: 0x%016lx  RBX: 0x%016lx  RCX: 0x%016lx  RDX: 0x%016lx\n", ef->rax, ef->rbx, ef->rcx, ef->rdx);
     debug_ll("RSI: 0x%016lx  RDI: 0x%016lx  RBP: 0x%016lx  R8:  0x%016lx\n", ef->rsi, ef->rdi, ef->rbp, ef->r8);
     debug_ll("R9:  0x%016lx  R10: 0x%016lx  R11: 0x%016lx  R12: 0x%016lx\n", ef->r9, ef->r10, ef->r11, ef->r12);
