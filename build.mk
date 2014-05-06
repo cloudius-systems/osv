7f05817130878e893480b20ac7a90349da48dc9c
Takuya ASADA <syuu@cloudius-systems.com>
2014-05-07 00:02:50 +0900

    build.mk: remove --vga from cmdline on osv.vmdk/.vdi
    Now we enabled both VGA and serial, we nolonger need to add --vga cmdline for VMW/VBox.
    
    Reviewed-by: Asias He <asias@cloudius-systems.com>
    Signed-off-by: Takuya ASADA <syuu@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- build.mk
+++ build.mk
@@ -841,7 +841,6 @@ usr.img: bare.img $(out)/usr.manifest $(out)/cmdline
 osv.vmdk osv.vdi:
 	$(call quiet, echo Creating $@ as $(subst osv.,,$@))
 	$(call quiet, qemu-img convert -O $(subst osv.,,$@) usr.img $@)
-	$(call quiet, $(src)/scripts/imgedit.py setargs $@ "--vga $(shell cat $(out)/cmdline)", IMGEDIT $@)
 .PHONY: osv.vmdk osv.vdi
 
 $(jni): INCLUDES += -I /usr/lib/jvm/java/include -I /usr/lib/jvm/java/include/linux/
