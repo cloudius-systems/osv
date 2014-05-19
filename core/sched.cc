34ddac252b87ec664220c89158a7728c40a818fc
Glauber Costa <glommer@cloudius-systems.com>
2014-05-19 12:32:17 +0400

    rework OSV_SYM macro
    As Nadav pointed out during review, this macro could use a bit more work, to
    use a single parameter instead of one. That is what is done in this patch.
    Unfortunately just pasting __COUNTER__ doesn't work because of preprocessor
    rules, and we need some indirection to get it working. Also, visibility
    "hidden" can go because that is already implied by "static". The problem then
    becomes the fact that gcc does not really like unreferenced static variables,
    which is solved by the "used" attribute. From gcc docs about "used":
    
       "This attribute, attached to a variable with the static storage, means that
        the variable must be emitted even if it appears that the variable is not
        referenced."
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- core/sched.cc
+++ core/sched.cc
@@ -31,13 +31,15 @@
 // a symbol for it even when the function is inlined into all call sites. In a
 // situation like that, the symbol would simply not be generated. That seems to
 // be true even if we use "inline" instead of "static inline"
-#define OSV_SYM(module, name) void *__address##name __attribute__((visibility("hidden"))) = (void *)&module::name
-OSV_SYM(sched::thread, current);
-OSV_SYM(sched, get_preempt_counter);
-OSV_SYM(sched, preemptable);
-OSV_SYM(sched, preempt);
-OSV_SYM(sched, preempt_disable);
-OSV_SYM(sched, preempt_enable);
+#define __MAKE_SYMBOL(name, num) static void *__address_##num __attribute__((used)) = (void *)&name
+#define _MAKE_SYMBOL(name, num) __MAKE_SYMBOL(name, num)
+#define MAKE_SYMBOL(name) _MAKE_SYMBOL(name, __COUNTER__)
+MAKE_SYMBOL(sched::thread::current);
+MAKE_SYMBOL(sched::get_preempt_counter);
+MAKE_SYMBOL(sched::preemptable);
+MAKE_SYMBOL(sched::preempt);
+MAKE_SYMBOL(sched::preempt_disable);
+MAKE_SYMBOL(sched::preempt_enable);
 
 __thread char* percpu_base;
 
