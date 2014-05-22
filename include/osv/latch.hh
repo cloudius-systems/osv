a1b0b9633d1102334dee6f6094d374ff14293a35
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2014-05-22 13:27:16 +0200

    tests: introduce latch.is_released()
    Signed-off-by: Tomasz Grabiec <tgrabiec@cloudius-systems.com>

--- include/osv/latch.hh
+++ include/osv/latch.hh
@@ -3,11 +3,12 @@
 
 #include <condition_variable>
 #include <mutex>
+#include <atomic>
 
 class latch
 {
 private:
-    int _count;
+    std::atomic<int> _count;
     std::mutex _mutex;
     std::condition_variable _condvar;
 public:
@@ -18,16 +19,25 @@ public:
 
     void count_down()
     {
-        std::lock_guard<std::mutex> l(_mutex);
-        if (--_count == 0) {
+        std::unique_lock<std::mutex> l(_mutex);
+        if (_count.fetch_add(-1, std::memory_order_release) == 1) {
             _condvar.notify_all();
         }
     }
 
+    bool is_released()
+    {
+        return _count.load(std::memory_order_acquire) <= 0;
+    }
+
     void await()
     {
+        if (is_released()) {
+            return;
+        }
+
         std::unique_lock<std::mutex> l(_mutex);
-        while (_count > 0) {
+        while (!is_released()) {
             _condvar.wait(l);
         }
     }
