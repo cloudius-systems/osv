c5ed256f160ca2301fcc3e054423a1e1be62b328
Glauber Costa <glommer@cloudius-systems.com>
2014-05-26 16:35:07 +0400

    libc: implement ffsl and ffsll functions
    We already have the ffs function, ffsl and ffsll are easy from here.
    Theoretically, a_ctz_l should do the job here as well since we're 64 all
    over, but I found a_ctz_64 safer.
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- libc/misc/ffs.c
+++ libc/misc/ffs.c
@@ -1,7 +1,14 @@
 #include <strings.h>
 #include "atomic.h"
+#include <libc.h>
 
 int ffs(int i)
 {
 	return i ? a_ctz_l(i)+1 : 0;
 }
+
+int ffsl(long i)
+{
+	return i ? a_ctz_64(i)+1 : 0;
+}
+weak_alias(ffsl, ffsll);
