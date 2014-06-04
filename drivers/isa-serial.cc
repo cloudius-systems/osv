56dbd25dfe19820d5fa7658c082e93347b27dfc9
Pekka Enberg <penberg@cloudius-systems.com>
2014-06-04 10:16:26 +0300

    serial: Split register definitions
    Split register definitions in per-register enumerations.
    
    Reviewed-by: Glauber Costa <glommer@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- drivers/isa-serial.cc
+++ drivers/isa-serial.cc
@@ -9,36 +9,45 @@
 
 namespace console {
 
-enum IsaSerialValues {
-    // UART registers, offsets to ioport:
-    IER_ADDRESS = 1,    // Interrupt Enable Register
-    FCR_ADDRESS = 2,    // FIFO Control Register
-    LCR_ADDRESS = 3,    // Line Control Register
-    MCR_ADDRESS = 4,    // Modem Control Register
-    LSR_ADDRESS = 5,    // Line Control Register
-    MSR_ADDRESS = 6,    // Modem Status Register
-    SCR_ADDRESS = 7,    // Scratch Register
+// UART registers, offsets to ioport:
+enum regs {
+    IER = 1,    // Interrupt Enable Register
+    FCR = 2,    // FIFO Control Register
+    LCR = 3,    // Line Control Register
+    MCR = 4,    // Modem Control Register
+    LSR = 5,    // Line Control Register
+    MSR = 6,    // Modem Status Register
+    SCR = 7,    // Scratch Register
+    DLL = 0,    // Divisor Latch LSB Register
+    DLM = 1,    // Divisor Latch MSB Register
+};
+
+enum lcr {
     // When bit 7 (DLAB) of LCR is set to 1, the two registers 0 and 1
     // change their meaning and become two bytes controlling the baud rate
-    LCR_DLAB = 0x80,    // Divisor Latch Access Bit in LCR register
-    LCR_8BIT = 3,
-    DLL_ADDRESS = 0,    // Divisor Latch LSB Register
-    DLM_ADDRESS = 1,    // Divisor Latch MSB Register
-    // Various bits of the Line Status Register
-    LSR_RECEIVE_DATA_READY = 0x1,
-    LSR_OVERRUN = 0x2,
-    LSR_PARITY_ERROR = 0x4,
-    LSR_FRAME_ERROR = 0x8,
-    LSR_BREAK_INTERRUPT = 0x10,
-    LSR_TRANSMIT_HOLD_EMPTY = 0x20,
-    LSR_TRANSMIT_EMPTY = 0x40,
-    LSR_FIFO_ERROR = 0x80,
-    // Various bits of the Modem Control Register
-    MCR_DTR = 0x1,
-    MCR_RTS = 0x2,
-    MCR_AUX_OUTPUT_1 = 0x4,
-    MCR_AUX_OUTPUT_2 = 0x8,
-    MCR_LOOPBACK_MODE = 0x16,
+    DLAB     = 0x80,    // Divisor Latch Access Bit in LCR register
+    LEN_8BIT = 3,
+};
+
+// Various bits of the Line Status Register
+enum lsr {
+    RECEIVE_DATA_READY  = 0x1,
+    OVERRUN             = 0x2,
+    PARITY_ERROR        = 0x4,
+    FRAME_ERROR         = 0x8,
+    BREAK_INTERRUPT     = 0x10,
+    TRANSMIT_HOLD_EMPTY = 0x20,
+    TRANSMIT_EMPTY      = 0x40,
+    FIFO_ERROR          = 0x80,
+};
+
+// Various bits of the Modem Control Register
+enum mcr {
+    DTR                 = 0x1,
+    RTS                 = 0x2,
+    AUX_OUTPUT_1        = 0x4,
+    AUX_OUTPUT_2        = 0x8,
+    LOOPBACK_MODE       = 0x16,
 };
 
 void IsaSerialConsole::write(const char *str, size_t len)
@@ -49,18 +58,18 @@ void IsaSerialConsole::write(const char *str, size_t len)
 
 bool IsaSerialConsole::input_ready()
 {
-    u8 lsr = pci::inb(ioport + LSR_ADDRESS);
-    return lsr & LSR_RECEIVE_DATA_READY;
+    u8 val = pci::inb(ioport + regs::LSR);
+    return val & lsr::RECEIVE_DATA_READY;
 }
 
 char IsaSerialConsole::readch()
 {
-    u8 lsr;
+    u8 val;
     char letter;
 
     do {
-        lsr = pci::inb(ioport + LSR_ADDRESS);
-    } while (!(lsr & (LSR_RECEIVE_DATA_READY | LSR_OVERRUN | LSR_PARITY_ERROR | LSR_FRAME_ERROR)));
+        val = pci::inb(ioport + regs::LSR);
+    } while (!(val & (lsr::RECEIVE_DATA_READY | lsr::OVERRUN | lsr::PARITY_ERROR | lsr::FRAME_ERROR)));
 
     letter = pci::inb(ioport);
 
@@ -69,11 +78,11 @@ char IsaSerialConsole::readch()
 
 void IsaSerialConsole::writeByte(const char letter)
 {
-    u8 lsr;
+    u8 val;
 
     do {
-        lsr = pci::inb(ioport + LSR_ADDRESS);
-    } while (!(lsr & LSR_TRANSMIT_HOLD_EMPTY));
+        val = pci::inb(ioport + regs::LSR);
+    } while (!(val & lsr::TRANSMIT_HOLD_EMPTY));
 
     pci::outb(letter, ioport);
 }
@@ -83,21 +92,21 @@ void IsaSerialConsole::reset() {
     // Divisor Latch registers, but to access these we need to temporarily
     // set the Divisor Latch Access Bit (DLAB) on the LSR register, because
     // the UART has fewer ports than registers...
-    pci::outb(LCR_8BIT | LCR_DLAB, ioport + LCR_ADDRESS);
-    pci::outb(1, ioport + DLL_ADDRESS);
-    pci::outb(0, ioport + DLM_ADDRESS);
-    pci::outb(LCR_8BIT, ioport + LCR_ADDRESS);
+    pci::outb(lcr::LEN_8BIT | lcr::DLAB, ioport + regs::LCR);
+    pci::outb(1, ioport + regs::DLL);
+    pci::outb(0, ioport + regs::DLM);
+    pci::outb(lcr::LEN_8BIT, ioport + regs::LCR);
 
     //  interrupt threshold
-    pci::outb(0, ioport + FCR_ADDRESS);
+    pci::outb(0, ioport + regs::FCR);
 
     // enable interrupts
-    pci::outb(1, ioport + IER_ADDRESS);
+    pci::outb(1, ioport + regs::IER);
 
     // Most physical UARTs need the MCR AUX_OUTPUT_2 bit set to 1 for
     // interrupts to be generated. QEMU doesn't bother checking this
     // bit, but interestingly VMWare does, so we must set it.
-    pci::outb(MCR_AUX_OUTPUT_2, ioport + MCR_ADDRESS);
+    pci::outb(mcr::AUX_OUTPUT_2, ioport + regs::MCR);
 }
 
 void IsaSerialConsole::dev_start() {
