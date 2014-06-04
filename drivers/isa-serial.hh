20fce3019af755472c7cef76442c86708ff931bb
Pekka Enberg <penberg@cloudius-systems.com>
2014-06-04 09:57:23 +0300

    serial: Move register definitions to source file
    Move register definitions to isa-serial.cc where they are used. This
    makes looking up register values easier because you don't need to jump
    between two files.
    
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- drivers/isa-serial.hh
+++ drivers/isa-serial.hh
@@ -25,38 +25,6 @@ private:
     gsi_edge_interrupt* _irq;
     static const u16 ioport = 0x3f8;
 
-    enum IsaSerialValues {
-        // UART registers, offsets to ioport:
-        IER_ADDRESS = 1,    // Interrupt Enable Register
-        FCR_ADDRESS = 2,    // FIFO Control Register
-        LCR_ADDRESS = 3,    // Line Control Register
-        MCR_ADDRESS = 4,    // Modem Control Register
-        LSR_ADDRESS = 5,    // Line Control Register
-        MSR_ADDRESS = 6,    // Modem Status Register
-        SCR_ADDRESS = 7,    // Scratch Register
-        // When bit 7 (DLAB) of LCR is set to 1, the two registers 0 and 1
-        // change their meaning and become two bytes controlling the baud rate
-        LCR_DLAB = 0x80,    // Divisor Latch Access Bit in LCR register
-        LCR_8BIT = 3,
-        DLL_ADDRESS = 0,    // Divisor Latch LSB Register
-        DLM_ADDRESS = 1,    // Divisor Latch MSB Register
-        // Various bits of the Line Status Register
-        LSR_RECEIVE_DATA_READY = 0x1,
-        LSR_OVERRUN = 0x2,
-        LSR_PARITY_ERROR = 0x4,
-        LSR_FRAME_ERROR = 0x8,
-        LSR_BREAK_INTERRUPT = 0x10,
-        LSR_TRANSMIT_HOLD_EMPTY = 0x20,
-        LSR_TRANSMIT_EMPTY = 0x40,
-        LSR_FIFO_ERROR = 0x80,
-        // Various bits of the Modem Control Register
-        MCR_DTR = 0x1,
-        MCR_RTS = 0x2,
-        MCR_AUX_OUTPUT_1 = 0x4,
-        MCR_AUX_OUTPUT_2 = 0x8,
-        MCR_LOOPBACK_MODE = 0x16,
-    };
-
     virtual void dev_start();
     void reset();
     static void writeByte(const char letter);
