6c68c4c61676b047da29dd6b90f65a7382c4a141
Prasad Joshi <prasadjoshi.linux@gmail.com>
2014-06-17 01:11:28 +0530

    tests: add symlink test
    Signed-off-by: Prasad Joshi <prasadjoshi.linux@gmail.com>
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- build.mk
+++ build.mk
@@ -257,6 +257,7 @@ tests += tests/tst-yield.so
 tests += tests/misc-ctxsw.so
 tests += tests/tst-readdir.so
 tests += tests/tst-read.so
+tests += tests/tst-symlink.so
 tests += tests/tst-remove.so
 tests += tests/misc-wake.so
 tests += tests/tst-epoll.so
