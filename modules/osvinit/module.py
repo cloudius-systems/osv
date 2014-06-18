7c4913ebce14e0759a1686c25129b7d18edd62ca
Amnon Heiman <amnon@cloudius-systems.com>
2014-06-18 18:10:38 +0300

    OSv Init: add the module.py
    Package the yaml-cpp lib, the osvinit.so and the osvinit.yaml init file.
    By default the osvinit will not be started, but the httpserver will.
    
    To change that, the starting line can be uncommentted or the osvinit can
    be started with a -e directly.
    
    The osvinit and httpserver should not be both started.
    
    Signed-off-by: Amnon Heiman <amnon@cloudius-systems.com>
    
    module.py need merge
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

---
+++ modules/osvinit/module.py
@@ -0,0 +1,14 @@
+import os
+from osv.modules.api import *
+from osv.modules.filemap import FileMap
+from osv.modules import api
+
+_module = '${OSV_BASE}/mgmt/osvinit'
+
+usr_files = FileMap()
+usr_files.add(os.path.join('${OSV_BASE}', 'external/x64/misc.bin/usr/lib64/libyaml-cpp.so.0.5.1')).to('/usr/lib/libyaml-cpp.so.0.5')
+usr_files.add(os.path.join(_module, 'osvinit.so')).to('/usr/mgmt/osvinit.so')
+usr_files.add(os.path.join(_module, 'osvinit.yaml')).to('/usr/mgmt/osvinit.yaml')
+
+#default = api.run('/usr/mgmt/osvinit.so --skip-error --file /usr/mgmt/osvinit.yaml')
+default = ""
