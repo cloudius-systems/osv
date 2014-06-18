90fad5af391e5dfbf9891978e2d5aec72053686d
Amnon Heiman <amnon@cloudius-systems.com>
2014-06-18 18:10:35 +0300

    OSv Init: the main
    The main starts an osvinit task. It should get in the command line
    either a file name or a server name and path on the server as the init
    file.
    
    It uses a parser for passing the configuration to the osvinit
    
    It call the osvinit wait, it would return imidiatialy if the httpserver
    was not run.
    
    Signed-off-by: Amnon Heiman <amnon@cloudius-systems.com>
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

---
+++ modules/osvinit/main.cc
@@ -0,0 +1,75 @@
+/*
+ * Copyright (C) 2014 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+#include "osvinit.hh"
+#include <boost/program_options/variables_map.hpp>
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/parsers.hpp>
+
+using namespace std;
+using namespace init;
+namespace po = boost::program_options;
+
+int main(int argc, char* argv[])
+{
+    try
+    {
+
+        po::options_description desc("Allowed options");
+        desc.add_options()
+        ("help", "produce help message")
+        ("skip-error",
+         "do not stop on error")
+        ("once",
+         "when set, prevent a file to be read more than once")
+        ("file", po::value<std::string>(),
+         "an init file")
+        ("server", po::value<std::string>(),
+         "a server to read the file from. must come with a --url")
+        ("url", po::value<std::string>(),
+         "a url at the server")
+        ("port", po::value<std::string>()->default_value("80"),
+         "a port at the server")
+        ;
+
+        po::variables_map config;
+        po::store(po::parse_command_line(argc, argv, desc), config);
+        po::notify(config);
+
+        if (config.count("help")) {
+            std::cerr << desc << "\n";
+            return 1;
+        }
+        osvinit init(config.count("skip-error") > 0);
+        YAML::Node entry;
+
+        entry["GET"] = "/include";
+        if (config.count("file")) {
+            entry["path"] = config["file"].as<std::string>();
+        } else if (config.count("server") > 0 && config.count("url") > 0) {
+            entry["host"] = config["server"].as<std::string>();
+            entry["url"] = config["url"].as<std::string>();
+            entry["port"] = config["port"].as<std::string>();
+        } else {
+            std::cerr << desc << "\n";
+            return 1;
+        }
+        if (config.count("once")) {
+            entry["once"] = "True";
+        }
+        YAML::Node node;
+        node.push_back(entry);
+        init.do_yaml(node);
+        init.wait();
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << "Exception: " << e.what() << "\n";
+    }
+
+    return 0;
+}
