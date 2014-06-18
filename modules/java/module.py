569a7ba52b89fdd526cd639191ce149303411f11
Avi Kivity <avi@cloudius-systems.com>
2014-06-18 16:50:28 +0300

    java: fix RPATH trouble
    Newer openjdk has RPATH in some shared objects containing the full package
    path (/usr/lib/jvm/java-1.7.0-openjdk-$release.x864_64), and tries to load
    packages from that path.
    
    While we should evnetually place the files there, that's too large a change.
    Instead, keep the jdk in /usr/lib/jvm/java and create a symbolic link
    /usr/lib/jvm/java-1.7.0-openjdk-$release.x864_64 to point to that, so RPATH
    can be satisified.
    
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- modules/java/module.py
+++ modules/java/module.py
@@ -1,12 +1,31 @@
 from osv.modules.filemap import FileMap
 from osv.modules import api
+import os, os.path
 
 usr_files = FileMap()
 
 api.require('fonts')
 
-usr_files.add('${jdkbase}').to('/usr/lib/jvm') \
+jdkdir = os.path.basename(os.path.expandvars('${jdkbase}'))
+if not os.path.exists('diskimage/usr/lib/jvm'):
+    os.makedirs('diskimage/usr/lib/jvm')
+
+def symlink_force(dest, name):
+    if os.path.lexists(name):
+        os.unlink(name)
+    os.symlink(dest, name)
+
+symlink_force('java', 'diskimage/usr/lib/jvm/' + jdkdir)
+symlink_force('java/jre', 'diskimage/usr/lib/jvm/jre')
+
+usr_files.add('${jdkbase}').to('/usr/lib/jvm/java') \
     .include('lib/**') \
     .include('jre/**') \
     .exclude('jre/lib/security/cacerts') \
     .exclude('jre/lib/audio/**')
+usr_files.add(os.getcwd() + '/diskimage/usr/lib/jvm/' + jdkdir) \
+    .to('/usr/lib/jvm/' + jdkdir) \
+    .allow_symlink()
+usr_files.add(os.getcwd() + '/diskimage/usr/lib/jvm/jre') \
+    .to('/usr/lib/jvm/jre') \
+    .allow_symlink()
