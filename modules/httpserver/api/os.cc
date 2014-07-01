810541324ab244795178f651f5af607f6e99bee8
Nadav Har'El <nyh@cloudius-systems.com>
2014-07-01 14:03:18 +0300

    REST API: Add /os/thread request
    Add a /os/threads request that can be used to implement a top command
    (in a separate patch). The request returns the list of all threads,
    and for each thread specifies its numeric id, it's name, and its total
    CPU time usage. The list is also accompanied by the current time (this
    is useful when "top" is run over a slow network).
    
    All times are printed in millisecond resolution, which I deemed enough
    for the purpose of this API (we have nanosecond resolution, but it will
    needlessly increase the size of the output).
    
    In the future, additional fields such as a thread's CPU, priority, pinned
    status, state (running, waiting, etc.), stack size, etc., can be added.
    The fields I added now are the minimum selection of fields necessary for
    a useful "top" that helps finding CPU-hogging threads.
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>

--- modules/httpserver/api/os.cc
+++ modules/httpserver/api/os.cc
@@ -14,6 +14,7 @@
 #include <time.h>
 #include <osv/shutdown.hh>
 #include <osv/debug.hh>
+#include <osv/sched.hh>
 #include <api/unistd.h>
 
 extern char debug_buffer[DEBUG_BUFFER_SIZE];
@@ -96,6 +97,23 @@ void init(routes& routes)
         sethostname(hostname.c_str(), hostname.size());
         return formatter::to_json("");
     });
+
+    listThreads.set_handler("json", [](const_req req)
+    {
+        using namespace std::chrono;
+        httpserver::json::Threads threads;
+        threads.time_ms = duration_cast<milliseconds>
+            (osv::clock::wall::now().time_since_epoch()).count();
+        httpserver::json::Thread thread;
+        sched::with_all_threads([&](sched::thread &t) {
+            thread.id = t.id();
+            thread.cpu_ms = duration_cast<milliseconds>(t.thread_clock()).count();
+            thread.name = t.name();
+            threads.list.push(thread);
+        });
+        return formatter::to_json(threads);
+    });
+
 }
 
 }
