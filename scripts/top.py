48881fa3578a0483832b17d78d7c20145d44618b
Nadav Har'El <nyh@cloudius-systems.com>
2014-07-01 20:25:32 +0300

    scripts: top.py
    This patch adds a "top" script (scripts/top.py) which one runs on the
    host, and it connects to an OSv guest through the REST API. Every two
    seconds it makes an /os/thread request to get the list of all threads,
    and each one's total CPU time. Subtracting two consecutive lists provides
    us with the CPU time consumed by each thread during this period, which
    is then divided by the elapsed time to produce the CPU usage percentage
    of each thread. On each refresh, the top 20 threads are displayed.
    
    For example, during a single-threaded memcached run:
    
    155 threads on 4 CPUs; idle:  12%  77%  98%  99%
       ID  %CPU    TIME NAME
      216  91.5   19.30 >/memcached
       41  17.9    3.32 virtio-net-rx
        2   2.1    1.25 page-access-sca
        6   0.5    0.09 rcu0
       14   0.4    0.04 percpu1
       13   0.1    0.04 rcu1
        5   0.0    0.02 balancer0
        7   0.0    0.04 percpu0
       16   0.0    0.01 rcu2
    
    The idle threads are not shown in the main list, but rather on the top
    summary line, which says how many CPUs were found, and what percentage
    of the time this CPU was idle over the last period.
    
    The percentage, including idle percentage, is not very accurate, and
    numbers slightly below and even slightly over 100 can be expected for
    running threads, because of a quirk in thread::thread_clock's
    implementation, which cannot see a running thread's current time-slice.
    But because typical time-slices are much shorter than top's period
    (2 seconds), this problem is usually negligable.
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

---
+++ scripts/top.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+# Connect to a running OSv guest through the HTTP API and periodically display
+# the list of threads getting most CPU time, similarly to the Linux top(1)
+# command.
+#
+# The host:port to connect to can be specified on the command line, or
+# defaults to localhost:8000 (which is where "run.py --api" redirects the
+# guest's API port). If only a hostname is specified, port 8000 is used by
+# default.
+
+import urllib.request
+import json
+import time
+import sys
+import collections
+
+try:
+    import curses
+    curses.setupterm()
+    clear = curses.tigetstr('clear').decode()
+except:
+    clear = '\033[H\033[2J'
+
+hostport = 'localhost:8000'
+if len(sys.argv) > 1:
+    if ':' in sys.argv[1]:
+        hostport = sys.argv[1]
+    else:
+        hostport = sys.argv[1] + ":8000"
+
+period = 2.0  # How many seconds between refreshes
+
+prevtime = collections.defaultdict(int)
+name = dict()
+timems = 0
+while True:
+    start_refresh = time.time()
+    result_json = urllib.request.urlopen("http://" + hostport + "/os/threads").read().decode()
+    result = json.loads(result_json)
+    print(clear, end='')
+    newtimems = result['time_ms']
+
+    print("%d threads " % (len(result['list'])), end='')
+    diff = dict()
+    idles = dict()
+    for thread in result['list']:
+        id = thread['id']
+        cpums = thread['cpu_ms']
+        if timems:
+            diff[id] = cpums - prevtime[id]
+        prevtime[id] = cpums
+        name[id] = thread['name']
+        # Display per-cpu idle threads differently from normal threads
+        if thread['name'].startswith('idle') and timems:
+            idles[thread['name']] = diff[id]
+            del diff[id]
+
+    if idles:
+        print("on %d CPUs; idle: " % (len(idles)), end='')
+        for n in sorted(idles):
+            percent = 100.0*idles[n]/(newtimems - timems)
+            print ("%3.0f%% "%percent, end='')
+    print("")
+
+    print("%5s  %s %7s %s" % ("ID", "%CPU", "TIME", "NAME"))
+    for id in sorted(diff, key=diff.get, reverse=True)[:20]:
+        percent = 100.0*diff[id]/(newtimems - timems)
+        print("%5d %5.1f %7.2f %s" % (id, percent, prevtime[id]/1000.0, name[id]))
+    timems = newtimems
+    time.sleep(max(0, period - (time.time() - start_refresh)))
