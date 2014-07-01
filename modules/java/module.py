6a292524742470346145e9a0ec33783fc2891304
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2014-07-01 14:09:02 +0200

    modules: java: Simplify creation of links
    Reviewed-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Tomasz Grabiec <tgrabiec@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- modules/java/module.py
+++ modules/java/module.py
@@ -7,25 +7,11 @@ usr_files = FileMap()
 api.require('fonts')
 
 jdkdir = os.path.basename(os.path.expandvars('${jdkbase}'))
-if not os.path.exists('diskimage/usr/lib/jvm'):
-    os.makedirs('diskimage/usr/lib/jvm')
-
-def symlink_force(dest, name):
-    if os.path.lexists(name):
-        os.unlink(name)
-    os.symlink(dest, name)
-
-symlink_force('java', 'diskimage/usr/lib/jvm/' + jdkdir)
-symlink_force('java/jre', 'diskimage/usr/lib/jvm/jre')
 
 usr_files.add('${jdkbase}').to('/usr/lib/jvm/java') \
     .include('lib/**') \
     .include('jre/**') \
     .exclude('jre/lib/security/cacerts') \
     .exclude('jre/lib/audio/**')
-usr_files.add(os.getcwd() + '/diskimage/usr/lib/jvm/' + jdkdir) \
-    .to('/usr/lib/jvm/' + jdkdir) \
-    .allow_symlink()
-usr_files.add(os.getcwd() + '/diskimage/usr/lib/jvm/jre') \
-    .to('/usr/lib/jvm/jre') \
-    .allow_symlink()
+usr_files.link('/usr/lib/jvm/' + jdkdir).to('java')
+usr_files.link('/usr/lib/jvm/jre').to('java/jre')
