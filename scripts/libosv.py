4134ff17a249538a08635f5482421fa77bc3ebc0
Nadav Har'El <nyh@cloudius-systems.com>
2014-07-04 01:25:55 +0300

    libosv.so: Add missing weak symbols
    We define a lot of symbols using weak_alias(). Unfortunately,
    scripts/libosv.py only copied global-binding symbols, not those with weak
    binding. We also need to copy those with weak binding.
    
    Note that they are not currently marked as weak symbols on the resulting
    libosv.so. This will not make a practical difference, because OSv doesn't
    respect the weakness of symbols anyway.
    
    Fixes #365.
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- scripts/libosv.py
+++ scripts/libosv.py
@@ -2,7 +2,7 @@
 import sys
 import re
 
-elf = re.compile(r"\s*?\d+:\s*([0-9a-f]+)\s+\d+\s+(FUNC|OBJECT)\s+GLOBAL\s+DEFAULT\s+\d+\s+(\w+)\s*")
+elf = re.compile(r"\s*?\d+:\s*([0-9a-f]+)\s+\d+\s+(FUNC|OBJECT)\s+(GLOBAL|WEAK)\s+DEFAULT\s+\d+\s+(\w+)\s*")
 
 f = open(sys.argv[1])
 out = open(sys.argv[2], "w")
@@ -28,7 +28,7 @@ version = """
 
 for line in f.readlines():
     try:
-        value,tp,sym = elf.match(line).groups()
+        value,tp,bnd,sym = elf.match(line).groups()
         out.write("%s = 0x%s;\n"%(sym, value))
         print (".global %s\n.type %s,@%s"%(sym, sym, asm_type[tp]))
     except AttributeError:
