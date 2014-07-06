7b9e83e2c2e42805268517fb8ce40477873a326d
Avi Kivity <avi@cloudius-systems.com>
2014-07-06 13:12:07 +0300

    epoll: properly lock epoll file
    epoll operations can be executed concurrently, protect against this using
    file::f_lock.
    
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- core/epoll.cc
+++ core/epoll.cc
@@ -74,76 +74,87 @@ public:
     }
     int add(epoll_key key, struct epoll_event *event)
     {
-        if (map.count(key)) {
-            return EEXIST;
-        }
-        auto fp = key._file;
-        WITH_LOCK(fp->f_lock) {
-            // I used poll_wake_count-1, to ensure EPOLLET returns once when
-            // registering an epoll after data is already available.
-            map.emplace(key,
-                    registered_epoll(*event, fp->poll_wake_count - 1));
-            if (!fp->f_epolls) {
-                fp->f_epolls.reset(new std::vector<epoll_ptr>);
+        WITH_LOCK(f_lock) {
+            if (map.count(key)) {
+                return EEXIST;
             }
-            fp->f_epolls->push_back(epoll_ptr{this, key});
+            auto fp = key._file;
+            WITH_LOCK(fp->f_lock) {
+                // I used poll_wake_count-1, to ensure EPOLLET returns once when
+                // registering an epoll after data is already available.
+                map.emplace(key,
+                        registered_epoll(*event, fp->poll_wake_count - 1));
+                if (!fp->f_epolls) {
+                    fp->f_epolls.reset(new std::vector<epoll_ptr>);
+                }
+                fp->f_epolls->push_back(epoll_ptr{this, key});
+            }
+            return 0;
         }
-        return 0;
     }
     int mod(epoll_key key, struct epoll_event *event)
     {
-        auto fp = key._file;
-        try {
-            WITH_LOCK(fp->f_lock) {
-                map.at(key) = registered_epoll(*event, fp->poll_wake_count - 1);
+        WITH_LOCK(f_lock) {
+            auto fp = key._file;
+            try {
+                WITH_LOCK(fp->f_lock) {
+                    map.at(key) = registered_epoll(*event, fp->poll_wake_count - 1);
+                }
+                return 0;
+            } catch (std::out_of_range &e) {
+                return ENOENT;
             }
-            return 0;
-        } catch (std::out_of_range &e) {
-            return ENOENT;
         }
     }
     int del(epoll_key key)
     {
-        if (map.erase(key)) {
-            remove_me(key);
-            return 0;
-        } else {
-            return ENOENT;
+        WITH_LOCK(f_lock) {
+            if (map.erase(key)) {
+                remove_me(key);
+                return 0;
+            } else {
+                return ENOENT;
+            }
         }
     }
     int wait(struct epoll_event *events, int maxevents, int timeout_ms)
     {
         std::vector<poll_file> pollfds;
         std::vector<epoll_key> keys;
-        pollfds.reserve(map.size());
-        keys.reserve(map.size());
-        for (auto &i : map) {
-            int eevents = i.second.events;
-            auto events = events_epoll_to_poll(eevents);
-            pollfds.emplace_back(i.first._file, events, 0,
-                    i.second.last_poll_wake_count);
-            keys.emplace_back(i.first);
-        }
-        int r = do_poll(pollfds, parse_poll_timeout(timeout_ms));
-        if (r > 0) {
-            r = std::min(r, maxevents);
-            int remain = r;
-            for (size_t i = 0; i < pollfds.size() && remain;  i++) {
-                if (pollfds[i].revents) {
-                    --remain;
-                    assert(pollfds[i].fp);
-                    events[remain].data = map.at(keys[i]).data;
-                    events[remain].events =
-                            events_poll_to_epoll(pollfds[i].revents);
-                    trace_epoll_ready(pollfds[i].fp.get(), pollfds[i].revents);
-                    if (pollfds[i].events & EPOLLET) {
-                        map.at(keys[i]).last_poll_wake_count =
-                                pollfds[i].last_poll_wake_count;
+        WITH_LOCK(f_lock) {
+            pollfds.reserve(map.size());
+            keys.reserve(map.size());
+            for (auto &i : map) {
+                int eevents = i.second.events;
+                auto events = events_epoll_to_poll(eevents);
+                pollfds.emplace_back(i.first._file, events, 0,
+                        i.second.last_poll_wake_count);
+                keys.emplace_back(i.first);
+            }
+            int r;
+            DROP_LOCK(f_lock) {
+                r = do_poll(pollfds, parse_poll_timeout(timeout_ms));
+            }
+            if (r > 0) {
+                r = std::min(r, maxevents);
+                int remain = r;
+                for (size_t i = 0; i < pollfds.size() && remain;  i++) {
+                    if (pollfds[i].revents) {
+                        --remain;
+                        assert(pollfds[i].fp);
+                        events[remain].data = map.at(keys[i]).data;
+                        events[remain].events =
+                                events_poll_to_epoll(pollfds[i].revents);
+                        trace_epoll_ready(pollfds[i].fp.get(), pollfds[i].revents);
+                        if (pollfds[i].events & EPOLLET) {
+                            map.at(keys[i]).last_poll_wake_count =
+                                    pollfds[i].last_poll_wake_count;
+                        }
                     }
                 }
             }
+            return r;
         }
-        return r;
     }
 private:
     void remove_me(epoll_key key) {
