1a990af853965dab0ce7ab8d8301983e25c94765
Pekka Enberg <penberg@cloudius-systems.com>
2014-07-09 11:11:11 +0300

    modules/osvinit: Fix module path
    The OSv init module now lives in modules/osvinit, not mgmt/osvinit.
    
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- modules/osvinit/module.py
+++ modules/osvinit/module.py
@@ -3,7 +3,7 @@ from osv.modules.api import *
 from osv.modules.filemap import FileMap
 from osv.modules import api
 
-_module = '${OSV_BASE}/mgmt/osvinit'
+_module = '${OSV_BASE}/modules/osvinit'
 
 usr_files = FileMap()
 usr_files.add(os.path.join('${OSV_BASE}', 'external/x64/misc.bin/usr/lib64/libyaml-cpp.so.0.5.1')).to('/usr/lib/libyaml-cpp.so.0.5')
