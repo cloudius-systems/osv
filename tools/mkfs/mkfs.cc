ae6677fe056e1e173cda7b94055832f5f14fd644
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2014-07-11 03:53:21 -0300

    mkfs: Automatic detection and insertion of additional vblk devices
    mkfs code is changed to detect additional vblk devices, and insert
    them into the vblk devices argument list for the pool creation.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- tools/mkfs/mkfs.cc
+++ tools/mkfs/mkfs.cc
@@ -6,6 +6,7 @@
  */
 
 #include <assert.h>
+#include <string.h>
 #include <osv/device.h>
 #include <osv/run.hh>
 #include <fs/vfs/vfs.h>
@@ -17,14 +18,40 @@ using namespace std;
 
 // Created to guarantee that shared objects resources will
 // be surely released at the function prologue.
-void run_cmd(const char *cmdpath, vector<string> args)
+static void run_cmd(const char *cmdpath, vector<string> args)
 {
     int ret;
     auto ok = run(cmdpath, args, &ret);
     assert(ok && ret == 0);
 }
 
-void mkfs()
+// Get extra blk devices for pool creation.
+static void get_blk_devices(vector<string> &zpool_args)
+{
+    DIR *dir;
+    struct dirent *entry;
+
+    dir = opendir("/dev/");
+    assert(dir != nullptr);
+
+    while ((entry = readdir(dir)) != nullptr) {
+        if (!strstr(entry->d_name, "vblk")) {
+            continue;
+        }
+
+        // Skip vblk0: partition where kernel text/data lives in.
+        // Skip vblk0.1: partition unconditionally used for pool creation.
+        if (strstr(entry->d_name, "vblk0")) {
+            continue;
+        }
+
+        zpool_args.push_back("/dev/" + string(entry->d_name));
+    }
+
+    closedir(dir);
+}
+
+static void mkfs(void)
 {
     // Create zfs device, then /etc/mnttab which is required by libzfs
     zfsdev::zfsdev_init();
@@ -35,9 +62,13 @@ void mkfs()
     assert(fd != -1);
     close(fd);
 
+    vector<string> zpool_args = {"zpool", "create", "-f", "-R", "/zfs", "osv",
+        "/dev/vblk0.1"};
+
+    get_blk_devices(zpool_args);
+
     // Create zpool named osv
-    run_cmd("/zpool.so",
-        {"zpool", "create", "-f", "-R", "/zfs", "osv", "/dev/vblk0.1"});
+    run_cmd("/zpool.so", zpool_args);
 
     // Create a zfs dataset within the pool named osv.
     run_cmd("/zfs.so", {"zfs", "create", "-o", "relatime=on", "osv/zfs"});
@@ -52,5 +83,6 @@ int main(int ac, char** av)
     cout << "Running mkfs...\n";
     mkfs();
     sync();
+    return 0;
 }
 
