38d8dd1347cb29c35e904ef02620e9e28e97f534
Prasad Joshi <prasadjoshi.linux@gmail.com>
2014-07-17 08:55:45 +0530

    libzfs: disable BSD specific geom_gettree call
    Reviewed-by: Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
    Signed-off-by: Prasad Joshi <prasadjoshi.linux@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- bsd/cddl/contrib/opensolaris/lib/libzfs/common/libzfs_import.c
+++ bsd/cddl/contrib/opensolaris/lib/libzfs/common/libzfs_import.c
@@ -1196,6 +1196,7 @@ zpool_find_import_impl(libzfs_handle_t *hdl, importargs_t *iarg)
 		avl_create(&slice_cache, slice_cache_compare,
 		    sizeof (rdsk_node_t), offsetof(rdsk_node_t, rn_node));
 
+#ifndef __OSV__
 		if (strcmp(rdsk, "/dev/") == 0) {
 			struct gmesh mesh;
 			struct gclass *mp;
@@ -1227,6 +1228,7 @@ zpool_find_import_impl(libzfs_handle_t *hdl, importargs_t *iarg)
 			geom_deletetree(&mesh);
 			goto skipdir;
 		}
+#endif
 
 		/*
 		 * This is not MT-safe, but we have no MT consumers of libzfs
@@ -1245,7 +1247,10 @@ zpool_find_import_impl(libzfs_handle_t *hdl, importargs_t *iarg)
 			slice->rn_nozpool = B_FALSE;
 			avl_add(&slice_cache, slice);
 		}
+
+#ifndef __OSV__
 skipdir:
+#endif
 		/*
 		 * create a thread pool to do all of this in parallel;
 		 * rn_nozpool is not protected, so this is racy in that
