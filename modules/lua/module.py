fce06794733fff9f75e8bbf7511a47f6fcbf0dff
Or Cohen <orc@fewbytes.com>
2014-07-23 16:23:59 +0300

    Lua: Install modules with LuaRocks
    Replace the manual building and installation of Lua modules with LuaRocks,
    a management system for Lua modules.
    
    Added the regular Lua executable to execute LuaRocks.
    LuaRocks libraries and executables are excluded from the image, since it's only
    needed in build time.
    
    Signed-off-by: Or Cohen <orc@fewbytes.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- modules/lua/module.py
+++ modules/lua/module.py
@@ -3,4 +3,8 @@ from osv.modules.filemap import FileMap
 usr_files = FileMap()
 usr_files.add('${OSV_BASE}/modules/lua/src/liblua.so').to('/usr/lib/liblua.so')
 
-usr_files.add('${OSV_BASE}/modules/lua/out').to('/usr')
+usr_files.add('${OSV_BASE}/modules/lua/out').to('/usr') \
+	.exclude('bin/*') \
+	.exclude('etc/*') \
+	.exclude('lib/luarocks/**') \
+	.exclude('share/lua/*/luarocks/**')
