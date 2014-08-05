364207701f20e1d326e6d34767f5f12ebbdead22
Amnon Heiman <amnon@cloudius-systems.com>
2014-08-05 10:48:31 +0300

    Http server: Expose ifconfig infromation in the API
    This adds the equivalent of the ifconfig information to the API.
    /network/ifconfig will return a list of all the interfaces with their
    configuration and data.
    To check a specific interface (ie. lo0) do:
    /network/ifconfig/lo0
    
    Interfaces information returns with timestamp. Currently all interfaces
    in a single call have the same timestamp, we assume that the
    difference is neglegtable, but API support specific
    timestamp per interface.
    
    Signed-off-by: Amnon Heiman <amnon@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- modules/httpserver/api/network.cc
+++ modules/httpserver/api/network.cc
@@ -7,8 +7,10 @@
 #include "network.hh"
 #include "autogen/network.json.hh"
 #include "tools/route/route_info.hh"
+#include "tools/ifconfig/network_interface.hh"
 #include "exception.hh"
 #include <vector>
+#include <osv/clock.hh>
 
 namespace httpserver {
 
@@ -18,6 +20,28 @@ namespace network {
 using namespace json;
 using namespace std;
 using namespace httpserver::json;
+using namespace osv::network;
+using namespace std::chrono;
+
+static Interface get_interface(const string& name, ifnet* ifp, long time)
+{
+    interface_config ifc;
+    interface_data ifd;
+    Interface f;
+    interface intf(name);
+
+    ifc.set(intf);
+    f.config = ifc;
+    if_data cur_data = { 0 };
+    if (!set_interface_info(ifp, cur_data)) {
+        throw server_error_exception("Failed getting interface information");
+    }
+
+    ifd.set(cur_data);
+    f.data = ifd;
+    f.time = time;
+    return f;
+}
 
 /**
  * Initialize the routes object with specific routes mapping
@@ -26,9 +50,34 @@ using namespace httpserver::json;
 void init(routes& routes)
 {
     network_json_init_path();
+    network_json::listIfconfig.set_handler([](const_req req) {
+        vector<Interface> res;
+        auto time = duration_cast<microseconds>
+                    (osv::clock::uptime::now().time_since_epoch()).count();
+        for (unsigned int i = 0; i <= number_of_interfaces(); i++) {
+            auto* ifp = get_interface_by_index(i);
+
+            if (ifp != nullptr) {
+                res.push_back(get_interface(get_interface_name(ifp), ifp, time));
+            }
+        }
+        return res;
+    });
+
+    network_json::getIfconfig.set_handler([](const_req req) {
+        string name = req.param.at("if").substr(1);
+        interface intf(name);
+        ifnet* ifp = get_interface_by_name(name);
+        if (ifp == nullptr) {
+            throw not_found_exception(string("Interface ") + name + " not found");
+        }
+        auto time = duration_cast<microseconds>(osv::clock::uptime::now().time_since_epoch()).count();
+        return get_interface(name, ifp, time);
+    });
+
     network_json::getRoute.set_handler([](const_req req) {
         vector<Route> res;
-        osv::foreach_route([&res](const osv::route_info& route){
+        osv::foreach_route([&res](const osv::route_info& route) {
             res.push_back(Route());
             res.back().set(route);
             return true;
