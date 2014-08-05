7cb183c9fa4fe48653c3560d8881b14dc0b8e18e
Avi Kivity <avi@cloudius-systems.com>
2014-08-05 15:12:35 +0300

    net: drop custom net_channel allocators
    Once upon a time, allocating a page was much faster than allocating half
    a page, so we used a custom allocator.
    
    These days, it's just as fast, and the custom allocator interferes with
    memory debugging.
    
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- include/osv/net_channel.hh
+++ include/osv/net_channel.hh
@@ -58,11 +58,6 @@ public:
     void del_poller(pollreq& pr);
     void add_epoll(const epoll_ptr& ep);
     void del_epoll(const epoll_ptr& ep);
-    static void* operator new (size_t size) {
-        static_assert(sizeof(net_channel) <= 4096, "net_channel too big");
-        return memory::alloc_page();
-    }
-    void operator delete(void *ptr) { memory::free_page(ptr); }
 private:
     void wake_pollers();
 private:
