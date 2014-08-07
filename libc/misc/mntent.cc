00671abab7078b3020673226b0b85a35f73a3630
Prasad Joshi <prasadjoshi.linux@gmail.com>
2014-08-08 00:25:31 +0530

    libc: add special file class to implement *mnt* functions
    Few well known files like /etc/mtab, /etc/mnttab, or /proc/mounts gives
    us view of mounted file systems. The APIs like getmntent, setmntent
    etc are simple interfaces to work with these files. However, OSv does
    not manage any of these well know files. Therefore, in order for these
    functions to work, the mounted file system information is generated
    from memory.
    
    This patch implements a new class, call mtab_file, inherited from
    special_file class. It allows us to implement all the standard file
    operations.
    
    This patch is essential for ZFS mount, unmount, and export to work
    correctly. ZFS has its own mechanism to mount or unmount a ZFS file
    system. For example: zfs mount command mounts a ZFS file system and
    zfs unmount command unmounts a ZFS file system. zfs export command,
    exports an earlier imported pool. When a pool is exported all the file
    systems created and mounted must be unmounted before a pool can be
    exported. Therefore, ZFS needs to have an exact view of currently
    mounted file systems. ZFS uses getmntent function to read currently
    mouted file systems.
    
    Reviewed-by: Glauber Costa <glommer@cloudius-systems.com>
    Signed-off-by: Prasad Joshi <prasadjoshi.linux@gmail.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- libc/misc/mntent.cc
+++ libc/misc/mntent.cc
@@ -3,6 +3,78 @@
 #include <mntent.h>
 #include <errno.h>
 #include <osv/mount.h>
+#include <fs/fs.hh>
+#include <sstream>
+#include <libc/libc.hh>
+
+class mtab_file final : public special_file {
+    public:
+        mtab_file() : special_file(FREAD, DTYPE_UNSPEC) {}
+
+        int close() { return 0; }
+
+        virtual int read(struct uio *uio, int flags) override;
+};
+
+int mtab_file::read(struct uio *uio, int flags)
+{
+    auto   fp = this;
+
+    size_t skip = uio->uio_offset;
+    if ((flags & FOF_OFFSET) == 0) {
+        skip = fp->f_offset;
+    }
+
+    int    j        = 0; /* IOV index */
+    int    iov_skip = 0;
+    size_t bc       = 0;
+
+    std::vector<osv::mount_desc> mounts = osv::current_mounts();
+
+    for (size_t i = 0; i < mounts.size() && uio->uio_resid > 0; i++) {
+        auto&        m = mounts[i];
+        std::string  line;
+
+        line = m.special + " " + m.path + " " + m.type + " " +
+            (m.options.size() ? m.options : MNTOPT_DEFAULTS) + " 0 0\n";
+
+        if (skip > 0) {
+            if (skip >= line.size()) {
+                skip -= line.size();
+                continue;
+            }
+        }
+
+        size_t     t  = line.size()  - skip;
+        const char *q = line.c_str() + skip;
+
+        for (; t > 0 && uio->uio_resid > 0; ) {
+            struct iovec *iov = uio->uio_iov + j;
+            char         *p   = static_cast<char *> (iov->iov_base) + iov_skip;
+            size_t       n    = std::min(iov->iov_len - iov_skip, t);
+
+            std::copy(q, q + n, p);
+
+            if (n == t) {
+                iov_skip += n;
+            } else {
+                iov_skip = 0;
+                j++;
+            }
+
+            t  -= n;
+            q  += n;
+            bc += n;
+        }
+        skip = 0;
+    }
+
+    if ((flags & FOF_OFFSET) == 0) {
+        fp->f_offset += bc;
+    }
+    uio->uio_resid -= bc;
+    return 0;
+}
 
 FILE *setmntent(const char *name, const char *mode)
 {
@@ -11,37 +83,30 @@ FILE *setmntent(const char *name, const char *mode)
             return nullptr;
         }
 
-        return OSV_DYNMOUNTS;
+        fileref f = make_file<mtab_file>();
+        struct file * fp = f.get();
+        if (fp == NULL) {
+            return nullptr;
+        }
+
+        fhold(fp);
+
+        int fd;
+        int rc = fdalloc(fp, &fd);
+        fdrop(fp);
+        if (rc) {
+            return nullptr;
+        }
+
+        return fdopen(fd, "r");
     }
     return fopen(name, mode);
 }
 
 int endmntent(FILE *f)
 {
-    if (f != OSV_DYNMOUNTS) {
-        fclose(f);
-    }
+    fclose(f);
     return 1;
-}
-
-bool osv_getmntent(char *linebuf, int buflen)
-{
-    // FIXME: we're using a static here in lieu of the file offset
-    static size_t last = 0;
-    auto mounts = osv::current_mounts();
-    if (last >= mounts.size()) {
-        last = 0;
-        return false;
-    } else {
-        auto& m = mounts[last++];
-
-        snprintf(linebuf, buflen, " %s %s %s %s 0 0",
-                 m.special.c_str(),
-                 m.path.c_str(),
-                 m.type.c_str(),
-                 m.options.size() ? m.options.c_str() : MNTOPT_DEFAULTS);
-        return true;
-    }
 }
 
 struct mntent *getmntent_r(FILE *f, struct mntent *mnt, char *linebuf, int buflen)
@@ -56,19 +121,11 @@ struct mntent *getmntent_r(FILE *f, struct mntent *mnt, char *linebuf, int bufle
     mnt->mnt_passno = 0;
 
     do {
-
-        if (f == OSV_DYNMOUNTS) {
-            bool ret = osv_getmntent(linebuf, buflen);
-            if (!ret) {
-                return nullptr;
-            }
-        } else {
             fgets(linebuf, buflen, f);
             if (feof(f) || ferror(f)) return 0;
             if (!strchr(linebuf, '\n')) {
                 fscanf(f, "%*[^\n]%*[\n]");
                 return nullptr;
-            }
         }
         cnt = sscanf(linebuf, " %n%*s%n %n%*s%n %n%*s%n %n%*s%n %d %d",
             n, n+1, n+2, n+3, n+4, n+5, n+6, n+7,
@@ -95,11 +152,27 @@ struct mntent *getmntent(FILE *f)
     return getmntent_r(f, &mnt, linebuf, sizeof linebuf);
 }
 
+bool is_mtab_file(FILE *fp)
+{
+    fileref fr(fileref_from_fd(fileno(fp)));
+
+    if (!fr) {
+        return false;
+    }
+
+    auto mpo = dynamic_cast<mtab_file *>(fr.get());
+    if (!mpo) {
+        return false;
+    }
+    return true;
+}
+
 int addmntent(FILE *f, const struct mntent *mnt)
 {
-    if (f == OSV_DYNMOUNTS) {
+    if (is_mtab_file(f)) {
         return 1;
     }
+
     if (fseek(f, 0, SEEK_END)) return 1;
     return fprintf(f, "%s\t%s\t%s\t%s\t%d\t%d\n",
         mnt->mnt_fsname, mnt->mnt_dir, mnt->mnt_type, mnt->mnt_opts,
