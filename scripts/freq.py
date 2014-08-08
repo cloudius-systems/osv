0d2aaf6ea4ece9beb7c6d0dc7ac8500e0a3257e1
Nadav Har'El <nyh@cloudius-systems.com>
2014-08-08 23:59:02 +0300

    add "freq.py" tool to show tracepoint frequency in a running OSv guest
    This patch adds a tool "freq.py" which can be used from outside the OSv
    guest to find the frequency of *any* tracepoint. This is done efficiently
    (see tracepoint_counter) and does not involve fully enabling (logging)
    this tracepoint. It can even be used for tracepoints which occur
    millions a time each second.
    
    Freq.py takes the guest's address:port (the port defaults to 8000), and
    a list of tracepoints whose frequency to display. To just see the whole
    list of tracepoints to choose from, run freq.py Without parameters
    (except the host). The frequency of each selected tracepoint (number of
    occurances per second) is averaged over 2-second periods and displayed
    every 2 seconds.
    
    Here is an example of the output for three tracepoints on a guest
    running memcached, before and after "memaslap" started exercising it:
    
    $ scripts/freq.py 192.168.122.89 sched_switch mutex_lock memory_malloc
    Showing number of events per second. Refreshing every 2 seconds
       sched_switch     mutex_lock  memory_malloc
               2650           4057            265
               2274           1298            163
               2033           1665            175
               3447           6344            343
              52782         805701         227800  <--- memaslap started here
              78631        1068659         304723
              84144        1060092         301155
    
    To reiterate, you *do not* need to enable tracepoints in the traditional
    sense (e.g., the "--trace ..." paramters to run.py) for this to work,
    and you can ask freq.py to count *any* existing tracepoint.
    The counted tracepoints are only counted - not logged to the trace log
    (of course, you can do both if you wish).
    
    One caveat on this implementation:
    
    By design, the REST API is session-less. When freq.py enables a counter,
    the kernel will never stop counting it - until freq.py asks it to stop
    counting. We were careful about stopping all counters when freq.py exits
    (even if it is killed by a signal), but this can still happen - e.g., if
    the host running freq.py dies. Moreover, freq.py is currently not designed
    to support two freq.py runs on the same OSv guest concurrently. As the
    code is written now, the second concurrent freq.py invocation will cancel
    all the counters requested by the first invocation, and confuse it.
    This situation can be fixed, but not easily (will involve some sort of
    reference counting?) but I'll leave it to a follow-up patch. I think
    this patch is already quite useful as-is.
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

---
+++ scripts/freq.py
@@ -0,0 +1,107 @@
+#!/usr/bin/env python3
+# Connect to a running OSv guest through the HTTP API and periodically
+# display the the frequency of chosen tracepoints.
+#
+# Usage: freq.py host[:port] [tracepoint] ...
+#
+# If the list of tracepoints whose frequencies to display is missing,
+# freq.py will display, together with the "Usage:" message, also
+# the list of all available tracepoints.
+
+import urllib.request
+import json
+import time
+import sys
+import collections
+import signal
+
+hostport = 'localhost:8000'
+if len(sys.argv) > 1:
+    if ':' in sys.argv[1]:
+        hostport = sys.argv[1]
+    else:
+        hostport = sys.argv[1] + ":8000"
+
+if len(sys.argv) <= 2:
+    print("Usage: %s host[:port] [tracepoint] ...\n" % sys.argv[0])
+    print("Valid values for <tracepoint>:\n")
+    # Display list of valid tracepoints
+    result_json = urllib.request.urlopen("http://" + hostport + "/trace/status").read().decode()
+    result = json.loads(result_json)
+    tracepoints = set()
+    for tracepoint in result:
+        tracepoints.add(tracepoint['name'])
+    for tracepoint in sorted(tracepoints):
+        print(tracepoint)
+    sys.exit()
+        
+        
+tracepoint_names= sys.argv[2:]
+
+period = 2.0  # How many seconds between refreshes
+
+print("Showing number of events per second. Refreshing every %g seconds" % period)
+
+def make_request(url, data, method):
+    opener = urllib.request.build_opener(urllib.request.HTTPHandler)
+    request = urllib.request.Request(url, data)
+    request.get_method = lambda: method
+    url = opener.open(request)
+    return url.read().decode()
+
+def delete_all_counters():
+    make_request('http://' + hostport + '/trace/count', None, 'DELETE')
+
+def enable_counter(name):
+    make_request('http://' + hostport + '/trace/count/' + name, 'enabled=true'.encode(), 'POST')
+
+delete_all_counters()
+
+def signal_handler(signal, frame):
+    delete_all_counters()
+    sys.exit(0)
+signal.signal(signal.SIGINT, signal_handler)
+signal.signal(signal.SIGTERM, signal_handler)
+signal.signal(signal.SIGHUP, signal_handler)
+
+for t in tracepoint_names:
+    try:
+        enable_counter(t)
+    except:
+        print("Unrecognized tracepoint %s" % t)
+        delete_all_counters()
+        sys.exit()
+        
+def get_counts():
+    result_json = make_request('http://' + hostport + '/trace/count', None, 'GET')
+    return json.loads(result_json)
+prevcount = collections.defaultdict()
+
+nline = 0
+timems = 0
+while True:
+    if (nline % 20) == 0:
+        for t in tracepoint_names:
+            print('%15s' % t, end='')
+        print();
+    nline = nline + 1
+
+    start_refresh = time.time()
+    result = get_counts()
+    newtimems = result['time_ms']
+    freq = dict()
+    for i in result['list']:
+        name = i['name'];
+        count = i['count'];
+        if timems > 0:
+            freq[name] = (count - prevcount[name]) / ((newtimems - timems) / 1000.0)
+        prevcount[name] = count
+    if timems > 0:
+        for t in tracepoint_names:
+            print("%15.0f" % freq[t], end='')
+        print()
+    time.sleep(max(0, period - (time.time() - start_refresh)))
+    timems = newtimems
+
+    
+delete_all_counters()
