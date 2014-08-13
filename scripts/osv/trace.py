cloudius-systems/osv

commit 3730c3953ca0cbae940f41134f73764ec1abb9d1

trace: show thread name in 'trace.py prof'
Applies when grouping by thread.

Before:

  === Thread 0xffff8001170c3050 ===

After:

  === Thread >>>>>>>java.so (0xffff8001170c3050) ===

Signed-off-by: Tomasz Grabiec <tgrabiec@cloudius-systems.com>
Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

scripts/osv/trace.py
@@ -95,14 +95,17 @@ class TracePoint:
                 self.format)
 
 class Trace:
-    def __init__(self, tp, thread, thread_name, time, cpu, data, backtrace=None):
+    def __init__(self, tp, thread, time, cpu, data, backtrace=None):
         self.tp = tp
         self.thread = thread
-        self.thread_name = thread_name
         self.time = time
         self.cpu = cpu
         self.data = data
         self.backtrace = backtrace
+
+    @property
+    def thread_name(self):
+        return self.thread.name
 
     @property
     def name(self):
@@ -120,8 +123,8 @@ class Trace:
             data_formatter = self.format_data
 
         return '0x%016x %-15s %2d %19s %-20s %s%s' % (
-            self.thread,
-            self.thread_name,
+            self.thread.ptr,
+            self.thread.name,
             self.cpu,
             format_time(self.time),
             self.name,
@@ -392,6 +395,14 @@ class TraceDumpReader :
         iters = map(lambda data: self.oneTrace(data), self.trace_buffers)
         return heapq.merge(*iters)
 
+class Thread(object):
+    def __init__(self, ptr, name):
+        self.ptr = ptr
+        self.name = name
+
+    def __str__(self):
+        return "%s (0x%x)" % (self.name, self.ptr)
+
 def read(buffer_view):
     unpacker = SlidingUnpacker(buffer_view)
     version, = unpacker.unpack('i')
@@ -407,7 +418,7 @@ def read(buffer_view):
             unpacker.unpack_str(), unpacker.unpack_str())
 
     while unpacker:
-        tp_key, thread, thread_name, time, cpu = unpacker.unpack('QQ16sQI')
+        tp_key, thread_ptr, thread_name, time, cpu = unpacker.unpack('QQ16sQI')
         thread_name = thread_name.rstrip('\0')
         tp = tracepoints[tp_key]
 
@@ -419,7 +430,7 @@ def read(buffer_view):
             backtrace.append(frame)
 
         data = unpacker.unpack(tp.signature)
-        yield Trace(tp, thread, thread_name, time, cpu, data, backtrace=backtrace)
+        yield Trace(tp, Thread(thread_ptr, thread_name), time, cpu, data, backtrace=backtrace)
 
 def write(traces, writer):
     packer = WritingPacker(writer)
@@ -432,7 +443,7 @@ def write(traces, writer):
         packer.pack_str(tp.name, tp.signature, tp.format)
 
     for trace in traces:
-        packer.pack('QQ16sQI', trace.tp.key, trace.thread, trace.thread_name.encode(),
+        packer.pack('QQ16sQI', trace.tp.key, trace.thread.ptr, trace.thread.name.encode(),
                     trace.time, trace.cpu)
 
         if trace.backtrace:

scripts/trace.py
@@ -488,7 +488,7 @@ def list_timed(args):
         for timed in timed_traces:
             t = timed.trace
             print '0x%016x %2d %20s %7s %-20s %s%s' % (
-                            t.thread,
+                            t.thread.ptr,
                             t.cpu,
                             trace.format_time(t.time),
                             trace.format_duration(timed.duration),

scripts/loader.py
@@ -21,7 +21,7 @@ modules = os.path.join(osv_dir, 'modules')
 
 sys.path.append(os.path.join(osv_dir, 'scripts'))
 
-from osv.trace import Trace,TracePoint,BacktraceFormatter,format_time,format_duration
+from osv.trace import Trace,Thread,TracePoint,BacktraceFormatter,format_time,format_duration
 from osv import trace, debug
 
 virtio_driver_type = gdb.lookup_type('virtio::virtio_driver')
@@ -1092,7 +1092,7 @@ def all_traces():
 
             data = unpacker.unpack(tp.signature)
             unpacker.align_up(8)
-            yield Trace(tp, thread, thread_name, time, cpu, data, backtrace=backtrace)
+            yield Trace(tp, Thread(thread, thread_name), time, cpu, data, backtrace=backtrace)
 
     iters = map(lambda cpu: one_cpu_trace(cpu), values(state.cpu_list))
     return heapq.merge(*iters)
@@ -1113,7 +1113,7 @@ def dump_trace(out_func):
     tp_fn_exit = lookup_tp('gdb_trace_function_exit')
 
     for trace in all_traces():
-        thread = trace.thread
+        thread = trace.thread.id
         time = trace.time
         cpu = trace.cpu
         tp = trace.tp

scripts/osv/prof.py
@@ -149,7 +149,7 @@ class PairTimedTraceMatcher(TimedTraceMatcher):
             return False
 
     def get_correlation_id(self, sample):
-        return sample.thread
+        return sample.thread.ptr
 
 class TimedConventionMatcher(TimedTraceMatcher):
     def __init__(self):
@@ -172,8 +172,8 @@ class TimedConventionMatcher(TimedTraceMatcher):
     def get_correlation_id(self, sample):
         ended = self.get_name_of_ended_func(sample.name)
         if ended:
-            return (sample.thread, ended)
-        return (sample.thread, sample.name)
+            return (sample.thread.ptr, ended)
+        return (sample.thread.ptr, sample.name)
 
 class timed_trace_producer(object):
     pair_matchers = [
@@ -274,11 +274,16 @@ def find_frame_index(frames, name):
     return None
 
 class GroupByThread:
-    def get_group(self, sample):
-        return sample.thread
+    def __init__(self):
+        self.name_by_ptr = {}
 
-    def format(self, group):
-        return 'Thread 0x%x' % group
+    def get_group(self, sample):
+        t = sample.thread
+        self.name_by_ptr[t.ptr] = t.name
+        return t.ptr
+
+    def format(self, ptr):
+        return 'Thread %s (0x%x)' % (self.name_by_ptr[ptr], ptr)
 
 class GroupByCpu:
     def get_group(self, sample):

commit 02602aa644a69db11c312981c336264bedd17ac3

trace: fix 'trace.py download'
Broken by 3730c3953ca0cbae940f41134f73764ec1abb9d1.

Signed-off-by: Tomasz Grabiec <tgrabiec@cloudius-systems.com>
Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

scripts/osv/trace.py
@@ -388,7 +388,7 @@ class TraceDumpReader :
             data = unpacker.unpack(tp.signature)
             unpacker.align_up(8)
             last_tp = tp;
-            last_trace = Trace(tp, thread, thread_name, time, cpu, data, backtrace=backtrace)
+            last_trace = Trace(tp, Thread(thread, thread_name), time, cpu, data, backtrace=backtrace)
             yield last_trace;
 
     def traces(self):