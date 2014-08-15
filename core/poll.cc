94fe0631ded54bf58941f61192da6cf8962e24b2
Asias He <asias@cloudius-systems.com>
2014-08-15 22:07:51 +0800

    poll: Fix two indention
    Spotted during code inspection.
    
    Signed-off-by: Asias He <asias@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- core/poll.cc
+++ core/poll.cc
@@ -330,20 +330,20 @@ int file::poll_many(struct pollfd _pfd[], nfds_t _nfds, timeout_t timeout)
     pfd.reserve(_nfds);
 
     for (nfds_t i = 0; i < _nfds; ++i) {
-	if (_pfd[i].fd >= 0) {
-           pfd.emplace_back(fileref_from_fd(_pfd[i].fd), _pfd[i].events, _pfd[i].revents);
-	}
+        if (_pfd[i].fd >= 0) {
+            pfd.emplace_back(fileref_from_fd(_pfd[i].fd), _pfd[i].events, _pfd[i].revents);
+        }
     }
 
     auto ret = do_poll(pfd, timeout);
 
     for (nfds_t i = 0, j = 0; i < _nfds; ++i) {
-	if (_pfd[i].fd >= 0) {
-           _pfd[i].revents = pfd[j].revents;
-	    j++;
-	} else {
-	    _pfd[i].revents = 0;
-	}
+        if (_pfd[i].fd >= 0) {
+            _pfd[i].revents = pfd[j].revents;
+            j++;
+        } else {
+            _pfd[i].revents = 0;
+        }
     }
 
     return ret;
