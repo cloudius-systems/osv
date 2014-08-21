000ce9a2cb638d00ecafe77780e21ad57331f239
Nadav Har'El <nyh@cloudius-systems.com>
2014-08-21 09:32:54 +0300

    libc/stdio: Flush stdout on read from stdin
    Fix read from stdin to flush stdout. This happens in Linux (glibc) and
    in traditional UNIX, although it was never guaranteed by any standard.
    
    This behavior was added in UNIX because it was useful for programs which
    print a prompt and read input - forgetting to fflush(stdout) in the meantime.
    It was later done to Linux to run programs which assumed it, and for the
    same reason we also want it in OSv.
    
    Fixes #464.
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- libc/stdio/stdin.c
+++ libc/stdio/stdin.c
@@ -1,12 +1,24 @@
 #include "stdio_impl.h"
 
+static size_t __stdin_read(FILE *fp, unsigned char *buf, size_t len)
+{
+    // Flush stdout when reading from stdin - this is a long UNIX tradition
+    // and also happens in glibc. The latter flushes stdout if it is line-
+    // buffered whenever any line-buffered file is read - but the intended
+    // effect was on stdin.
+    if (stdout->lbf == '\n') {
+        fflush(stdout);
+    }
+    return __stdio_read(fp, buf, len);
+}
+
 static unsigned char buf[BUFSIZ+UNGET];
 static FILE f = {
 	.buf = buf+UNGET,
 	.buf_size = sizeof buf-UNGET,
 	.fd = 0,
 	.flags = F_PERM | F_NOWR,
-	.read = __stdio_read,
+	.read = __stdin_read,
 	.seek = __stdio_seek,
 	.close = __stdio_close,
 };
