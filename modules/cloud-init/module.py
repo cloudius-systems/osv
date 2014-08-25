1a11bbe2d63abebefa05299fdcaa51e7e9966a54
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2014-08-25 12:21:08 +0200

    cloud-init: depend on libyaml
    Signed-off-by: Tomasz Grabiec <tgrabiec@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- modules/cloud-init/module.py
+++ modules/cloud-init/module.py
@@ -6,12 +6,12 @@ from osv.modules import api
 _module = '${OSV_BASE}/modules/cloud-init'
 
 usr_files = FileMap()
-usr_files.add(os.path.join('${OSV_BASE}', 'external/x64/misc.bin/usr/lib64/libyaml-cpp.so.0.5.1')).to('/usr/lib/libyaml-cpp.so.0.5')
 usr_files.add(os.path.join(_module, 'cloud-init.so')).to('/usr/mgmt/cloud-init.so')
 usr_files.add(os.path.join(_module, 'cloud-init.yaml')).to('/usr/mgmt/cloud-init.yaml')
 usr_files.add(os.path.join(_module, 'cmdline')).to('/init/00-cmdline')
 
 api.require('httpserver')
+api.require('libyaml')
 
 #default = api.run('/usr/mgmt/cloud-init.so --skip-error --file /usr/mgmt/cloud-init.yaml')
 default = ""
