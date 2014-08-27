d2c0a71ba525482dd11bcfaaec328be072584bcd
Tomasz Grabiec <tgrabiec@cloudius-systems.com>
2014-08-27 18:08:19 +0200

    java: depend on ca-certificates
    Fixes #400.
    
    Signed-off-by: Tomasz Grabiec <tgrabiec@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- modules/java/module.py
+++ modules/java/module.py
@@ -5,6 +5,7 @@ import os, os.path
 usr_files = FileMap()
 
 api.require('fonts')
+api.require('ca-certificates')
 api.require('libz')
 
 jdkdir = os.path.basename(os.path.expandvars('${jdkbase}'))
@@ -14,5 +15,7 @@ usr_files.add('${jdkbase}').to('/usr/lib/jvm/java') \
     .include('jre/**') \
     .exclude('jre/lib/security/cacerts') \
     .exclude('jre/lib/audio/**')
+
 usr_files.link('/usr/lib/jvm/' + jdkdir).to('java')
 usr_files.link('/usr/lib/jvm/jre').to('java/jre')
+usr_files.link('/usr/lib/jvm/java/jre/lib/security/cacerts').to('/etc/pki/java/cacerts')
