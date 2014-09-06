64fc63cc55ed5583d41b58656b242625af29ca38
David Halls <dahalls@gmail.com>
2014-09-06 08:10:08 +0100

    Add a couple more syscalls (to get express.js hello-world example running)

--- linux.cc
+++ linux.cc
@@ -18,6 +18,7 @@
 #include <time.h>
 #include <sys/epoll.h>
 #include <sys/eventfd.h>
+#include <sys/socket.h>
 
 #include <unordered_map>
 
@@ -122,6 +123,22 @@ int futex(int *uaddr, int op, int val, const struct timespec *timeout,
         return fn(arg1, arg2, arg3);            \
         } while (0)
 
+#define SYSCALL4(fn, __t1, __t2, __t3, __t4)    \
+        case (__NR_##fn): do {                  \
+        va_list args;                           \
+        __t1 arg1;                              \
+        __t2 arg2;                              \
+        __t3 arg3;                              \
+        __t4 arg4;                              \
+        va_start(args, number);                 \
+        arg1 = va_arg(args, __t1);              \
+        arg2 = va_arg(args, __t2);              \
+        arg3 = va_arg(args, __t3);              \
+        arg4 = va_arg(args, __t4);              \
+        va_end(args);                           \
+        return fn(arg1, arg2, arg3, arg4);      \
+        } while (0)
+
 #define SYSCALL6(fn, __t1, __t2, __t3, __t4, __t5, __t6)        \
         case (__NR_##fn): do {                                  \
         va_list args;                                           \
@@ -155,6 +172,9 @@ long syscall(long number, ...)
     SYSCALL2(pipe2, int *, int);
     SYSCALL1(epoll_create1, int);
     SYSCALL2x(eventfd2, eventfd, unsigned int, int);
+    SYSCALL4(epoll_ctl, int, int, int, struct epoll_event *);
+    SYSCALL4(epoll_wait, int, struct epoll_event *, int, int);
+    SYSCALL4(accept4, int, struct sockaddr *, socklen_t *, int);
     }
 
     abort("syscall(): unimplemented system call %d. Aborting.\n", number);
