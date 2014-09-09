93f66e650d7047e39025b67400e91434f8a9c3cc
Glauber Costa <glommer@cloudius-systems.com>
2014-09-09 08:25:09 -0400

    fpu: enhance test for FPU
    The present patch introduces a test case that reproduces the bug described in
    issue #9. It only happens in the "yield" case, but for completeness I am
    including a test that yield, and a test that does not yield.
    
    Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>
    Reviewed-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- tests/tst-fpu.cc
+++ tests/tst-fpu.cc
@@ -13,6 +13,7 @@
 #include <osv/debug.hh>
 #include <boost/format.hpp>
 #include <cmath>
+#include <fenv.h>
 
 bool test()
 {
@@ -36,6 +37,34 @@ bool test()
     return !bad;
 }
 
+int callee_saved_loop(int mode, bool yield)
+{
+    constexpr unsigned repeats = 1000000;
+    fesetround(mode);
+    for (unsigned i = 0; i < repeats; ++i) {
+        int rmode = fegetround();
+        if (rmode != mode) {
+            printf("thread expected %d, got %d (yield = %d)\n", mode, rmode, yield);
+            return 1;
+        }
+        fesetround(mode);
+        if (yield) {
+            sched::thread::yield();
+        }
+    }
+    return 0;
+}
+
+int callee_saved_zero(bool yield)
+{
+    return callee_saved_loop(FE_TOWARDZERO, yield);
+}
+
+int callee_saved_nearest(bool yield)
+{
+    return callee_saved_loop(FE_TONEAREST, yield);
+}
+
 typedef boost::format fmt;
 
 int main(int ac, char **av)
@@ -57,8 +86,32 @@ int main(int ac, char **av)
     }
     for (auto t : threads) {
         t->join();
-        delete t;
     }
+    threads.clear();
+
+    auto do_callee_saved = [&](bool yield) {
+        tests++;
+        for (unsigned i = 0; i < nr_threads; ++i) {
+            tests++;
+            sched::thread *t;
+            if (i % 2) {
+                t = new sched::thread([&] { fails += callee_saved_zero(yield); }, sched::thread::attr().pin(sched::cpus[0]));
+            } else {
+                t = new sched::thread([&] { fails += callee_saved_nearest(yield); }, sched::thread::attr().pin(sched::cpus[0]));
+            }
+            threads.push_back(t);
+            t->start();
+        }
+
+        for (auto t : threads) {
+            t->join();
+        }
+        threads.clear();
+    };
+
+    do_callee_saved(false);
+    do_callee_saved(true);
+
     printf("fpu test done, %d/%d fails/tests\n", fails.load(std::memory_order_relaxed), tests.load(std::memory_order_relaxed));
     return !fails ? 0 : 1;
 }
