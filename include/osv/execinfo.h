c876b0f35878217d51c748e675accd8d307b9359
Nadav Har'El <nyh@cloudius-systems.com>
2014-09-14 13:53:36 +0300

    execinfo: implement backtrace_symbols*
    From Linux's <execinfo.h>, we already implemented backtrace() but
    not backtrace_symbols() or backtrace_symbols_fd(). This patch
    implements both. These functions are used by several applications,
    including MySQL, so we should have them.
    
    Fixes #493.
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- include/osv/execinfo.h
+++ include/osv/execinfo.h
@@ -14,6 +14,9 @@ extern "C" {
 
 int backtrace (void **buffer, int size);
 
+char **backtrace_symbols(void *const *buffer, int size);
+void backtrace_symbols_fd(void *const *buffer, int size, int fd);
+
 #ifdef __cplusplus
 }
 #endif
--- libc/misc/backtrace.cc
+++ libc/misc/backtrace.cc
@@ -105,3 +105,56 @@ int backtrace(void **buffer, int size)
     return 28;
 }
 #endif
+
+#include <osv/demangle.hh>
+#include <string.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void backtrace_symbols_fd(void *const *addrs, int len, int fd)
+{
+    for (int i = 0; i < len; i++) {
+        char name[1024];
+        // FIXME: I think Linux also shows the name of the object
+        osv::lookup_name_demangled(addrs[i], name, sizeof(name));
+        auto remain = strlen(name);
+        snprintf(name + remain, sizeof(name) - remain, " [%p]\n", addrs[i]);
+        remain = strlen(name);
+        while (remain > 0) {
+            auto n = write(fd, name, remain);
+            if (n < 0) {
+                return; // write error, nothing better we can do...
+            }
+            remain -= n;
+        }
+    }
+}
+char **backtrace_symbols(void *const *addrs, int len)
+{
+    // We need to return a newly allocated char **, i.e., an array of len
+    // pointers. We put the strings we point to after this array, allocated
+    // together, so when the user free()s the array, the strings will also
+    // be freed.
+    size_t used = len * sizeof(char*);
+    size_t bufsize = used + 1;
+    char *buf = (char*)malloc(bufsize);
+    char **ret = (char **)buf;
+    for (int i = 0; i < len; i++) {
+        char name[1024];
+        // FIXME: I think Linux also shows the name of the object
+        osv::lookup_name_demangled(addrs[i], name, sizeof(name));
+        auto remain = strlen(name);
+        snprintf(name + remain, sizeof(name) - remain, " [%p]", addrs[i]);
+        remain = strlen(name);
+        if (remain >= (bufsize - used)) {
+            bufsize = bufsize*2 + remain + 1;
+            buf = (char*)realloc(buf, bufsize);
+            ret = (char **)buf;
+        }
+        ret[i] = buf + used;
+        strcpy(buf + used, name);
+        used += remain + 1;
+    }
+    return ret;
+}
