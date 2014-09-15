e4fd5989f1c9b46c258b7b6942856d5991840f7e
Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
2014-09-15 17:22:04 -0300

    vfs: fix comment describing locking in get_arcbuf
    vn_lock is no longer taken when calling VOP_CACHE,
    thus the locking description should be fixed.
    
    Signed-off-by: Raphael S. Carvalho <raphaelsc@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- fs/vfs/vfs_fops.cc
+++ fs/vfs/vfs_fops.cc
@@ -148,7 +148,7 @@ void vfs_file::sync(off_t start, off_t end)
     pagecache::sync(this, start, end);
 }
 
-// Locking: vn_lock will call into the filesystem, and that can trigger an
+// Locking: VOP_CACHE will call into the filesystem, and that can trigger an
 // eviction that will hold the mmu-side lock that protects the mappings
 // Always follow that order. We however can't just get rid of the mmu-side lock,
 // because not all invalidations will be synchronous.
