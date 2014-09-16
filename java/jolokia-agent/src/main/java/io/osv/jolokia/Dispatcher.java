2b7169ecf5f99d96106e5e25adcc366700ac1c4b
Calle Wilund <calle@cloudius-systems.com>
2014-09-16 16:55:06 +0200

    Jolokia: Ensure all requests are run with context classloader set
    Internal lookups can fail/exception otherwise, esp. write case
    
    Signed-off-by: Calle Wilund <calle@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- java/jolokia-agent/src/main/java/io/osv/jolokia/Dispatcher.java
+++ java/jolokia-agent/src/main/java/io/osv/jolokia/Dispatcher.java
@@ -1,6 +1,7 @@
 package io.osv.jolokia;
 
 import java.io.ByteArrayInputStream;
+import java.security.PrivilegedAction;
 
 import javax.management.RuntimeMBeanException;
 
@@ -15,7 +16,7 @@ public class Dispatcher {
 	private final HttpRequestHandler handler;
 
 	public Dispatcher() {
-		LogHandler h = new LogHandler() {
+		final LogHandler h = new LogHandler() {
 			public void debug(String message) {
 			}
 
@@ -27,61 +28,76 @@ public class Dispatcher {
 				t.printStackTrace(System.err);
 			}
 		};
+		handler = runWithContextClassLoader(new PrivilegedAction<HttpRequestHandler>() {
+			public HttpRequestHandler run() {
+				Configuration cfg = new Configuration(ConfigKey.AGENT_ID,
+						"OSv Jolokia Bridge");
+				BackendManager mgr = new BackendManager(cfg, h);
+				return new HttpRequestHandler(cfg, mgr, h);
+			}
+		});
+	}
+
+	public int dispatch(final RequestAndResponse req) {
+		return runWithContextClassLoader(new PrivilegedAction<Integer>() {
+			public Integer run() {
+				int returnCode = 200;
+				JSONAware json = null;
+				try {
+					switch (req.getMode()) {
+					case RequestAndResponse.GET_REQUEST:
+						json = handler.handleGetRequest(req.getUri(),
+								req.getPath(), req.getParameters());
+						break;
+					case RequestAndResponse.POST_REQUEST:
+						json = handler.handlePostRequest(req.getUri(),
+								new ByteArrayInputStream(req.getBody()),
+								req.getEncoding(), req.getParameters());
+
+						break;
+					}
+				} catch (Throwable exp) {
+					json = handler
+							.handleThrowable(exp instanceof RuntimeMBeanException ? ((RuntimeMBeanException) exp)
+									.getTargetException() : exp);
+				} finally {
+					String callback = req.getParameter(ConfigKey.CALLBACK
+							.getKeyValue());
+					String answer = json != null ? json.toJSONString()
+							: handler
+									.handleThrowable(
+											new Exception(
+													"Internal error while handling an exception"))
+									.toJSONString();
+					String mimeType = null;
+					if (callback != null) {
+						answer = callback + "(" + answer + ");";
+					} else {
+						mimeType = req.getParameter(ConfigKey.MIME_TYPE
+								.getKeyValue());
+					}
+					req.setMimeType(mimeType);
+					req.setResponse(answer);
+				}
+				// Note. Vanilla Jolokia connectors (Servlet etc) always give
+				// back
+				// 200, even if a call fails. I.e. caller must check the
+				// response body
+				// for the actual operation results.
+				// Ensure we keep this behaviour for client compatibility.
+				return returnCode;
+			}
+		});
+	}
+
+	private <T> T runWithContextClassLoader(PrivilegedAction<T> r) {
 		Thread t = Thread.currentThread();
 		ClassLoader l = t.getContextClassLoader();
+		t.setContextClassLoader(getClass().getClassLoader());
 		try {
-			t.setContextClassLoader(getClass().getClassLoader());
-			Configuration cfg = new Configuration(ConfigKey.AGENT_ID,
-					"OSv Jolokia Bridge");
-			BackendManager mgr = new BackendManager(cfg, h);
-			handler = new HttpRequestHandler(cfg, mgr, h);
+			return r.run();
 		} finally {
 			t.setContextClassLoader(l);
 		}
-	}
-
-	public int dispatch(RequestAndResponse req) {
-		int returnCode = 200;
-		JSONAware json = null;
-		try {
-			switch (req.getMode()) {
-			case RequestAndResponse.GET_REQUEST:
-				json = handler.handleGetRequest(req.getUri(), req.getPath(),
-						req.getParameters());
-				break;
-			case RequestAndResponse.POST_REQUEST:
-				json = handler.handlePostRequest(req.getUri(),
-						new ByteArrayInputStream(req.getBody()),
-						req.getEncoding(), req.getParameters());
-
-				break;
-			}
-		} catch (Throwable exp) {
-			json = handler
-					.handleThrowable(exp instanceof RuntimeMBeanException ? ((RuntimeMBeanException) exp)
-							.getTargetException() : exp);
-		} finally {
-			String callback = req
-					.getParameter(ConfigKey.CALLBACK.getKeyValue());
-			String answer = json != null ? json.toJSONString()
-					: handler
-							.handleThrowable(
-									new Exception(
-											"Internal error while handling an exception"))
-							.toJSONString();
-			String mimeType = null;
-			if (callback != null) {
-				answer = callback + "(" + answer + ");";
-			} else {
-				mimeType = req.getParameter(ConfigKey.MIME_TYPE.getKeyValue());
-			}
-			req.setMimeType(mimeType);
-			req.setResponse(answer);
-		}
-		// Note. Vanilla Jolokia connectors (Servlet etc) always give back
-		// 200, even if a call fails. I.e. caller must check the response body
-		// for the actual operation results.
-		// Ensure we keep this behaviour for client compatibility.
-		return returnCode;
 	}
 }
