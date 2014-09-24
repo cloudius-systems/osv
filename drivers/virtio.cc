b202c342603e88f7a1989aaa6bcc6818d0eadb7f
Claudio Fontana <claudio.fontana@huawei.com>
2014-09-24 10:09:49 +0200

    virtio: move functionality out of assert expression
    having functionality inside the assert expression is dangerous
    because someone could be tempted to use the NDEBUG macro, and
    the assert expression would disappear from the generated code.
    
    Signed-off-by: Claudio Fontana <claudio.fontana@huawei.com>
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- drivers/virtio.cc
+++ drivers/virtio.cc
@@ -31,7 +31,8 @@ virtio_driver::virtio_driver(pci::device& dev)
     for (unsigned i = 0; i < max_virtqueues_nr; i++) {
         _queues[i] = nullptr;
     }
-    assert(parse_pci_config() == true);
+    bool status = parse_pci_config();
+    assert(status == true);
 
     _dev.set_bus_master(true);
 
