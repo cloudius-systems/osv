9db51c3a509269690e3a5b422b640e4a849461b8
Amnon Heiman <amnon@cloudius-systems.com>
2014-10-21 09:52:46 +0300

    Http Server: dynamically generate the api-doc swagger
    This adds a handler that collect api entry and return the api-doc when
    needed.
    
    It allows dynamically adds api entry, for example if the jvm is not
    active, it will be possible not to register it and the API (with the
    swagger-ui) will not be returned.
    
    Signed-off-by: Amnon Heiman <amnon@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

---
+++ modules/httpserver/json/api_docs.cc
@@ -0,0 +1,55 @@
+/*
+ * Copyright (C) 2014 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+#include "api_docs.hh"
+#include "handlers.hh"
+#include "transformers.hh"
+#include "formatter.hh"
+
+using namespace std;
+
+namespace httpserver {
+
+namespace json {
+
+class api_registry : public handler_base {
+    static const string base_path;
+    api_docs docs;
+
+    routes& _routes;
+public:
+    api_registry(routes& _routes) : _routes(_routes)
+    {
+        _routes.put(GET, base_path, this);
+    }
+    virtual void handle(const std::string& path, parameters* params,
+                        const http::server::request& req, http::server::reply& rep) {
+        rep.content = formatter::to_json(docs);
+        set_headers(rep,"json");
+    }
+
+    void reg(const std::string& api, const std::string& description) {
+        api_doc doc;
+        doc.description = description;
+        doc.path = api;
+        docs.apis.push(doc);
+        file_handler* index = new file_handler("/usr/mgmt/api/listings/" + api + ".json", new content_replace("json"));
+        _routes.put(GET, base_path + "/" + api,index);
+    }
+};
+const string api_registry::base_path = "/api-doc";
+static api_registry* registry = nullptr;
+
+void register_api(const std::string& api, const std::string& description) {
+    registry->reg(api, description);
+}
+
+void api_doc_init(routes& _routes) {
+    registry = new api_registry(_routes);
+}
+
+}
+}
---
+++ modules/httpserver/json/api_docs.hh
@@ -0,0 +1,102 @@
+/*
+ * Copyright (C) 2014 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+#ifndef API_DOCS_HH_
+#define API_DOCS_HH_
+#include "json/json_elements.hh"
+#include "routes.hh"
+#include <string>
+
+namespace httpserver {
+
+namespace json {
+
+struct api_doc : public json::json_base {
+    json::json_element<std::string> path;
+    json::json_element<std::string> description;
+
+    void register_params()
+    {
+        add(&path, "path");
+        add(&description, "description");
+
+    }
+    api_doc()
+    {
+        register_params();
+    }
+    api_doc(const api_doc & e)
+    {
+        register_params();
+        path = e.path;
+        description = e.description;
+    }
+    template<class T>
+    api_doc& operator=(const T& e)
+    {
+        path = e.path;
+        description = e.description;
+        return *this;
+    }
+    api_doc& operator=(const api_doc& e)
+    {
+        path = e.path;
+        description = e.description;
+        return *this;
+    }
+};
+
+struct api_docs : public json::json_base {
+    json::json_element<std::string> apiVersion;
+    json::json_element<std::string> swaggerVersion;
+    json::json_list<api_doc> apis;
+
+    void register_params()
+    {
+        add(&apiVersion, "apiVersion");
+        add(&swaggerVersion, "swaggerVersion");
+        add(&apis, "apis");
+
+    }
+    api_docs()
+    {
+        apiVersion = "0.0.1";
+        swaggerVersion = "1.2";
+        register_params();
+    }
+    api_docs(const api_docs & e)
+    {
+        apiVersion = "0.0.1";
+        swaggerVersion = "1.2";
+        register_params();
+    }
+    template<class T>
+    api_docs& operator=(const T& e)
+    {
+        apis = e.apis;
+        return *this;
+    }
+    api_docs& operator=(const api_docs& e)
+    {
+        apis = e.apis;
+        return *this;
+    }
+};
+
+void register_api(const std::string& api,
+                  const std::string& description);
+
+/**
+ * Initialize the routes object with specific routes mapping
+ * @param routes - the routes object to fill
+ */
+void api_doc_init(routes& routes);
+
+}
+}
+
+#endif /* API_DOCS_HH_ */
