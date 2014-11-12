a0d83efcfdbb0dd39aedbebf5ccff6efa37fb409
Pekka Enberg <penberg@cloudius-systems.com>
2014-11-12 08:59:01 +0200

    x64: Remove VMware hypervisor probing
    This removes the VMware hypervisor probing introduced in commit 7cb1ddf
    ("Hypervisor API: VMware support") because people are reporting that it
    causes boot issues on some Amazon EC2 instance types.
    
    Reported-by: Asias He <asias@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- arch/x64/hypervisor.cc
+++ arch/x64/hypervisor.cc
@@ -11,35 +11,6 @@
 
 namespace osv {
 
-static constexpr uint32_t vmware_magic  = 0x564D5868;
-static constexpr uint16_t vmware_port   = 0x5658;
-enum vmware_cmd {
-    vmware_cmd_getversion       = 10,
-    vmware_cmd_getdevice        = 11,
-    vmware_cmd_gethwmodelver    = 17
-};
-enum vmware_type {
-    vmware_type_error = 0,
-    vmware_type_esxi = 2,
-    vmware_type_workstation = 4
-};
-
-static uint32_t check_vmware_version()
-{
-    uint32_t eax = 0, ebx = 0, ecx = 0, edx = 0;
-
-    asm volatile("inl (%%dx)" :
-        "=a"(eax), "=c"(ecx), "=d"(edx), "=b"(ebx) :
-        "0"(vmware_magic),
-        "1"(vmware_cmd_getversion),
-        "2"(vmware_port),
-        "3"(ebx) :
-        "memory");
-    if (ebx != vmware_magic)
-        return vmware_type_error;
-    return ecx;
-}
-
 hypervisor_type hypervisor()
 {
     if (processor::features().kvm_clocksource || processor::features().kvm_clocksource2) {
@@ -47,12 +18,6 @@ hypervisor_type hypervisor()
     }
     if (processor::features().xen_clocksource) {
         return hypervisor_type::xen;
-    }
-    switch (check_vmware_version()) {
-    case vmware_type_esxi:
-        return hypervisor_type::vmware_esxi;
-    case vmware_type_workstation:
-        return hypervisor_type::vmware_workstation;
     }
     return hypervisor_type::unknown;
 }
@@ -64,10 +29,6 @@ std::string hypervisor_name()
         return "kvm";
     case osv::hypervisor_type::xen:
         return "xen";
-    case osv::hypervisor_type::vmware_workstation:
-        return "vmware_workstation";
-    case osv::hypervisor_type::vmware_esxi:
-        return "vmware_esxi";
     default:
         return "Unknown";
     }
