c2fb41dc06ab9d1d75fe439cd300657da0e4e056
Paweł Dziepak <pdziepak@quarnos.org>
2014-11-13 15:00:23 +0100

    x64: fix format string in dump_registers()
    rip and rflags are 64 bit so %lx should be used instead of %x.
    
    Reviewed-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Paweł Dziepak <pdziepak@quarnos.org>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- arch/x64/dump.cc
+++ arch/x64/dump.cc
@@ -17,8 +17,8 @@ void dump_registers(exception_frame* ef)
     osv::lookup_name_demangled(
         reinterpret_cast<void *>(ef->rip), name, sizeof(name));
     debug_ll("[registers]\n");
-    debug_ll("RIP: 0x%016x <%s>\n", ef->rip, name);
-    debug_ll("RFL: 0x%016x  CS:  0x%016x  SS:  0x%016x\n", ef->rflags, ef->cs, ef->ss);
+    debug_ll("RIP: 0x%016lx <%s>\n", ef->rip, name);
+    debug_ll("RFL: 0x%016lx  CS:  0x%016x  SS:  0x%016x\n", ef->rflags, ef->cs, ef->ss);
     debug_ll("RAX: 0x%016lx  RBX: 0x%016lx  RCX: 0x%016lx  RDX: 0x%016lx\n", ef->rax, ef->rbx, ef->rcx, ef->rdx);
     debug_ll("RSI: 0x%016lx  RDI: 0x%016lx  RBP: 0x%016lx  R8:  0x%016lx\n", ef->rsi, ef->rdi, ef->rbp, ef->r8);
     debug_ll("R9:  0x%016lx  R10: 0x%016lx  R11: 0x%016lx  R12: 0x%016lx\n", ef->r9, ef->r10, ef->r11, ef->r12);
