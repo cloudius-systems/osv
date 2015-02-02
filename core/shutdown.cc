703bf08d72ef617b212632e038a93be0d00e879f
Nadav Har'El <nyh@cloudius-systems.com>
2015-02-02 11:55:56 +0200

    Avoid crash during exit()
    As reported in issue #580 (and also earlier in issue #374), sometimes OSv
    crashes during a call to exit(), with the message "exception nested too
    deeply" usually printed. The "shutdown" REST API can probably cause
    a similar issue, though this was not reported in practice.
    
    The problem is that exit(), calling osv::shutdown(), forcibly unmounts the
    filesystem, but other threads might still be running code from executables
    mapped from files. If these threads happen to run after osv::shutdown()
    unmounts the filesystem but before it powers off the VM, they can cause
    OSv to crash, a crash which is usually accompanied by a "exception nested
    too deeply" message, as the page-fault handler itself crashes.
    
    A complete solution of this problem is difficult - in Unix it is usually
    solved by killing all user processes before unmounting the filesystem,
    but in OSv we have no reliable method of killing threads. The closest
    approximation is what I do in this patch - use thread::unsafe_stop()
    to stop all application threads (presumably, these would be the threads
    running code from the filesystem).
    
    This technique will usually work - in particular it eliminates the crashes
    reported in issue #580. But is not an entirely reliable solution - if an
    application thread happens to hold a critical OSv mutex while it is stopped,
    this can deadlock the entire system. This should be a rare problem (a
    reasonable application is unlikely to call exit() while its other threads are
    busy doing important work), but we should nevertheless re-consider this
    workaround in the future.
    
    One idea we can consider is not to preemptively stop the application
    threads before unmounting (as we do in this patch). Instead, we can leave
    these threads running, and in the page-fault handler code recognize
    this case (the backing file has been unmounted), and stop this thread
    only at at that point. It still remains to be seen whether that technique
    can completely avoid deadlocks (theoretically, we can be holding a mutex
    while a page fault happens...) or other types of horrible crashes.
    
    Fixes #580.
    Fixed #374.
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

--- core/shutdown.cc
+++ core/shutdown.cc
@@ -1,6 +1,7 @@
 #include <osv/shutdown.hh>
 #include <osv/power.hh>
 #include <osv/debug.hh>
+#include <osv/sched.hh>
 
 extern void vfs_exit(void);
 
@@ -8,6 +9,27 @@ namespace osv {
 
 void shutdown()
 {
+    // The vfs_exit() call below will forcibly unmount the filesystem. If any
+    // thread is executing code mapped from a file, these threads may crash if
+    // they happen to run between the call to vfs_exit() and the call to
+    // osv::poweroff() - see issue #580.
+    // As an attempt to avoid this problem, we stop here all application
+    // threads. Unfortunately, this is NOT a completely reliable solution,
+    // as stopping a thread while it holds an important OSv mutex can cause
+    // the entire system to deadlock. Hopefully, in *most* cases, reasonable
+    // applications will not call exit() while their other threads are doing
+    // important work. But eventually, we need a better solution here.
+    bool stopped_others = false;
+    auto current = sched::thread::current();
+    while (!stopped_others) {
+        stopped_others = true;
+        sched::with_all_threads([&](sched::thread &t) {
+            if (&t != current && t.is_app()) {
+                stopped_others &= t.unsafe_stop();
+            }
+        });
+    }
+
     vfs_exit();
     debug("Powering off.\n");
     osv::poweroff();
