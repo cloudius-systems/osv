b9868a94f6f62ce664e7084f02f3f16529520a00
Nadav Har'El <nyh@cloudius-systems.com>
2015-02-10 01:11:12 +0200

    Fix freelocale()
    Usually, newlocale() allocates memory, and freelocale() is supposed to free
    it. However, in our implementation, newlocale() can return a pointer to
    static data, c_locale. That cannot be freed - and freelocale() must ignore
    it.
    
    Without this patch, ImageMagick's "convert" crashes when it ends, as it
    calls freelocale().
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- libc/locale/freelocale.c
+++ libc/locale/freelocale.c
@@ -4,6 +4,13 @@
 
 void __freelocale(locale_t l)
 {
+    // In our implementation, newlocale() might return c_locale instead of
+    // allocating a new copy of the locale... We can't free that.
+    extern locale_t __c_locale_ptr;
+    if (l == __c_locale_ptr) {
+        return;
+    }
+
 	free(l);
 }
 
--- runtime.cc
+++ runtime.cc
@@ -268,6 +268,8 @@ static struct __locale_struct c_locale = {
     c_locale_array + 128, // __ctype_b
 };
 
+locale_t __c_locale_ptr = &c_locale;
+
 void* __stack_chk_guard = reinterpret_cast<void*>(0x12345678abcdefull);
 UNIMPL(void __stack_chk_fail(void))
 
