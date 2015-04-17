cloudius-systems/osv

commit c1dff82f3312712d0078d397a8b775c1de9d15ac

procfs: add initial support for /proc/cpuinfo
Fixes #615.

Signed-off-by: Ivan Krivonos <int0dster@gmail.com>
Reviewed-by: Nadav Har'El <nyh@cloudius-systems.com>
Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

fs/procfs/procfs_vnops.cc
@@ -361,6 +361,7 @@ procfs_mount(mount* mp, const char *dev, int flags, const void* data)
     root->add("self", self);
     root->add("0", self); // our standard pid
     root->add("mounts", inode_count++, procfs_mounts);
+    root->add("cpuinfo", inode_count++, [] { return processor::features_str(); });
 
     vp->v_data = static_cast<void*>(root);
 

commit ca00e8d665d4a14b2a3eb0cf95511a71e85cd7e6

procfs: Add missing cpuid.hh include
Fixes AARCH64 build breakage introduced in commit c1dff82 ("procfs: add
initial support for /proc/cpuinfo").

Signed-off-by: Pekka Enberg <penberg@cloudius-systems.com>

fs/procfs/procfs_vnops.cc
@@ -24,6 +24,8 @@
 #include <sys/resource.h>
 #include <mntent.h>
 
+#include "cpuid.hh"
+
 namespace procfs {
 
 using namespace std;