910649dcad127e407754296600ee9d00efbb1514
Nadav Har'El <nyh@cloudius-systems.com>
2015-06-11 14:52:18 +0300

    tests: another fix for misc-wake.cc
    OSv doesn't deal well with parts of a thread object being paged out.
    This can't normally happen for malloc()ed threads, but in misc-wake.cc
    we deliberately put a thread object in an mmap'ed area. Fix this problem
    by mmap'ing with the MAP_POPULATE parameter, ensuring that the memory
    is preallocated.
    
    The first half of the misc-wake.cc test now works again, but the second
    half hangs - I guess we need yet another patch :-)
    
    Signed-off-by: Nadav Har'El <nyh@cloudius-systems.com>
    Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

--- tests/misc-wake.cc
+++ tests/misc-wake.cc
@@ -28,13 +28,13 @@ int main(int argc, char **argv)
     //   wr->t->wake_with([] { wr->t = nullptr; });
     int npages = (sizeof(sched::thread)-1)/4096+1;
     void *pages[4];
-    pages[0] = mmap(NULL, npages*4096, PROT_NONE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
+    pages[0] = mmap(NULL, npages*4096, PROT_NONE, MAP_ANONYMOUS | MAP_SHARED | MAP_POPULATE, 0, 0);
     assert(pages[0] != MAP_FAILED);
-    pages[1] = mmap(NULL, npages*4096, PROT_NONE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
+    pages[1] = mmap(NULL, npages*4096, PROT_NONE, MAP_ANONYMOUS | MAP_SHARED | MAP_POPULATE, 0, 0);
     assert(pages[1] != MAP_FAILED);
-    pages[2] = mmap(NULL, npages*4096, PROT_NONE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
+    pages[2] = mmap(NULL, npages*4096, PROT_NONE, MAP_ANONYMOUS | MAP_SHARED | MAP_POPULATE, 0, 0);
     assert(pages[2] != MAP_FAILED);
-    pages[3] = mmap(NULL, npages*4096, PROT_NONE, MAP_ANONYMOUS | MAP_SHARED, 0, 0);
+    pages[3] = mmap(NULL, npages*4096, PROT_NONE, MAP_ANONYMOUS | MAP_SHARED | MAP_POPULATE, 0, 0);
     assert(pages[3] != MAP_FAILED);
     // double-buffering - two page regions out of the above four hold the
     // current two threads, and two are mprotect()ed to catch access to the
