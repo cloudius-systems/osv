b6db5c3bd73de2a5c6ab3850d82ac18b5d3fba8a
James Root <jamesroot@gmail.com>
2015-07-31 15:25:50 -0400

    Updated the license to contain CMU's full BSD license

--- fs/mfs/mfs_vfsops.cc
+++ fs/mfs/mfs_vfsops.cc
@@ -1,25 +1,16 @@
 /*
- * Copyright 2015 Carnegie Mellon University
- * This material is based upon work funded and supported by the Department of
- * Defense under Contract No. FA8721-05-C-0003 with Carnegie Mellon University
- * for the operation of the Software Engineering Institute, a federally funded
- * research and development center.
- * 
- * Any opinions, findings and conclusions or recommendations expressed in this
- * material are those of the author(s) and do not necessarily reflect the views
- * of the United States Department of Defense.
- * 
- * NO WARRANTY. THIS CARNEGIE MELLON UNIVERSITY AND SOFTWARE ENGINEERING
- * INSTITUTE MATERIAL IS FURNISHED ON AN "AS-IS" BASIS. CARNEGIE MELLON
- * UNIVERSITY MAKES NO WARRANTIES OF ANY KIND, EITHER EXPRESSED OR IMPLIED, AS
- * TO ANY MATTER INCLUDING, BUT NOT LIMITED TO, WARRANTY OF FITNESS FOR PURPOSE
- * OR MERCHANTABILITY, EXCLUSIVITY, OR RESULTS OBTAINED FROM USE OF THE
- * MATERIAL. CARNEGIE MELLON UNIVERSITY DOES NOT MAKE ANY WARRANTY OF ANY KIND
- * WITH RESPECT TO FREEDOM FROM PATENT, TRADEMARK, OR COPYRIGHT INFRINGEMENT.
- * 
- * This material has been approved for public release and unlimited
- * distribution.
- * 
+ * Copyright (c) 2015 Carnegie Mellon University.
+ * All Rights Reserved.
+ *
+ * THIS SOFTWARE IS PROVIDED "AS IS," WITH NO WARRANTIES WHATSOEVER. CARNEGIE
+ * MELLON UNIVERSITY EXPRESSLY DISCLAIMS TO THE FULLEST EXTENT PERMITTEDBY LAW
+ * ALL EXPRESS, IMPLIED, AND STATUTORY WARRANTIES, INCLUDING, WITHOUT
+ * LIMITATION, THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE, AND NON-INFRINGEMENT OF PROPRIETARY RIGHTS.
+ *
+ * Released under a modified BSD license. For full terms, please see the
+ * license file in this folder or contact permission@sei.cmu.edu.
+ *
  * DM-0002621
  *
  * Based on https://github.com/jdroot/mfs
