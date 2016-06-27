abd451d59822816115dd099270893e9d0cc30b59
Nadav Har'El <nyh@scylladb.com>
2016-06-27 13:57:46 +0300

    scripts/setup.py: fix dependency list for Fedora 23
    Fedora 23 recently obsoleted the "python-requests" package in favor of
    "python2-request". So now setup.py fails when it tries to install the
    obsolete package, when the new one is already installed.
    
    So we need the script to ask for the new name on Fedora 23.
    This assumes that people will use the script on an updated system. I think
    this is a fair assumption.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- scripts/setup.py
+++ scripts/setup.py
@@ -27,7 +27,7 @@ class Fedora(object):
                 'qemu-system-x86', 'libvirt', 'maven',
                 'ant', 'autoconf', 'automake', 'boost-static', 'genromfs', 'libtool',
                 'flex', 'bison', 'maven-shade-plugin', 'python-dpkt', 'tcpdump', 'gdb',
-                'gnutls-utils', 'openssl', 'python-requests', 'p11-kit', 'patch', 'wget',
+                'gnutls-utils', 'openssl', 'p11-kit', 'patch', 'wget',
                 'unzip', 'ncurses', 'ncurses-devel', 'libstdc++-static', 'openssl-libs',
                 'openssl-devel', 'libedit-devel', 'yaml-cpp-devel'
                 ]
@@ -36,35 +36,35 @@ class Fedora(object):
     ec2_post_install = standard_ec2_post_install
 
     class Fedora_19(object):
-        packages = ['java-1.7.0-openjdk']
+        packages = ['java-1.7.0-openjdk', 'python-requests']
         ec2_packages = []
         test_packages = []
         ec2_post_install = None
         version = '19'
 
     class Fedora_20(object):
-        packages = ['java-1.7.0-openjdk']
+        packages = ['java-1.7.0-openjdk', 'python-requests']
         ec2_packages = []
         test_packages = []
         ec2_post_install = None
         version = '20'
 
     class Fedora_21(object):
-        packages = ['java-1.7.0-openjdk']
+        packages = ['java-1.7.0-openjdk', 'python-requests']
         ec2_packages = []
         test_packages = []
         ec2_post_install = None
         version = '21'
 
     class Fedora_22(object):
-        packages = ['java-1.8.0-openjdk']
+        packages = ['java-1.8.0-openjdk', 'python-requests']
         ec2_packages = []
         test_packages = []
         ec2_post_install = None
         version = '22'
 
     class Fedora_23(object):
-        packages = ['java-1.8.0-openjdk']
+        packages = ['java-1.8.0-openjdk', 'python2-requests']
         ec2_packages = []
         test_packages = []
         ec2_post_install = None
