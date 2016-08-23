9a3066c91d449553fe7eceb79db267efdd5555f8
Nadav Har'El <nyh@scylladb.com>
2016-08-23 11:52:58 +0300

    tests: test for osv::run()
    Add a simple test for osv::run. The test tries to run itself, and verifies
    that it runs on the same thread (and a new thread is not needlessly
    created). It also checks that the return code is returned as expected,
    and that running a non-existant object fails with an exception as expected.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>
    Message-Id: <1471942378-19972-1-git-send-email-nyh@scylladb.com>

---
+++ tests/tst-run.cc
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2013 Cloudius Systems, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+#include <osv/run.hh>
+#include <atomic>
+#include <iostream>
+
+static std::atomic_int tests {0}, fails {0};
+
+static void report(bool ok, const char* msg)
+{
+    ++tests;
+    fails += !ok;
+    debug("%s: %s\n", (ok ? "PASS" : "FAIL"), msg);
+}
+
+extern "C" long gettid();
+
+
+int main(int ac, char** av)
+{
+    bool child = (ac == 2) && (av[1][0] == '\0');
+    std::cout << "main: thread id=" << gettid() << ", child=" << child << "\n";
+
+    if (child) {
+        return gettid();
+    }
+
+    // See that I can run myself (with a special argument to stop the recursion)
+    char *child_args[] = {"/tests/tst-run.so", ""};
+    int ret;
+    bool b = (bool)osv::run("/tests/tst-run.so", 2, child_args, &ret);
+    report(b == true, "Run myself");
+    // Check that the child ran in my thread - not a new thread
+    report(ret == gettid(), "Child ran in same thread as parent");
+
+
+    // Check that running a non-existant object fails
+    try {
+        osv::run("/nonexistant.so", 0, nullptr, nullptr);
+        // should throw and not get here
+        report(false, "Run nonexistant");
+    } catch(osv::launch_error) {
+        report(true, "Run nonexistant");
+    }
+
+    return 0;
+}
+
+
