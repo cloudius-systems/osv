27965e4bafb9799cf36ce0fe7cf3b3dc473268b8
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2016-12-22 00:20:12 -0500

    Changed java-tests module to require module java
    This patch addresses a build issue where this build command would fail due to both jdk 7 and 8 included:
    scripts/build -j4 image=java-tests,openjdk8-fedora && scripts/test.py --name java.*
    
    Following build commands should work now:
    scripts/build -j4 image=java-tests && scripts/test.py --name java.*
    scripts/build -j4 image=openjdk8-fedora,java-tests && scripts/test.py --name java.*
    
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>
    
    Message-Id: <1482384012-20585-1-git-send-email-jwkozaczuk@gmail.com>

--- modules/java-tests/module.py
+++ modules/java-tests/module.py
@@ -1,25 +1,12 @@
 from osv.modules import api
 from osv.modules.filemap import FileMap
-import os, os.path
 
-api.require('java-base')
-
-usr_files = FileMap()
-jdkdir = os.path.basename(os.path.expandvars('${jdkbase}'))
-
-usr_files.add('${jdkbase}').to('/usr/lib/jvm/java') \
-    .include('lib/**') \
-    .include('jre/**') \
-    .exclude('jre/lib/security/cacerts') \
-    .exclude('jre/lib/audio/**')
-
-usr_files.link('/usr/lib/jvm/' + jdkdir).to('java')
-usr_files.link('/usr/lib/jvm/jre').to('java/jre')
-usr_files.link('/usr/lib/jvm/java/jre/lib/security/cacerts').to('/etc/pki/java/cacerts')
+api.require('java')
 
 _jar = '/tests/java/tests.jar'
 _isolates_jar = '/tests/java/isolates.jar'
 
+usr_files = FileMap()
 usr_files.add('${OSV_BASE}/modules/java-tests/tests/target/runjava-tests.jar').to(_jar)
 usr_files.add('${OSV_BASE}/modules/java-tests/tests-isolates/target/tests-isolates-jar-with-dependencies.jar').to(_isolates_jar)
 
