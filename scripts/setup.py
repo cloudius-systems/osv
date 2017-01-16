1b0ca8b2db31974c8feba46892cffa92afcf3df5
Sergiy Kibrik' via OSv Development <osv-dev@googlegroups.com>
2017-01-16 11:04:10 +0000

    scripts/setup.py: add support for Ubuntu 16.04
    Signed-off-by: Sergiy Kibrik <sergiy.kibrik@globallogic.com>
    Message-Id: <1484564650-2843-2-git-send-email-sergiy.kibrik@globallogic.com>

--- scripts/setup.py
+++ scripts/setup.py
@@ -128,6 +128,13 @@ class Ubuntu(object):
     test_packages = ['libssl-dev', 'zip']
     ec2_post_install = None
 
+    class Ubuntu_16_04(object):
+        packages = ['openjdk-8-jdk']
+        ec2_packages = ['ec2-api-tools', 'awscli']
+        test_packages = []
+        ec2_post_install = None
+        version = '16.04'
+
     class Ubuntu_15_04(object):
         packages = ['openjdk-7-jdk']
         ec2_packages = ['ec2-api-tools', 'awscli']
@@ -149,7 +156,7 @@ class Ubuntu(object):
         ec2_post_install = standard_ec2_post_install
         version = '13.10'
 
-    versions = [Ubuntu_15_04, Ubuntu_14_04, Ubuntu_13_10]
+    versions = [Ubuntu_16_04, Ubuntu_15_04, Ubuntu_14_04, Ubuntu_13_10]
 
 distros = [
            Debian(),
