cloudius-systems/osv

commit db4a954fc5bebf02a0ad38b2323f874ed05c8c5f

math: add nearbyint() and friends
nearbyint() is just like rint(), with slightly different handling of
"inexact" exceptions. Just use the Musl implementation (we need to
ignore warnings on unsupported #pragmas in those files).

Fixes #851.

Signed-off-by: Nadav Har'El <nyh@scylladb.com>
Message-Id: <20170206085535.24209-1-nyh@scylladb.com>

Makefile
@@ -1220,9 +1220,12 @@ musl += math/modfl.o
 musl += math/nan.o
 musl += math/nanf.o
 musl += math/nanl.o
-#musl += math/nearbyint.o
-#musl += math/nearbyintf.o
-#musl += math/nearbyintl.o
+musl += math/nearbyint.o
+$(out)/musl/src/math/nearbyint.o: CFLAGS += -Wno-unknown-pragmas
+musl += math/nearbyintf.o
+$(out)/musl/src/math/nearbyintf.o: CFLAGS += -Wno-unknown-pragmas
+musl += math/nearbyintl.o
+$(out)/musl/src/math/nearbyintl.o: CFLAGS += -Wno-unknown-pragmas
 musl += math/nextafter.o
 musl += math/nextafterf.o
 musl += math/nextafterl.o

modules/tests/Makefile
@@ -85,7 +85,7 @@ tests := tst-pthread.so misc-ramdisk.so tst-vblk.so tst-bsd-evh.so \
 	payload-merge-env.so misc-execve.so misc-execve-payload.so misc-mutex2.so \
 	tst-pthread-setcancelstate.so tst-syscall.so tst-pin.so tst-run.so \
 	tst-ifaddrs.so tst-pthread-affinity-inherit.so tst-sem-timed-wait.so \
-	tst-ttyname.so tst-pthread-barrier.so tst-feexcept.so
+	tst-ttyname.so tst-pthread-barrier.so tst-feexcept.so tst-math.so
 
 #	libstatic-thread-variable.so tst-static-thread-variable.so \
 

tests/tst-math.cc
@@ -1,0 +1,43 @@
+/*
+ * Copyright (C) 2017 ScyllaDB, Ltd.
+ *
+ * This work is open source software, licensed under the terms of the
+ * BSD license as described in the LICENSE file in the top-level directory.
+ */
+
+// This test works on both Linux and OSv.
+// To compile on Linux, use: c++ -std=c++11 tests/tst-math.cc
+
+#include <math.h>
+
+#include <iostream>
+
+static int tests = 0, fails = 0;
+
+#define expect(actual, expected) do_expect(actual, expected, #actual, #expected, __FILE__, __LINE__)
+template<typename T>
+bool do_expect(T actual, T expected, const char *actuals, const char *expecteds, const char *file, int line)
+{
+    ++tests;
+    if (actual != expected) {
+        fails++;
+        std::cout << "FAIL: " << file << ":" << line << ": For " << actuals <<
+                ", expected " << expecteds << ", saw " << actual << ".\n";
+        return false;
+    }
+    return true;
+}
+
+#define expect_errno(call, experrno) ( \
+        do_expect(call, -1, #call, "-1", __FILE__, __LINE__) && \
+        do_expect(errno, experrno, #call " errno",  #experrno, __FILE__, __LINE__) )
+
+int main(int argc, char **argv)
+{
+    // Test nearbyint()
+    expect(nearbyint(1.3), 1.0);
+    expect(nearbyint(1.7), 2.0);
+
+    std::cout << "SUMMARY: " << tests << " tests, " << fails << " failures\n";
+    return fails == 0 ? 0 : 1;
+}

commit cdd253e9e06eda407c026f6f24c896d4a32d3a1c

makefile: use dummy fenv if platform doesn't provide one
Fix aarch64 build broken by commit:

db4a954fc5 math: add nearbyint() and friends

Signed-off-by: Sergiy Kibrik <sergiy.kibrik@globallogic.com>
Message-Id: <1486391978-20268-1-git-send-email-sergiy.kibrik@globallogic.com>

Makefile
@@ -1691,6 +1691,8 @@ musl += fenv/fegetexceptflag.o
 musl += fenv/feholdexcept.o
 musl += fenv/fesetexceptflag.o
 musl += fenv/$(musl_arch)/fenv.o
+else
+musl += fenv/fenv.o
 endif
 
 musl += crypt/crypt_blowfish.o