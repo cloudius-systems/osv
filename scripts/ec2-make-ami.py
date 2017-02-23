d230e99996cdbb542d950c06f961aff22a5b69ad
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2017-02-23 08:54:41 -0500

    Updated ec2-make-ami.py to boto3, added timers and allowed to specify location of img to create AMI from
    New ec2-make-ami.py uses boto3 to interact with AWS. Also it logs time it takes to execute
    all steps involved in creating an AMI. It also accepts new parameter -i that allows
    to specify location of the OSv image file.
    
    Please note that the new script (as old one) needs to be run on EC2 instance that it can
    mount EBS volume locally and copy OSv image into /dev/xvdf. Also creating the snapshot takes longest
    and is proportinonal to the size of the image.
    
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>
    Message-Id: <1487858081-2694-1-git-send-email-jwkozaczuk@gmail.com>

--- scripts/ec2-make-ami.py
+++ scripts/ec2-make-ami.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python2
 
-import boto.ec2, argparse, urllib, time, json, subprocess, os.path
+import boto3, argparse, urllib, time, json, subprocess, os.path
 import argparse
 
 class Metadata(object):
@@ -15,12 +15,10 @@ class Metadata(object):
         return self.availability_zone()[:-1]
 
 def wait_for(ec2_obj, status='available'):
-    while ec2_obj.status != status:
-        #print('object status {} wanted {}'.format(ec2_obj.status, status))
+    while ec2_obj.state != status:
+        print('object status {} wanted {}'.format(ec2_obj.state, status))
         time.sleep(1)
-        ec2_obj.update()
-
-input = 'build/release.x64/usr.img'
+        ec2_obj.reload()
 
 def to_gib(size):
     gib = 1 << 30
@@ -33,40 +31,66 @@ def image_size(filename):
 def copy_image(img, out):
     subprocess.check_call(['sudo', 'qemu-img', 'convert', '-O', 'raw', img, out])
 
-def make_ami(input, name):
+def make_snapshot(input):
     metadata = Metadata()
     print('Connecting')
-    conn = boto.ec2.connect_to_region(metadata.region())
-    print('Creating volume')
-    vol = conn.create_volume(size=to_gib(image_size(input)),
-                              zone=metadata.availability_zone(),
-                              )
+    ec2 = boto3.resource('ec2',region_name=metadata.region())
+    print('STEP 1: Creating volume') # aws ec2 create-volume
+    time_point = time.time()
+    vol = ec2.create_volume(Size=to_gib(image_size(input)),
+                            AvailabilityZone=metadata.availability_zone(),
+                            VolumeType='gp2',
+                            )
     print('Waiting for {}'.format(vol.id))
     wait_for(vol)
-    vol = conn.get_all_volumes([vol.id])[0]
-    print('Attaching {} to {}'.format(vol.id, metadata.instance_id()))
-    att = vol.attach(metadata.instance_id(), 'xvdf')
+    print('STEP 1: Took {} seconds to create volume {}'.format(time.time() - time_point,vol.id))
+    print('STEP 2: Attaching {} to {}'.format(vol.id, metadata.instance_id())) # aws ec2 attach-volume
+    time_point = time.time()
+    vol.attach_to_instance(InstanceId=metadata.instance_id(), Device='xvdf')
     while not os.path.exists('/dev/xvdf'):
-        #print('waiting for volume to attach')
+        print('waiting for volume to attach')
         time.sleep(1)
-    print('Copying image')
+    print('STEP 2: Took {} seconds to attach volume to instance {}'.format(time.time() - time_point,metadata.instance_id()))
+    print('STEP 3: Copying image')
+    time_point = time.time()
     copy_image(input, '/dev/xvdf')
-    print('Detaching {}'.format(vol.id))
-    vol.detach()
-    print('Creating snapshot from {}'.format(vol.id))
+    print('STEP 3: Took {} seconds to copy image'.format(time.time() - time_point))
+    print('STEP 4; Detaching volume {}'.format(vol.id))
+    time_point = time.time()
+    vol.detach_from_instance()
+    print('STEP 4: Took {} seconds to detach volume'.format(time.time() - time_point))
+    print('STEP 5: Creating snapshot from {}'.format(vol.id)) # aws ec2 create-snapshot
+    time_point = time.time()
     snap = vol.create_snapshot()
-    snap = conn.get_all_snapshots([snap.id])[0]
     wait_for(snap, 'completed')
-    print('Deleting {}'.format(vol.id))
+    print('STEP 5: Took {} seconds to create snapshot {}'.format(time.time() - time_point,snap.id))
+    print('STEP 6: Deleting volume {}'.format(vol.id))
+    time_point = time.time()
     vol.delete()
-    print('Registering image from {}'.format(snap.id))
-    ami = conn.register_image(name=name,
-                              architecture='x86_64',
-                              root_device_name='xvda',
-                              virtualization_type='hvm',
-                              snapshot_id=snap.id,
-                              delete_root_volume_on_termination=True,
-                              )
+    print('STEP 6: Took {} seconds to delete volume'.format(time.time() - time_point))
+    print('Snapshot {} created\n'.format(snap))
+    return snap.id
+
+def make_ami_from_snapshot(name,snapshot_id):
+    metadata = Metadata()
+    print('Connecting')
+    ec2 = boto3.resource('ec2',region_name=metadata.region())
+    print('STEP 7: Registering image from {}'.format(snapshot_id)) # aws ec2 register-image
+    time_point = time.time()
+    ami = ec2.register_image(Name=name,
+                             Architecture='x86_64',
+                             RootDeviceName='xvda',
+                             VirtualizationType='hvm',
+                             BlockDeviceMappings=[
+                                 {
+                                     'DeviceName' : 'xvda',
+                                     'Ebs': {
+                                         'SnapshotId': snapshot_id,
+                                         'DeleteOnTermination': True
+                                     }
+                                 },
+                             ])
+    print('STEP 7: Took {} seconds to create ami'.format(time.time() - time_point,ami))
     print('ami {} created\n'.format(ami))
     return ami
 
@@ -75,6 +99,9 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='run')
     parser.add_argument("-n", "--name", action="store", default="test-ami",
                         help="ami name to be created")
+    parser.add_argument("-i", "--input", action="store", default="build/release.x64/usr.img",
+                        help="path to the image on local filesysten")
 
     args = parser.parse_args()
-    make_ami(input, args.name)
+    snapshot_id = make_snapshot(args.input)
+    make_ami_from_snapshot(args.name,snapshot_id)
