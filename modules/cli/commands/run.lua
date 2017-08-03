cloudius-systems/osv

commit c621a007340913aa4832f3fda195b29114a41a6a

cli: optionaly run command in new ELF namespace
Extend cli run with option to start program in new ELF namespace.
Required for starting wrk, as it crashes if run in same ELF namespace as
cli.so - see also #882.

Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>
Message-Id: <20170627074952.1664-1-justin.cinkelj@xlab.si>

modules/cli/commands/run.lua
@@ -7,15 +7,22 @@ cmd.desc = [[run an executable in the background]]
 cmd.parser = OptionParser [[
 run - run an executable in the background
 
-Usage: run "command.so args..." ...
+Usage: run [OPTION]... "command.so args..." ...
+
+Options:
+
+      --newprogram start program in new ELF namespace
+      --help       display this help and exit
 ]]
 
 cmd.main = function(args)
   local args, opts = cmd.parser:parse(args)
+  flag_newprogram = opts.newprogram
 
   for i = 1, #args do
     osv_request({"app"}, "PUT", {
-      command = args[i]
+      command = args[i],
+      new_program = tostring(flag_newprogram)
     })
   end
 end

commit 7f58b2e5f089f6c8208c6f551906628c09de805c

cli: fix run command
The c621a007340913aa4832f3fda195b29114a41a6a broke cli run command.
It was possible to "run --newprogram /myapp". But "run /myapp" didn't do
anything - it didn't even complain about "Failed to load object: /myapp".

Signed-off-by: Justin Cinkelj <justin.cinkelj@xlab.si>
Message-Id: <20170802104652.22524-1-justin.cinkelj@xlab.si>

modules/cli/commands/run.lua
@@ -17,7 +17,8 @@ Options:
 
 cmd.main = function(args)
   local args, opts = cmd.parser:parse(args)
-  flag_newprogram = opts.newprogram
+  flag_newprogram = 0
+  if opts.newprogram then flag_newprogram=1 end
 
   for i = 1, #args do
     osv_request({"app"}, "PUT", {