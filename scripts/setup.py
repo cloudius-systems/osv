4caf065ea57fa2e6a206b986d4c296b038fdf3a3
Nadav Har'El <nyh@scylladb.com>
2017-11-16 14:02:19 +0200

    scripts/setup.py: support Fedora 27
    No changes appear to be needed in Fedora 27 vs what we needed in Fedora 26.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- scripts/setup.py
+++ scripts/setup.py
@@ -91,7 +91,14 @@ class Fedora(object):
         ec2_post_install = None
         version = '26'
 
-    versions = [Fedora_19, Fedora_20, Fedora_21, Fedora_22, Fedora_23, Fedora_24, Fedora_25, Fedora_26]
+    class Fedora_27(object):
+        packages = ['java-1.8.0-openjdk', 'python2-requests', 'compat-openssl10-devel']
+        ec2_packages = []
+        test_packages = []
+        ec2_post_install = None
+        version = '27'
+
+    versions = [Fedora_19, Fedora_20, Fedora_21, Fedora_22, Fedora_23, Fedora_24, Fedora_25, Fedora_26, Fedora_27]
 
 class RHELbased(Fedora):
     name = ['Scientific Linux', 'NauLinux', 'CentOS Linux',
