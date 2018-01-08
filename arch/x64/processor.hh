cloudius-systems/osv

commit 0959f20863b119c5ca7d1e663bc22e6329814386

save fpu state/control registers on every context switch
As described in issue #9, we have a dangling problem related to context switch
and some of the FPU state. In a quick summary, the problem arises from the fact
that while the FPU state per se is caller saved, the control registers are
callee saved. This means that threads being voluntarily taken out of the cpu
will have that state broken.

The simplest way to solve this problem is to save / restore those registers at
every context switch. It should not be that expensive, since we're not talking
about the whole FPU state, but rather just a couple of registers. To be sure of
that, I am using misc-ctxt.so, which yields the following numbers:

Base:
OSv v0.12-rc1-21-g8660a6f
eth0: 192.168.122.15
       454 colocated
       704 apart
       546 nopin

Patched:
OSv v0.12-rc1-21-g8660a6f
eth0: 192.168.122.15
       459 colocated
       685 apart
       506 nopin

Signed-off-by: Glauber Costa <glommer@cloudius-systems.com>
Reviewed-by: Nadav Har'El <nyh@cloudius-systems.com>
Signed-off-by: Avi Kivity <avi@cloudius-systems.com>

arch/x64/processor.hh
@@ -388,6 +388,30 @@ inline bool rdrand(u64* dest)
     return ok;
 }
 
+inline uint32_t stmxcsr()
+{
+    uint32_t addr;
+    asm volatile ("stmxcsr %0" : "=m" (addr));
+    return addr;
+}
+
+inline void ldmxcsr(uint32_t addr)
+{
+    asm volatile ("ldmxcsr %0" : : "m" (addr));
+}
+
+inline uint16_t fnstcw()
+{
+    uint16_t addr;
+    asm volatile ("fnstcw %0" : "=m" (addr));
+    return addr;
+}
+
+inline void fldcw(uint16_t addr)
+{
+    asm volatile ("fldcw %0" : : "m" (addr));
+}
+
 struct task_state_segment {
     u32 reserved0;
     u64 rsp[3];

arch/x64/arch-switch.hh
@@ -55,6 +55,8 @@ void thread::switch_to()
     old->_state.exception_stack = c->arch.get_exception_stack();
     c->arch.set_interrupt_stack(&_arch);
     c->arch.set_exception_stack(_state.exception_stack);
+    auto fpucw = processor::fnstcw();
+    auto mxcsr = processor::stmxcsr();
     asm volatile
         ("mov %%rbp, %c[rbp](%0) \n\t"
          "movq $1f, %c[rip](%0) \n\t"
@@ -70,6 +72,8 @@ void thread::switch_to()
            [rip]"i"(offsetof(thread_state, rip))
          : "rbx", "rdx", "rsi", "rdi", "r8", "r9",
            "r10", "r11", "r12", "r13", "r14", "r15", "memory");
+    processor::fldcw(fpucw);
+    processor::ldmxcsr(mxcsr);
 }
 
 void thread::switch_to_first()

commit fbbc26d5aa9844f3280731d74f5dfbcfc33a1cff

x64/processor.hh: fix wrongly named variable
Commit 0959f20863b119c5ca7d1e663bc22e6329814386 introduced new x64 functions
for saving and restoring the FPU control registers. The patch was correct,
but used a very misleading variable name "addr", while in fact these functions
pass values - the actual bit mask saved in those register - not addresses.

This patch only changes variable names - it should have no functional
significance.

Signed-off-by: Nadav Har'El <nyh@scylladb.com>
Message-Id: <20180107115058.5026-1-nyh@scylladb.com>

arch/x64/processor.hh
@@ -413,26 +413,26 @@ inline bool rdrand(u64* dest)
 
 inline uint32_t stmxcsr()
 {
-    uint32_t addr;
-    asm volatile ("stmxcsr %0" : "=m" (addr));
-    return addr;
+    uint32_t bits;
+    asm volatile ("stmxcsr %0" : "=m" (bits));
+    return bits;
 }
 
-inline void ldmxcsr(uint32_t addr)
+inline void ldmxcsr(uint32_t bits)
 {
-    asm volatile ("ldmxcsr %0" : : "m" (addr));
+    asm volatile ("ldmxcsr %0" : : "m" (bits));
 }
 
 inline uint16_t fnstcw()
 {
-    uint16_t addr;
-    asm volatile ("fnstcw %0" : "=m" (addr));
-    return addr;
+    uint16_t bits;
+    asm volatile ("fnstcw %0" : "=m" (bits));
+    return bits;
 }
 
-inline void fldcw(uint16_t addr)
+inline void fldcw(uint16_t bits)
 {
-    asm volatile ("fldcw %0" : : "m" (addr));
+    asm volatile ("fldcw %0" : : "m" (bits));
 }
 
 struct task_state_segment {