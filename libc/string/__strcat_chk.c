a3aab6784120b00ad69816ee5ed37f256775641a
Nadav Har'El <nyh@scylladb.com>
2018-05-02 23:51:21 +0300

    Fix warning about strncat
    The implementation of __strcat_chk used strncat() in a way (with length
    equal to the length of the source string) that causes Gcc 8 to warn about.
    I think this warning is a false-positive, but nevertheless, the easiest
    way to shut it up is to switch to strcat(), which works just as well here.
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- libc/string/__strcat_chk.c
+++ libc/string/__strcat_chk.c
@@ -3,7 +3,7 @@
 
 char *__strcat_chk(char *d, const char *s, size_t s1len)
 {
-    // TODO: This repeats some of strncat's work. Make it more efficent.
+    // TODO: This repeats some of strcat's work. Make it more efficent.
     size_t n = strlen(s);
     size_t space = s1len;
     for (const char *tmp = d; *tmp; tmp++) {
@@ -12,5 +12,5 @@ char *__strcat_chk(char *d, const char *s, size_t s1len)
     }
     // Need space for strlen(s) and the null
     assert (space > n);
-    return strncat(d, s, n);
+    return strcat(d, s);
 }
