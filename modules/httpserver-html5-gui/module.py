03ed630433be20b4a4bb1354cf06ff1a4a04889e
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2019-03-10 00:49:41 -0500

    Changed httpserver-html5-gui conf location to /etc
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- modules/httpserver-html5-gui/module.py
+++ modules/httpserver-html5-gui/module.py
@@ -8,13 +8,13 @@ _module = '${OSV_BASE}/modules/httpserver-html5-gui'
 usr_files = FileMap()
 usr_files.add(os.path.join(_module, 'swagger-ui', 'dist')).to('/usr/mgmt/swagger-ui/dist')
 usr_files.add(os.path.join(_module, 'osv-gui/public')).to('/usr/mgmt/gui')
-usr_files.add(os.path.join(_module, 'httpserver.conf')).to('/tmp/httpserver.conf')
+usr_files.add(os.path.join(_module, 'httpserver.conf')).to('/etc/httpserver.conf')
 
 api.require('httpserver-api')
 
 # httpserver will run regardless of an explicit command line
 # passed with "run.py -e".
-_exe = '/libhttpserver.so'
+_exe = '/libhttpserver-api.so --config-file=/etc/httpserver.conf'
 daemon = api.run_on_init(_exe + ' &!')
 
 fg = api.run(_exe)
