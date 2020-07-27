c741802e3043bb2127c03391a9ae13cac772557f
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2020-07-27 12:43:50 -0400

    libc: revert some files with "__restrict" to point back to musl
    At some point in the beginning of OSv the musl sources were copied
    to libc/ folder and then gradually removed in favor of the original
    copies in musl/src/ folder.
    
    This patch removes another 15 files from libc/string/
    which only differred by "__reserved" vs "restrict" keyword.
    The original musl sources have "restrict" keyword and their
    corresponding copies have "__reserved" keyword even though
    per this commit log 5cd0168fc566e5f7263b04948b4df6695d8ae721
    they seem to have been copied "as is" from musl.
    
    Based on what I have researched the __restrict keyword
    is a GCC extension and predates "restrict" and it happens
    to have same effect on pointer optimization as the latter one.
    We also compile all C source files with "-std=gnu99" option
    which adds GCC extensions to c99. So all in all I think
    we can drop the difference and simply point to the original
    musl sources.
    
    This patch further eliminates unnecessary files in libc/
    folder.
    
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>
    Message-Id: <20200727164350.10609-2-jwkozaczuk@gmail.com>

--- libc/string/strcat.c
+++
@@ -1,7 +0,0 @@
-#include <string.h>
-
-char *strcat(char *__restrict dest, const char *__restrict src)
-{
-	strcpy(dest + strlen(dest), src);
-	return dest;
-}
--- libc/string/wcscat.c
+++
@@ -1,7 +0,0 @@
-#include <wchar.h>
-
-wchar_t *wcscat(wchar_t *__restrict dest, const wchar_t *__restrict src)
-{
-	wcscpy(dest + wcslen(dest), src);
-	return dest;
-}
