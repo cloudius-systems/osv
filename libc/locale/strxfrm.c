28f5e62eaae90c35d2f49f3193662368625bd6a6
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2020-08-15 00:27:17 -0400

    libc: replace locale/strxfrm*.c (from old musl) with musl copy of locale/strxfrm.c
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>
    Message-Id: <20200815042719.51117-7-jwkozaczuk@gmail.com>

--- libc/locale/strxfrm.c
+++
@@ -1,9 +0,0 @@
-#include <string.h>
-
-/* collate only by code points */
-size_t strxfrm(char *restrict dest, const char *restrict src, size_t n)
-{
-	size_t l = strlen(src);
-	if (n > l) strcpy(dest, src);
-	return l;
-}
