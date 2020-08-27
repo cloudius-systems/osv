7597b2e87ecb056eea3359f8efc16a1d3bf7537c
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2020-08-27 00:57:23 -0400

    libc: replace 8 headers in include/api with their symlinked copies
    All these files were copied from musl and then modified to
    rename fields like __unused and __unused2 to ___unused and ___unused2
    respectively to avoid conflicts with exactly same named macros
    in BSD source tree headers.
    
    Because previous patch renamed those BSD macros to
    __bsd_unused and __bsd_unused2 we are reverting these files
    to musl copies with symlinks.
    
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>
    Message-Id: <20200827045723.29698-1-jwkozaczuk@gmail.com>

--- include/api/x64/bits/msg.h
+++ include/api/x64/bits/msg.h
@@ -1,13 +1,1 @@
-struct msqid_ds
-{
-	struct ipc_perm msg_perm;
-	time_t msg_stime;
-	time_t msg_rtime;
-	time_t msg_ctime;
-	unsigned long msg_cbytes;
-	msgqnum_t msg_qnum;
-	msglen_t msg_qbytes;
-	pid_t msg_lspid;
-	pid_t msg_lrpid;
-	unsigned long ___unused[2];
-};
+../../../../musl/arch/x86_64/bits/msg.h
