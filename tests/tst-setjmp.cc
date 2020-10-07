8afecd2ed0dde62ca78eae35049ed5e4445abc0b
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2020-10-07 10:46:48 -0400

    tests: add simple tst-setjmp.cc to verify setjmp/longjmp/sigsetjmp/siglongjmp functionality
    This test fails on aarch64 until we fix broken setjmp/longjmp/sigsetjmp/siglongjmp
    for aarch64.
    
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

---
+++ tests/tst-setjmp.cc
@@ -0,0 +1,59 @@
+#include <fenv.h>
+#include <__fenv.h>
+#include <signal.h>
+#include <assert.h>
+#include <setjmp.h>
+#include <math.h>
+
+#include <iostream>
+
+static int tests = 0, fails = 0;
+
+#define expect(actual, expected) do_expect(actual, expected, #actual, #expected, __FILE__, __LINE__)
+template<typename T>
+bool do_expect(T actual, T expected, const char *actuals, const char *expecteds, const char *file, int line)
+{
+    ++tests;
+    if (actual != expected) {
+        fails++;
+        std::cout << "FAIL: " << file << ":" << line << ": For " << actuals <<
+                ", expected " << expecteds << ", saw " << actual << ".\n";
+        return false;
+    }
+    return true;
+}
+
+//This is a simple test that verifies that setjmp/longjmp/sigsetjmp/siglongjmp
+//flow control works correctly.
+
+static jmp_buf env;
+bool setjmp_check() {
+    if (setjmp(env)) {
+        std::cout << "Back from longjmp()!\n";
+        return true;
+    }
+    std::cout << "After setjmp()!\n";
+    longjmp(env, 1);
+    return false;
+}
+
+static sigjmp_buf sig_env;
+bool sigsetjmp_check(int savesigs) {
+    if (sigsetjmp(sig_env, savesigs)) {
+        std::cout << "Back from siglongjmp()!\n";
+        return true;
+    }
+    std::cout << "After sigsetjmp()!\n";
+    siglongjmp(sig_env, 1);
+    return false;
+}
+
+int main(int argc, char **argv)
+{
+    expect(setjmp_check(), true);
+    expect(sigsetjmp_check(0), true);
+    expect(sigsetjmp_check(1), true);
+
+    std::cout << "SUMMARY: " << tests << " tests, " << fails << " failures\n";
+    return fails == 0 ? 0 : 1;
+}
