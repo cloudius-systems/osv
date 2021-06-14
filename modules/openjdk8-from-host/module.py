615e456ec91690c608f6e4a7b33e8d849cd4ae69
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2021-06-14 00:20:46 -0400

    openjdk8: make it work with aarch64
    This patch modifies existing openjdk8-from-host module to make
    it work on aarch64 Linux host.
    
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>
    Message-Id: <20210614042047.13437-2-jwkozaczuk@gmail.com>

--- modules/openjdk8-from-host/module.py
+++ modules/openjdk8-from-host/module.py
@@ -3,22 +3,32 @@ from osv.modules import api
 import os, os.path
 import subprocess
 
+host_arch = os.uname().machine
+if host_arch == 'aarch64':
+    arch_dir = 'aarch64'
+elif host_arch == 'x86_64':
+    arch_dir = 'amd64'
+else:
+    print('Unsupported architecture: %s' % host_arch)
+    exit(-1)
+
 api.require('java-cmd')
 provides = ['java','java8']
 
 #Verify that the jdk exists by trying to locate javac (java compiler)
 if subprocess.call(['which', 'javac']) != 0:
-     print('Could not find any jdk on the host. Please install openjdk8!')
-     os.exit(-1)
-
-java_version = subprocess.check_output(['java', '-version'], stderr=subprocess.STDOUT).decode('utf-8')
-if not 'openjdk version "1.8.0' in java_version:
-    print('Could not find openjdk version 8 on the host. Please install openjdk8!')
-    os.exit(-1)
+    print('Could not find any jdk on the host. Please install openjdk8!')
+    exit(-1)
 
 javac_path = subprocess.check_output(['which', 'javac']).decode('utf-8').split('\n')[0]
 javac_real_path = os.path.realpath(javac_path)
+java_real_path = os.path.dirname(javac_real_path) + '/java'
 jdk_path = os.path.dirname(os.path.dirname(javac_real_path))
+
+java_version = subprocess.check_output([java_real_path, '-version'], stderr=subprocess.STDOUT).decode('utf-8')
+if not 'openjdk version "1.8.0' in java_version:
+    print('Could not find openjdk version 8 on the host. Please install openjdk8!')
+    exit(-1)
 
 usr_files = FileMap()
 
@@ -27,8 +37,8 @@ jdk_dir = os.path.basename(jdk_path)
 usr_files.add(jdk_path).to('/usr/lib/jvm/java') \
     .include('jre/**') \
     .exclude('jre/lib/security/cacerts') \
-    .exclude('jre/lib/amd64/*audio*') \
-    .exclude('jre/lib/amd64/*sound*') \
+    .exclude('jre/lib/%s/*audio*' % arch_dir) \
+    .exclude('jre/lib/%s/*sound*' % arch_dir) \
     .exclude('') \
     .exclude('jre/man/**') \
     .exclude('jre/bin/**') \
