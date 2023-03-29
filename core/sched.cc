894ceb2c035cba71c9b6bdc83e8d0613850e5dc6
Nadav Har'El <nyh@scylladb.com>
2023-03-29 12:05:13 +0300

    sched: low-level support for POSIX-compatible real-time scheduling
    This patch adds "real-time priority" to OSv threads, which can be used
    to implement Posix's SCHED_FIFO and SCHED_RR scheduling policies.
    
    Refs #386. It doesn't yet "Fixes" it because this patch does not yet add
    the Posix front-end to this implementation (sched_setscheduler() et al.) -
    this will be added in a separate patch.
    
    We add to each thread a _realtime._priority, an unsigned integer.
    Normal (non-real-time) threads have _realtime._priority of 0, and are
    time-shared fairly as before, while realtime threads have
    _realtime._priority > 0, and preempt any thread with a lower realtime
    priority, including all normal threads.
    
    We add a new API, t->set_realtime_priority(priority), to make a thread
    real-time scheduled. The "priority" is an unsigned int as explained above
    (0 means normal thread). The resulting scheduling policy is fully
    compatible with POSIX's SCHED_FIFO. This includes following the intricate
    rules on what happens to the running thread's place in the queue when there
    are several real-time threads with the same priority and the running
    thread yields, waits, or is preempted by a higher-priority thread.
    This patch also adds an exercise for these cases in tests/misc-scheduler.cc.
    
    This patch does not yet implement time slices as needed for supporting
    POSIX's SCHED_RR policy. This patch already includes a time-slice setting
    API, t->set_realtime_time_slice(duration), but if used it produces a
    warning message, and the scheduler currently ignores this setting.
    Adding support for real-time time slices is left for a future patch:
    To implement time slices, we will need to keep in _realtime the total
    time this thread ran since starting this slice (only zeroed when the
    thread is removed from the run queue), and each time we switch to a
    realtime thread with timeslice >= 0, we'll set an appropriate timer
    (for timeslice minus already used time).
    
    I tested that although this patch adds code into the scheduler it does
    not noticably slow down context switches of normal threads. E.g.,
    tests/misc-ctxsw.so measured colocated context switch time of 240ns
    before this patch, and exactly the same after it.
    
    While developing this patch I tested it using added workloads in
    tests/misc-scheduler.cc, included in this patch, but unfortunately
    this is not a real regression test - it has no assertions and cannot
    automatically detect failure (and, like all the "misc-*" tests, it
    doesn't run automatically in "make check").
    
    Signed-off-by: Nadav Har'El <nyh@scylladb.com>

--- core/sched.cc
+++ core/sched.cc
@@ -26,6 +26,7 @@
 #include <osv/preempt-lock.hh>
 #include <osv/app.hh>
 #include <osv/symbols.hh>
+#include <osv/stubbing.hh>
 
 MAKE_SYMBOL(sched::thread::current);
 MAKE_SYMBOL(sched::cpu::current);
@@ -290,7 +291,16 @@ void cpu::reschedule_from_interrupt(bool called_from_yield,
 #endif
         } else if (!called_from_yield) {
             auto &t = *runqueue.begin();
-            if (p->_runtime.get_local() < t._runtime.get_local()) {
+            if (p->_realtime._priority > 0) {
+                // Only switch to a higher-priority realtime thread
+                if (t._realtime._priority <= p->_realtime._priority) {
+#ifdef __aarch64__
+                    return switch_data;
+#else
+                    return;
+#endif
+                }
+            } else if (t._realtime._priority == 0 && p->_runtime.get_local() < t._runtime.get_local()) {
                 preemption_timer.cancel();
                 auto delta = p->_runtime.time_until(t._runtime.get_local());
                 if (delta > 0) {
@@ -302,6 +312,20 @@ void cpu::reschedule_from_interrupt(bool called_from_yield,
                 return;
 #endif
             }
+        } else { /* called_from_yield */
+            if (p->_realtime._priority > 0) {
+                auto &t = *runqueue.begin();
+                // When yielding, only switch if the next thread has a higher
+                // or same priority as p (i.e., don't switch if t it has a
+                // lesser priority than p).
+                if (t._realtime._priority < p->_realtime._priority) {
+#ifdef __aarch64__
+                    return switch_data;
+#else
+                    return;
+#endif
+                }
+            }
         }
         // If we're here, p no longer has the lowest runtime. Before queuing
         // p, return the runtime it borrowed for hysteresis.
@@ -309,7 +333,10 @@ void cpu::reschedule_from_interrupt(bool called_from_yield,
         p->_detached_state->st.store(thread::status::queued);
 
         if (!called_from_yield) {
-            enqueue(*p);
+            // POSIX requires that if a real-time thread doesn't yield but
+            // rather is preempted by a higher-priority thread, it be
+            // reinserted into the runqueue first, not last, among its equals.
+            enqueue_first_equal(*p);
         }
 
         trace_sched_preempt();
@@ -350,16 +377,18 @@ void cpu::reschedule_from_interrupt(bool called_from_yield,
         n->_runtime.add_context_switch_penalty();
     }
     preemption_timer.cancel();
-    if (!called_from_yield) {
-        if (!runqueue.empty()) {
-            auto& t = *runqueue.begin();
-            auto delta = n->_runtime.time_until(t._runtime.get_local());
-            if (delta > 0) {
-                preemption_timer.set_with_irq_disabled(now + delta);
+    if (n->_realtime._priority == 0) {
+        if (!called_from_yield) {
+            if (!runqueue.empty()) {
+                auto& t = *runqueue.begin();
+                auto delta = n->_runtime.time_until(t._runtime.get_local());
+                if (delta > 0) {
+                    preemption_timer.set_with_irq_disabled(now + delta);
+                }
             }
+        } else {
+            preemption_timer.set_with_irq_disabled(now + preempt_after);
         }
-    } else {
-        preemption_timer.set_with_irq_disabled(now + preempt_after);
     }
 
     if (app_thread.load(std::memory_order_relaxed) != n->_app) { // don't write into a cache line if it can be avoided
@@ -528,6 +557,16 @@ void cpu::enqueue(thread& t)
 {
     trace_sched_queue(&t);
     runqueue.insert_equal(t);
+}
+
+// When the run queue has several threads with equal thread_runtime_compare,
+// enqueue() puts a thread after its equals, while enqueue_first_equal()
+// puts it before its equals. The distinction is mostly interesting for real-
+// time priority threads.
+void cpu::enqueue_first_equal(thread& t)
+{
+    trace_sched_queue(&t);
+    runqueue.insert_before(runqueue.lower_bound(t), t);
 }
 
 void cpu::init_on_cpu()
@@ -864,6 +903,29 @@ void thread::set_priority(float priority)
 float thread::priority() const
 {
     return _runtime.priority();
+}
+
+void thread::set_realtime_priority(unsigned priority)
+{
+    _realtime._priority = priority;
+}
+
+unsigned thread::realtime_priority() const
+{
+    return _realtime._priority;
+}
+
+void thread::set_realtime_time_slice(thread_realtime::duration time_slice)
+{
+    if (time_slice > 0) {
+        WARN_ONCE("set_realtime_time_slice() used but real-time time slices"
+                " not yet supported");
+    }
+    _realtime._time_slice = time_slice;
+}
+
+thread_realtime::duration thread::realtime_time_slice() const {
+    return _realtime._time_slice;
 }
 
 sched::thread::status thread::get_status() const
