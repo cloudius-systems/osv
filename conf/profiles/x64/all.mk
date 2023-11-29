b309abbe7a6a0c9d30ecba1587d856a7c18582ff
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2023-11-29 18:01:20 -0500

    ena: add upper "thin" layer in form of OSv driver class
    This almost final patch implements a very upper "thin" layer in form of
    the aws::ena driver class that subclasses from hw_driver.
    
    The contructor, destructor and probe() merely delegate to functions
    ena_attach(), ena_detach() and ena_probe() respectively implemented
    in bsd/sys/dev/ena/ena.cc.
    
    Please note that some of the statistics functionality (see fill_stats())
    and if_getinfo are left unimplemented for now.
    
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- conf/profiles/x64/all.mk
+++ conf/profiles/x64/all.mk
@@ -4,6 +4,6 @@ include conf/profiles/$(arch)/virtio-mmio.mk
 include conf/profiles/$(arch)/virtio-pci.mk
 include conf/profiles/$(arch)/vmware.mk
 include conf/profiles/$(arch)/xen.mk
+include conf/profiles/$(arch)/aws.mk
 
 conf_drivers_vga?=1
-conf_drivers_ena?=1
--- core/debug.cc
+++ core/debug.cc
@@ -47,6 +47,7 @@ bool logger::parse_configuration(void)
     add_tag("poll", logger_info);
     add_tag("dhcp", logger_info);
     add_tag("acpi", logger_error);
+    add_tag("ena", logger_debug);
 
     return (true);
 }
