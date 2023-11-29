5c55d5b972bc663a0b4ffd58fe84af22930e1465
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2023-11-29 13:09:50 -0500

    ena: adapt data path code to OSv
    This patch adapts the middle layer of data path handling logic
    to make it work in OSv. For more details about it please see
    https://github.com/amzn/amzn-drivers/tree/master/kernel/fbsd/ena#data-path-interface.
    and https://github.com/amzn/amzn-drivers/tree/master/kernel/fbsd/ena#data-path.
    
    In high level the main entry point for RX part is ena_cleanup() that
    delegates to ena_rx_cleanup() and eventually ends up calling net
    channel ifp->if_classifier.post_packet() (fast path) or ifp->if_input()
    (slow path). The ena_cleanup() is called by cleanup_work thread that
    is woken every time the MSI-X vector for given TX/RX queue is called.
    
    Similarly, the main entry point for TX part is ena_mq_start() which
    is what ifp->if_transmit is set to and ena_deferred_mq_start() which is
    called by enqueue_work thread that is woken in ena_mq_start() and
    ena_tx_cleanup() (other part of ena_cleanup() routine).
    
    Finally, ena_qflush is what ifp->if_qflush is set to.
    
    The particular code changes to ena_datapath.* involve following:
    
    - implement critical_enter()/critical_exit() used by buf_ring (see
      https://man.freebsd.org/cgi/man.cgi?query=critical_enter)
    
    - for now remove RSS and DEV_NETMAP related code
    
    - replace the drbr_* functions with buf_ring_* equivalent ones
    
    - replace taskqueue_enqueue() with OSv wake_with()
    
    - adapt references to the mbuf fields to match OSv version of
      it (please https://github.com/freebsd/freebsd-src/commit/3d1a9ed34e10dc6c5b6d86617464534ac9fc928a
      commit that changed the layout of mbuf struct a bit)
    
    - simplify ena_tx_map_mbuf() given we hard-code to use ENA_ADMIN_PLACEMENT_POLICY_HOST
      TX queue type and do not use bus_dma API (see
      https://man.freebsd.org/cgi/man.cgi?query=bus_dma)
    
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- bsd/sys/dev/ena/ena_datapath.h
+++ bsd/sys/dev/ena/ena_datapath.h
@@ -34,12 +34,14 @@
 #ifndef ENA_TXRX_H
 #define ENA_TXRX_H
 
-void ena_cleanup(void *arg, int pending);
+typedef struct ifnet* if_t;
+
+void ena_cleanup(struct ena_que *que);
 void ena_qflush(if_t ifp);
 int ena_mq_start(if_t ifp, struct mbuf *m);
-void ena_deferred_mq_start(void *arg, int pending);
+void ena_deferred_mq_start(ena_ring *ring);
 
 #define CSUM_OFFLOAD (CSUM_IP | CSUM_TCP | CSUM_UDP)
-#define CSUM6_OFFLOAD (CSUM_IP6_UDP | CSUM_IP6_TCP)
+#define CSUM6_OFFLOAD (CSUM_UDP_IPV6 | CSUM_TCP_IPV6)
 
 #endif /* ENA_TXRX_H */
