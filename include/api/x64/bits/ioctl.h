482901f6cde14b500e2858e9a4fc0ad3508c0698
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2024-06-16 22:38:18 -0400

    fix _IOW, _IOR and _IORW macro
    This fixes the overflow issue as described by the issue #1313.
    
    Fixes #1313
    
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- include/api/x64/bits/ioctl.h
+++ include/api/x64/bits/ioctl.h
@@ -4,9 +4,9 @@
 #define _IOC_READ  2U
 
 #define _IO(a,b) _IOC(_IOC_NONE,(a),(b),0)
-#define _IOW(a,b,c) _IOC(1,(a),(b),sizeof(c))
-#define _IOR(a,b,c) _IOC(2,(a),(b),sizeof(c))
-#define _IOWR(a,b,c) _IOC(3,(a),(b),sizeof(c))
+#define _IOW(a,b,c) _IOC(_IOC_WRITE,(a),(b),sizeof(c))
+#define _IOR(a,b,c) _IOC(_IOC_READ,(a),(b),sizeof(c))
+#define _IOWR(a,b,c) _IOC(_IOC_READ | _IOC_WRITE,(a),(b),sizeof(c))
 
 #define TCGETS		0x5401
 #define TCSETS		0x5402
