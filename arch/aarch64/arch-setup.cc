d86de93202fa58e9b99a228f901ef7b7fc17aba1
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2025-05-21 12:19:12 -0400

    aarch64: enable NVME support
    This patch enables NVME driver on aarch64 and enhances run.py
    to allow running OSv from a NVME disk.
    
    This makes the Graviton support (see #1295) one more step closer to
    and "only" two issues away from the full implementation.
    
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- arch/aarch64/arch-setup.cc
+++ arch/aarch64/arch-setup.cc
@@ -198,6 +198,9 @@ void arch_init_premain()
 #if CONF_drivers_virtio_fs
 #include "drivers/virtio-fs.hh"
 #endif
+#if CONF_drivers_nvme
+#include "drivers/nvme.hh"
+#endif
 
 void arch_init_drivers()
 {
@@ -252,6 +255,9 @@ void arch_init_drivers()
 #endif
 #if CONF_drivers_virtio_fs
     drvman->register_driver(virtio::fs::probe);
+#endif
+#if CONF_drivers_nvme
+    drvman->register_driver(nvme::driver::probe);
 #endif
     boot_time.event("drivers probe");
     drvman->load_all();
