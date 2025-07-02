2eb03b424f484ae271a522c0d75cb4c80fb49e4f
Waldemar Kozaczuk <jwkozaczuk@gmail.com>
2025-07-02 11:11:37 -0400

    support building ext images
    This patch updates existing scripts and adds new `ext-disk-utils.sh`
    to support building OSv images with ext root filesystem.
    
    More specifically, we update `scripts/build` to support new values of the
    parameter `fs`:
    - `ext` - to build an image with the `ext` filesystem mounted at root
      (`/`)
    - `rofs_with_ext` - to build an image with the `rofs` at `/` and `ext`
      in the 2nd disk partition mounted at `/data`
    
    In addition, we add new script `ext-disk-utils.sh`, called by `scripts/build`.
    The `ext-disk-utils.sh` utilizes the standard `mkfs.ext` utility from
    Linux host to create empty ext disk file, which is then set up as a
    loop device and mounted to populate its content using the `export_manifest.py`.
    
    The `ext` filesystem is implemented as a library similarly to the ZFZ.
    Just like `libsolaris.so` of ZFS, the ext library object files - `libext.so`
    and its dependent `liblwext4.so` - are placed on the bootfs filesystem
    which is part of the kernel loader - `loader.elf`, if the root
    filesystem is ext. If ext is on the 2nd partition - `fs=rofs_with_ext`,
    then `libext.so` and `liblwext4.so` are loaded from the root rofs
    partition just like other binaries would.
    
    The `libext.so` comes from the `libext` module and the `liblwext4.so`
    from the `lwext4` one. These two modules get added implicitly by
    `scripts/build` is `fs=ext` or `fs=rofs_with_ext`.
    
    Finally, this patch modifies the main makefile, to handle passed in `fs`
    parameter to drive what files need to be put into the `bootfs.bin`. It
    is worth noting that as a side effect, the `libsolaris.so` is only built
    if `fs=zfs` which stays as a default in `scripts/build`.
    
    For more information please read this Wiki page -
    https://github.com/cloudius-systems/osv/wiki/Filesystems#ext.
    
    Signed-off-by: Waldemar Kozaczuk <jwkozaczuk@gmail.com>

--- modules/libext/module.py
+++ modules/libext/module.py
@@ -1,3 +1,11 @@
+import os
 from osv.modules import api
+from osv.modules.filemap import FileMap
+
+build_dir = 'build/' + os.environ['mode'] + '.' + os.environ['ARCH']
+
+usr_files = FileMap()
+if os.environ.get("fs_type") != 'ext':
+    usr_files.add('${OSV_BASE}/' + build_dir + '/modules/libext/libext.so').to('/usr/lib/fs/libext.so')
 
 api.require('lwext4')
