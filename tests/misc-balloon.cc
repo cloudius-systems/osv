cb8c7205b3475be6041d3223dd33481126c79e07
Greg Burd <greg@burd.me>
2026-07-14 07:01:21 -0400

    copyright: correct new-file attribution to the actual author

--- tests/misc-balloon.cc
+++ tests/misc-balloon.cc
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2026 Waldemar Kozaczuk
+ * Copyright (C) 2026 Greg Burd
  *
  * This work is open source software, licensed under the terms of the
  * BSD license as described in the LICENSE file in the top-level directory.
